a26a1a2cccd490dcf1a1deeccd2231753167c175
Jón Bjarni Bjarnason <jon.bjarni@skaginn3x.com>
2023-02-17 14:57:46 +0000

    add vcpkg dependencies and fix cmake warnings

--- vcpkg-overlays/ports/glaze/portfile.cmake
+++ vcpkg-overlays/ports/glaze/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO stephenberry/glaze
     REF v0.3.6
     SHA512 80e5dd02f1bc41458aa483affed8a1a00320d2301ee7ba32bb87aebf02e9a0db19ac748d2252f2a9a9756ccd6b0af5cb673070730b7a9e4bf6e1f9aafaf05447
+    PATCHES
+      disable-dev-mode.patch
 )
 
 vcpkg_cmake_configure(
@@ -10,6 +12,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-#vcpkg_fixup_cmake_targets()
 
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/glaze RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/glaze RENAME copyright)
