79f86014bbb3d4d00675384038ea07345c529311
Jón Bjarni Bjarnason <jon.bjarni@skaginn3x.com>
2023-03-13 15:50:52 +0000

    wip creating abstract jsonrpc socket server

--- libs/confman/inc/public/tfc/confman.hpp
+++ libs/confman/inc/public/tfc/confman.hpp
@@ -6,27 +6,27 @@
 constexpr std::string_view confman_socket = "/var/run/confman.sock";
 
 namespace tfc::confman {
-template<typename T>
-concept non_floating = std::equality_comparable<T> && !std::is_floating_point_v<T>;
+template<typename conf_param_t>
+concept non_floating = std::equality_comparable<conf_param_t> && !std::is_floating_point_v<conf_param_t>;
 
-template<non_floating T>
+template<non_floating conf_param_t>
 class on_change {
 public:
-  on_change(const T&& value, std::function< void (T)>&& callback) : value_{std::move(value)}, callback_{std::move(callback)} {}
-  void set(const T&& value){
+  on_change(const conf_param_t&& value, std::function< void (conf_param_t)>&& callback) : value_{std::move(value)}, callback_{std::move(callback)} {}
+  void set(const conf_param_t&& value){
     if (value != value_){
       value_ = std::move(value);
       callback_(value_);
     }
   }
-  auto value() const noexcept -> T const & { return value_; }
+  auto value() const noexcept -> conf_param_t const & { return value_; }
 private:
-  T value_;
-  std::function<void (T)> callback_;
+  conf_param_t value_;
+  std::function<void(conf_param_t const&)> callback_;
 };
 
-template<typename K>
-concept service = std::is_class_v<K> && std::is_trivially_copyable_v<K> && std::is_default_constructible_v<K>;
+template<typename service_t>
+concept service = std::is_class_v<service_t> && std::is_trivially_copyable_v<service_t> && std::is_default_constructible_v<service_t>;
 
 template<service service_t>
 class confman {
@@ -35,4 +35,4 @@ public:
 
   void register_service(service_t to_register) {}
 };
-}
+}  // namespace tfc::confman
