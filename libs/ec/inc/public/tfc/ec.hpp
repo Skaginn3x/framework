7c8aaecebd5a747907d6923fa46f8d1f91012770
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-03-17 14:25:23 +0000

    Ethercat library added as vcpkg basic structure for calling the c library started

--- libs/ec/inc/public/tfc/ec.hpp
+++ libs/ec/inc/public/tfc/ec.hpp
@@ -1,2 +1,29 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
+#include <functional>
+#include <expected>
+
+struct ec_master;
+using ec_master_t = ec_master;
+
+namespace tfc::ec {
+class domain;
+
+class master {
+public:
+  using master_t = std::unique_ptr<ec_master_t, std::function<void(ec_master_t*)>>;
+  static auto request(uint32_t const& master_index) -> void; //std::expected<master, std::error_code>;
+
+  auto create_domain() -> void;
+
+private:
+  explicit master(master_t&& master_created) : master_{std::move(master_created)}{};
+  master_t master_;
+};
+
+class domain {
+
+};
+}  // namespace tfc::ec
---
+++ libs/ec/tests/examples/run_motor.cpp
@@ -0,0 +1,8 @@
+#include "tfc/ec.hpp"
+#include <memory>
+#include <iostream>
+
+
+int main(){
+  tfc::ec::master::request(0);
+}
