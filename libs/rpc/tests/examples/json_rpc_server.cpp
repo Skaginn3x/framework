21b1780341b51345425038fc6eb755b7d08d924d
Jón Bjarni Bjarnason <jon.bjarni@skaginn3x.com>
2023-03-21 15:33:54 +0000

    wip

--- libs/rpc/tests/examples/json_rpc_server.cpp
+++ libs/rpc/tests/examples/json_rpc_server.cpp
@@ -7,6 +7,7 @@
 #include <tfc/rpc.hpp>
 
 namespace asio = boost::asio;
+namespace bpo = boost::program_options;
 
 struct get_config_request {
   std::string executable_name{};
@@ -48,63 +49,91 @@ struct set_config_return {
   };
 };
 
+using server_t =
+    tfc::rpc::server<glz::rpc::server<glz::rpc::server_method_t<"get_config", get_config_request, get_config_return>,
+                                      glz::rpc::server_method_t<"set_config", set_config_request, set_config_return> > >;
+using client_t =
+    tfc::rpc::client<glz::rpc::client<glz::rpc::client_method_t<"get_config", get_config_request, get_config_return>,
+                                      glz::rpc::client_method_t<"set_config", set_config_request, set_config_return> > >;
+
 auto main(int argc, char** argv) -> int {
-  tfc::base::init(argc, argv, tfc::base::default_description());
+  auto desc{ tfc::base::default_description() };
+
+  bool make_client{}, make_server{}, make_faulty_server{};
+
+  desc.add_options()("client", bpo::bool_switch(&make_client)->default_value(false), "Make client.")(
+      "server", bpo::bool_switch(&make_server)->default_value(false), "Make server.")(
+      "faulty-server", bpo::bool_switch(&make_faulty_server)->default_value(false),
+      "Make faulty server. FYI, cannot make both faulty and normal server, faulty server throws(crashes) during any request.");
+  tfc::base::init(argc, argv, desc);
 
   asio::io_context ctx{};
 
-  tfc::rpc::server<glz::rpc::server<glz::rpc::server_method_t<"get_config", get_config_request, get_config_return>,
-                                    glz::rpc::server_method_t<"set_config", set_config_request, set_config_return> > >
-      server(ctx, "/tmp/my_rpc_server");
+  std::shared_ptr<server_t> server{};
 
-  server.converter().on<"get_config">([](get_config_request const& request) -> get_config_return {
-    fmt::print("Got get_config request:\nexe_name:{}\nid:{}\nkey:{}\n\n", request.executable_name, request.executable_id,
-               request.config_key);
-    return get_config_return{ .json = "this is my response" };
-  });
+  if (make_server) {
+    server = std::make_shared<server_t>(ctx, "/tmp/my_rpc_server");
+    server->converter().on<"get_config">([](get_config_request const& request) -> get_config_return {
+      fmt::print("Got get_config request:\nexe_name:{}\nid:{}\nkey:{}\n\n", request.executable_name, request.executable_id,
+                 request.config_key);
+      return get_config_return{ .json = "this is my response" };
+    });
 
-  server.converter().on<"set_config">([](set_config_request const& request) -> set_config_return {
-    fmt::print("Got set_config request:\nexe_name:{}\nid:{}\nkey:{}\nvalue:{}\n\n", request.executable_name,
-               request.executable_id, request.config_key, request.value);
-    return set_config_return{ .succeeded = false, .error = "can't succeed straight away" };
-  });
+    server->converter().on<"set_config">([](set_config_request const& request) -> set_config_return {
+      fmt::print("Got set_config request:\nexe_name:{}\nid:{}\nkey:{}\nvalue:{}\n\n", request.executable_name,
+                 request.executable_id, request.config_key, request.value);
+      return set_config_return{ .succeeded = false, .error = "can't succeed straight away" };
+    });
+  }
+  else if(make_faulty_server) {
+    server = std::make_shared<server_t>(ctx, "/tmp/my_rpc_server");
+    server->converter().on<"get_config">([](get_config_request const& request) -> get_config_return {
+      throw std::runtime_error{"get_config Faulty server"};
+    });
+    server->converter().on<"set_config">([](set_config_request const& request) -> set_config_return {
+      throw std::runtime_error{"set_config Faulty server"};
+    });
+  }
 
-  tfc::rpc::client<glz::rpc::client<glz::rpc::client_method_t<"get_config", get_config_request, get_config_return>,
-                                    glz::rpc::client_method_t<"set_config", set_config_request, set_config_return> > >
-      client(ctx, "/tmp/my_rpc_server");
+  std::shared_ptr<client_t> client{};
 
-  asio::deadline_timer verification_timer{ ctx };
-  verification_timer.expires_from_now(boost::posix_time::milliseconds(1));
-  verification_timer.async_wait([&client](auto) {
-    //
-    for (int i = 0; i < 1; ++i) {
-      client.async_request<"get_config">(
-          get_config_request{ .executable_name = "hello world", .executable_id = "bar", .config_key = "foo" },
-          [](glz::expected<get_config_return, glz::rpc::error> const& response, glz::rpc::jsonrpc_id_type const&) -> void {
+  if (make_client) {
+    client = std::make_shared<client_t>(ctx, "/tmp/my_rpc_server");
+    asio::deadline_timer a_timer{ ctx };
+    a_timer.expires_from_now(boost::posix_time::milliseconds(1));
+    a_timer.async_wait([&client](auto) {
+      //
+      for (int i = 0; i < 1; ++i) {
+        client->async_request<"get_config">(
+            get_config_request{ .executable_name = "hello world", .executable_id = "bar", .config_key = "foo" },
+            [](glz::expected<get_config_return, glz::rpc::error> const& response, glz::rpc::jsonrpc_id_type const&) -> void {
+              if (response.has_value()) {
+                fmt::print("Got response: {}\n", response->json);
+              } else {
+                fmt::print("Got error man: {}\n", response.error().get_message());
+              }
+            });
+      }
+
+      client->async_notify<"set_config">(
+          set_config_request{ .executable_name = "notify1", .executable_id = "notify1", .config_key = "notify1" });
+
+      client->async_request<"set_config">(
+          set_config_request{ .executable_name = "hello world", .executable_id = "bar", .config_key = "foo" },
+          [](glz::expected<set_config_return, glz::rpc::error> const& response, glz::rpc::jsonrpc_id_type const&) -> void {
             if (response.has_value()) {
-              fmt::print("Got response: {}\n", response->json);
+              fmt::print("Got response:{} {}\n", response->error, response->succeeded);
             } else {
-              fmt::print("Got error man: {}\n", response.error().get_message());
+              fmt::print("Got error man:{}\n", response.error().get_message());
             }
           });
-    }
 
-    client.async_notify<"set_config">(
-        set_config_request{ .executable_name = "notify1", .executable_id = "notify1", .config_key = "notify1" });
+      client->async_notify<"set_config">(
+          set_config_request{ .executable_name = "notify2", .executable_id = "notify2", .config_key = "notify2" });
+    });
+  }
 
-    client.async_request<"set_config">(
-        set_config_request{ .executable_name = "hello world", .executable_id = "bar", .config_key = "foo" },
-        [](glz::expected<set_config_return, glz::rpc::error> const& response, glz::rpc::jsonrpc_id_type const&) -> void {
-          if (response.has_value()) {
-            fmt::print("Got response:{} {}\n", response->error, response->succeeded);
-          } else {
-            fmt::print("Got error man:{}\n", response.error().get_message());
-          }
-        });
 
-    client.async_notify<"set_config">(
-        set_config_request{ .executable_name = "notify2", .executable_id = "notify2", .config_key = "notify2" });
-  });
 
   ctx.run();
 
