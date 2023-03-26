f77a9f1d4ecf16f87d2b034450a61cb9737a84a0
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-03-26 20:23:00 +0000

    struct test

--- libs/confman/tests/observer_test.cpp
+++ libs/confman/tests/observer_test.cpp
@@ -57,4 +57,51 @@ auto main(int, char**) -> int {
     the_question = int_observable{ 1337 };
     expect(called);
   };
+
+  "copy assignment struct test"_test = [] {
+    struct foo {
+      int_observable a{ 0 };
+      int non_observable{ 1 };
+      int_observable b{ 2 };
+      int_observable c{ 3 };
+    };
+    bool called_a{};
+    bool called_b{};
+    bool called_c{};
+    foo test_obj{ .a = { 0, [&called_a](int, int) { called_a = true; } },
+                  .non_observable = 32,
+                  .b = { 0, [&called_b](int, int) { called_b = true; } },
+                  .c = { 0, [&called_c](int, int) { called_c = true; } } };
+    expect(!called_a);
+    expect(!called_b);
+    expect(!called_c);
+    foo const default_constructed_foo{};
+    test_obj = default_constructed_foo;  // `a` is same value in test_obj and default constructed
+    expect(!called_a);
+    expect(called_b);
+    expect(called_c);
+  };
+
+  "move assignment struct test"_test = [] {
+    struct foo {
+      int_observable a{ 0 };
+      int non_observable{ 1 };
+      int_observable b{ 2 };
+      int_observable c{ 3 };
+    };
+    bool called_a{};
+    bool called_b{};
+    bool called_c{};
+    foo test_obj{ .a = { 0, [&called_a](int, int) { called_a = true; } },
+                  .non_observable = 32,
+                  .b = { 0, [&called_b](int, int) { called_b = true; } },
+                  .c = { 0, [&called_c](int, int) { called_c = true; } } };
+    expect(!called_a);
+    expect(!called_b);
+    expect(!called_c);
+    test_obj = foo{};  // `a` is same value in test_obj and default constructed
+    expect(!called_a);
+    expect(called_b);
+    expect(called_c);
+  };
 }
