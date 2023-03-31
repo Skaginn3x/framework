6a5ce418032ed1d0315d27e528845c0df25aac82
Jón Bjarni <jbbjarnason@gmail.com>
2023-03-31 17:03:27 +0000

    16 create confman executable (#23)
    * adding exe of confman
    
    * add exit signal handler to progbase
    
    ---------
    
    Co-authored-by: Jón Bjarni Bjarnason <jon.bjarni@skaginn3x.com>

--- exes/confman/src/main.cpp
+++ exes/confman/src/main.cpp
@@ -1,1 +1,29 @@
-auto main() -> int {}
+#include <cstdlib>
+#include <string>
+
+#include <tfc/confman/detail/config_rpc_server.hpp>
+#include <tfc/progbase.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+
+namespace bpo = boost::program_options;
+namespace asio = boost::asio;
+
+auto main(int argc, char** argv) -> int {
+  auto desc{ tfc::base::default_description() };
+  std::string file_name{};
+  desc.add_options()(
+      "file,f", bpo::value<std::string>(&file_name)->default_value(tfc::confman::detail::default_config_filename.data()),
+      "Name of configuration file.");
+  tfc::base::init(argc, argv, desc);
+
+  asio::io_context ctx{};
+
+  tfc::confman::detail::config_rpc_server const services_server{ ctx, file_name };
+
+  asio::co_spawn(ctx, tfc::base::exit_signals(ctx), asio::detached);
+
+  ctx.run();
+  return EXIT_SUCCESS;
+}
