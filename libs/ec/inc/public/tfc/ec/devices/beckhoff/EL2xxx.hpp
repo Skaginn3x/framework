8e4539e7c4021d0418c0ac451be59453bd3b27a4
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-04-14 12:49:16 +0000

    Converted the slots already in ec to utilize a configurable one

--- libs/ec/inc/public/tfc/ec/devices/beckhoff/EL2xxx.hpp
+++ libs/ec/inc/public/tfc/ec/devices/beckhoff/EL2xxx.hpp
@@ -5,19 +5,13 @@ template <size_t size, uint32_t pc>
 class el200x : public base {
 public:
   explicit el200x(boost::asio::io_context& ctx, uint16_t slave_index) : base(slave_index) {
-    // This can be constructed each time the slave goes from pre-op->op
-    // Empty memory s.t. it does not grow the list.
-    bool_receivers_.erase(bool_receivers_.begin(), bool_receivers_.end());
     for (size_t i = 0; i < size; i++) {
       bool_receivers_.emplace_back(
-          tfc::ipc::bool_recv_cb::create(ctx, fmt::format("EL200{}.{}.bool.out.{}", size, slave_index, i)));
-      // TODO: Don't supply ipc signal name. IPC should do this by itself using confman?
-      // As a test now, just connect it with the example signal that the test program creates
-      bool_receivers_.back()->init(
-          fmt::format("{}.{}.EL1008.5.in.{}", tfc::base::get_exe_name(), tfc::base::get_proc_name(), i),
-          std::bind(&el200x::set_output, this, i, std::placeholders::_1));
+          std::make_unique<tfc::ipc::bool_recv_conf_cb>(ctx, fmt::format("EL200{}.{}.bool.out.{}", size, slave_index, i),
+                                                        std::bind_front(&el200x::set_output, this, i)));
     }
   }
+
   static constexpr uint32_t product_code = pc;
   static constexpr uint32_t vendor_id = 0x2;
 
@@ -29,8 +23,9 @@ public:
 
 private:
   auto set_output(size_t position, bool value) -> void { output_states_.set(position, value); }
+
   std::bitset<size> output_states_;
-  std::vector<std::shared_ptr<tfc::ipc::bool_recv_cb>> bool_receivers_;
+  std::vector<std::unique_ptr<tfc::ipc::bool_recv_conf_cb>> bool_receivers_;
 };
 
 using el2008 = el200x<8, 0x7d83052>;
