01de655cf268c84488d39c1bd058ba22297fdcbe
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-04-18 20:58:08 +0000

    wip

--- cmake/toolchains/aarch64-linux-gcc.cmake
+++ cmake/toolchains/aarch64-linux-gcc.cmake
@@ -8,25 +8,21 @@ set(CMAKE_CROSSCOMPILING TRUE)
 
 # Name of C compiler.
 find_program(AARCH64-GCC "aarch64-linux-gnu-gcc")
-if (AARCH64-GCC)
+find_program(GCC-12 "aarch64-linux-gnu-gcc-12")
+if (GCC-12)
+set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-12")
+set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-12")
+else ()
 set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
+endif ()
 
 # Where to look for the target environment. (More paths can be added here)
 set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
 set(CMAKE_INCLUDE_PATH  /usr/include/aarch64-linux-gnu)
 set(CMAKE_LIBRARY_PATH  /usr/lib/aarch64-linux-gnu)
 set(CMAKE_PROGRAM_PATH  /usr/bin/aarch64-linux-gnu)
-else ()
-set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabi-gcc-12")
-set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabi-g++-12")
 
-# Where to look for the target environment. (More paths can be added here)
-set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabi)
-set(CMAKE_INCLUDE_PATH  /usr/include/arm-linux-gnueabi)
-set(CMAKE_LIBRARY_PATH  /usr/lib/arm-linux-gnueabi)
-set(CMAKE_PROGRAM_PATH  /usr/bin/arm-linux-gnueabi)
-endif ()
 # Adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment only.
 #set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
