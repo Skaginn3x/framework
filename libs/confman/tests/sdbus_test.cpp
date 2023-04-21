c40f94368536dfd858041efe5a29b617d22269a4
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-04-21 11:24:38 +0000

    sdbus-cpp in vcpkg building statically linked systemd

---
+++ libs/confman/tests/sdbus_test.cpp
@@ -0,0 +1,38 @@
+#include <filesystem>
+
+#include <boost/asio.hpp>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/program_options.hpp>
+#include <boost/ut.hpp>
+#include <sdbus-c++/sdbus-c++.h>
+
+#include <tfc/confman.hpp>
+#include <tfc/confman/detail/config_rpc_client.hpp>
+#include <tfc/confman/detail/config_rpc_server.hpp>
+#include <tfc/confman/observable.hpp>
+#include <tfc/ipc_connector.hpp>
+#include <tfc/progbase.hpp>
+
+namespace asio = boost::asio;
+namespace ut = boost::ut;
+using ut::operator""_test;
+using ut::operator/;
+
+namespace bpo = boost::program_options;
+
+
+auto main(int argc, char** argv) -> int {
+  auto desc{ tfc::base::default_description() };
+  bool run_slow{};
+  desc.add_options()("slow,s", bpo::bool_switch(&run_slow)->default_value(false));
+  tfc::base::init(argc, argv, desc);
+
+  auto connection = sdbus::createSessionBusConnection();
+
+  [[maybe_unused]] ut::suite const rpc_test_cases = [] {
+    "happy dbus"_test = [] {
+
+    };
+  };
+  return static_cast<int>(boost::ut::cfg<>.run({ .report_errors = true }));
+}
