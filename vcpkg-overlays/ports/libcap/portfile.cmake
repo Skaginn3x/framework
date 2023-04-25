f793b3341a303b4610f2f3a7bed0b666f63ef63c
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-04-25 15:17:57 +0000

    systemd in progress

--- vcpkg-overlays/ports/libcap/portfile.cmake
+++ vcpkg-overlays/ports/libcap/portfile.cmake
@@ -8,53 +8,45 @@ vcpkg_from_git(
 )
 
 # skip_configure is broken https://github.com/microsoft/vcpkg/issues/14389
-#vcpkg_configure_make(
-#  SOURCE_PATH "${SOURCE_PATH}"
-#  SKIP_CONFIGURE
-#)
-foreach(BUILDTYPE "debug" "release")
-  if(BUILDTYPE STREQUAL "debug")
-    set(SHORT_BUILDTYPE "-dbg")
-    set(CMAKE_BUILDTYPE "DEBUG")
-    set(PATH_SUFFIX "/debug")
-  else()
-    if (_VCPKG_NO_DEBUG)
-      set(SHORT_BUILDTYPE "")
-    else()
-      set(SHORT_BUILDTYPE "-rel")
-    endif()
-    set(CMAKE_BUILDTYPE "RELEASE")
-    set(PATH_SUFFIX "")
-  endif()
+# so we use patch to create empty configure executable
 
-  set(WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
-
-  if (NOT EXISTS "${WORKING_DIRECTORY}")
-    file(MAKE_DIRECTORY "${WORKING_DIRECTORY}")
-  endif()
-
-  message(STATUS "Source path: ${SOURCE_PATH}")
-  message(STATUS "Destination: ${WORKING_DIRECTORY}")
-
-  file(INSTALL ${SOURCE_PATH}/ DESTINATION ${WORKING_DIRECTORY})
-endforeach()
-
-vcpkg_build_make(
+vcpkg_configure_make(
+  SOURCE_PATH "${SOURCE_PATH}"
+  COPY_SOURCE
 )
 
-#vcpkg_cmake_configure(
-#    SOURCE_PATH "${SOURCE_PATH}"
-#    OPTIONS
-#    -DBUILD_DOC=OFF
-#    -DBUILD_LIBSYSTEMD=ON
-#)
-#
-#vcpkg_cmake_install()
-#
-#vcpkg_cmake_config_fixup(PACKAGE_NAME sdbus-c++ CONFIG_PATH lib/cmake/sdbus-c++/)
-#vcpkg_fixup_pkgconfig()
-#vcpkg_copy_pdbs()
-#
-#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-#file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/sdbus-cpp RENAME copyright)
+vcpkg_build_make(SUBPATH libcap
+    OPTIONS
+      prefix=${CURRENT_INSTALLED_DIR}
+)
+
+file(INSTALL ${SOURCE_PATH}/libcap/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN "*.h")
+
+set(BUILD_DIR_RELEASE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
+message(STATUS "Custom libcap INSTALL to ${BUILD_DIR_RELEASE}")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib )
+  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib )
+else()
+  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.so DESTINATION ${CURRENT_PACKAGES_DIR}/lib FOLLOW_SYMLINK_CHAIN)
+  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.so DESTINATION ${CURRENT_PACKAGES_DIR}/lib FOLLOW_SYMLINK_CHAIN)
+endif()
+
+set(BUILD_DIR_DEBUG "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
+message(STATUS "Custom libcap INSTALL to ${BUILD_DIR_DEBUG}")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib )
+  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib )
+else()
+  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.so DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib FOLLOW_SYMLINK_CHAIN)
+  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.so DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib FOLLOW_SYMLINK_CHAIN)
+endif()
+
+file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig )
+file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig )
+file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.pc DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig )
+file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.pc DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig )
+
+vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/License)
+
+vcpkg_fixup_pkgconfig()
--- vcpkg-overlays/ports/libsystemd/portfile.cmake
+++ vcpkg-overlays/ports/libsystemd/portfile.cmake
@@ -5,24 +5,26 @@ vcpkg_from_github(
   SHA512 3bbc431a292ab590b70d3b490a528f71d30ccf478ddfa66d1c210f40c260ef49ac30651c19f2d073acf38d68398a4a6fbf95391f0e3ea0333d94b9d4e81d514f
 )
 
+message(WARNING "package dir ${CURRENT_PACKAGES_DIR}")
+
 vcpkg_configure_meson(
   SOURCE_PATH "${SOURCE_PATH}"
-  NO_PKG_CONFIG
   OPTIONS
-#    -Dstatic-libsystemd=pic
-#    -Dselinux=false
+    -Daudit=false
   OPTIONS_DEBUG
     -Drootprefix=${CURRENT_PACKAGES_DIR}/debug
+    -Dpkgconfiglibdir={CURRENT_PACKAGES_DIR}/debug
   OPTIONS_RELEASE
     -Drootprefix=${CURRENT_PACKAGES_DIR}
+    -Dpkgconfiglibdir={CURRENT_PACKAGES_DIR}
 )
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   vcpkg_build_ninja(
-    TARGETS libsystemd.a
+    TARGETS libsystemd.a devel
   )
 else()
   vcpkg_build_ninja(
-    TARGETS libsystemd
+    TARGETS libsystemd devel
   )
 endif()
 
@@ -43,6 +45,9 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
 else()
   file(INSTALL ${BUILD_DIR_DEBUG}/libsystemd.so DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib FOLLOW_SYMLINK_CHAIN)
 endif()
+
+file(INSTALL ${BUILD_DIR_RELEASE}/src/libsystemd/libsystemd.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig )
+file(INSTALL ${BUILD_DIR_DEBUG}/src/libsystemd/libsystemd.pc DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig )
 
 vcpkg_fixup_pkgconfig()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.GPL2" "${SOURCE_PATH}/LICENSE.LGPL2.1")
