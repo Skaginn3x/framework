e8176e3965004f81ffd79beb0354e8a564307d91
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-04-26 12:20:52 +0000

    sdbusplus wip

--- vcpkg-overlays/ports/libcap/portfile.cmake
+++ vcpkg-overlays/ports/libcap/portfile.cmake
@@ -5,9 +5,10 @@ vcpkg_from_git(
   REF 3c7dda330bd9a154bb5b878d31fd591e4951fe17
   PATCHES
     configure.patch
+    cross-compile.patch
 )
 
-# skip_configure is broken https://github.com/microsoft/vcpkg/issues/14389
+# SKIP_CONFIGURE is broken https://github.com/microsoft/vcpkg/issues/14389
 # so we use patch to create empty configure executable
 
 vcpkg_configure_make(
@@ -16,8 +17,30 @@ vcpkg_configure_make(
 )
 
 vcpkg_build_make(SUBPATH libcap
+  BUILD_TARGET cap_names.h
+  OPTIONS
+    prefix=${CURRENT_INSTALLED_DIR}
+    CC=gcc # use host architecture to generate cap_names.h
+)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  vcpkg_build_make(SUBPATH libcap
+    BUILD_TARGET libcap.a
     OPTIONS
       prefix=${CURRENT_INSTALLED_DIR}
+  )
+else()
+  vcpkg_build_make(SUBPATH libcap
+    BUILD_TARGET libcap.so
+    OPTIONS
+      prefix=${CURRENT_INSTALLED_DIR}
+  )
+endif()
+
+vcpkg_build_make(SUBPATH libcap
+  BUILD_TARGET libcap.pc
+  OPTIONS
+    prefix=${CURRENT_INSTALLED_DIR}
 )
 
 file(INSTALL ${SOURCE_PATH}/libcap/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN "*.h")
@@ -26,26 +49,20 @@ set(BUILD_DIR_RELEASE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
 message(STATUS "Custom libcap INSTALL to ${BUILD_DIR_RELEASE}")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib )
-  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib )
 else()
   file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.so DESTINATION ${CURRENT_PACKAGES_DIR}/lib FOLLOW_SYMLINK_CHAIN)
-  file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.so DESTINATION ${CURRENT_PACKAGES_DIR}/lib FOLLOW_SYMLINK_CHAIN)
 endif()
 
 set(BUILD_DIR_DEBUG "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
 message(STATUS "Custom libcap INSTALL to ${BUILD_DIR_DEBUG}")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib )
-  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.a DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib )
 else()
   file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.so DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib FOLLOW_SYMLINK_CHAIN)
-  file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.so DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib FOLLOW_SYMLINK_CHAIN)
 endif()
 
 file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libcap.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig )
-file(INSTALL ${BUILD_DIR_RELEASE}/libcap/libpsx.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig )
 file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libcap.pc DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig )
-file(INSTALL ${BUILD_DIR_DEBUG}/libcap/libpsx.pc DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig )
 
 vcpkg_install_copyright(FILE_LIST ${SOURCE_PATH}/License)
 
