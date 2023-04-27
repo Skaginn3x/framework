cf8c63231aa157d73cfc63e94dee6b41cb1f0e67
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-04-27 15:00:38 +0000

    sdbus wip

--- libs/operation_mode/inc/public/tfc/operation_mode.hpp
+++ libs/operation_mode/inc/public/tfc/operation_mode.hpp
@@ -9,6 +9,9 @@
 
 #include <tfc/logger.hpp>
 
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
 namespace tfc::operation {
 
 enum struct mode_e : std::uint8_t {
@@ -20,6 +23,7 @@ enum struct mode_e : std::uint8_t {
   emergency = 5,
   maintenance = 6,
 };
+
 namespace concepts {
 using new_mode_e = mode_e;
 using old_mode_e = mode_e;
@@ -28,8 +32,8 @@ concept transition_callback = std::invocable<callback_t, new_mode_e, old_mode_e>
 }  // namespace concepts
 
 class interface {
-  interface() : logger_("operation") {}
-  explicit interface(std::string_view log_key) : logger_(log_key) {}
+  interface();
+  explicit interface(std::string_view log_key);
 
   /// \brief set operation mode controller to new state
   /// \note take care since this will affect the whole system
@@ -39,19 +43,19 @@ class interface {
   using new_mode_e = mode_e;
   using old_mode_e = mode_e;
 
-  std::error_code remove_callback(uuid_t uuid) {
-    auto number_of_erased_items{ std::erase_if(callbacks, [uuid](auto const& item) -> bool { return item.uuid == uuid; }) };
+  auto remove_callback(uuid_t uuid) -> std::error_code {
+    auto number_of_erased_items{ std::erase_if(callbacks_, [uuid](auto const& item) -> bool { return item.uuid == uuid; }) };
     if (number_of_erased_items == 0) {
       return std::make_error_code(std::errc::argument_out_of_domain);
     }
     return {};
   }
 
-  uuid_t on_enter(mode_e mode, concepts::transition_callback auto&& callback) {
+  auto on_enter(mode_e mode, concepts::transition_callback auto&& callback) -> uuid_t {
     return append_callback(mode, transition_e::enter, std::forward<decltype(callback)>(callback));
   }
 
-  uuid_t on_leave(mode_e mode, concepts::transition_callback auto&& callback) {
+  auto on_leave(mode_e mode, concepts::transition_callback auto&& callback) -> uuid_t {
     return append_callback(mode, transition_e::leave, std::forward<decltype(callback)>(callback));
   }
 
@@ -79,9 +83,9 @@ private:
     uuid_t uuid{};
   };
 
-  uuid_t append_callback(mode_e mode_value, transition_e transition, concepts::transition_callback auto&& callback) {
-    uuid_t uuid{ next_uuid++ };
-    callbacks.emplace_back(callback_item{ .mode = mode_value,
+  auto append_callback(mode_e mode_value, transition_e transition, concepts::transition_callback auto&& callback) -> uuid_t {
+    uuid_t const uuid{ next_uuid_++ };
+    callbacks_.emplace_back(callback_item{ .mode = mode_value,
                                           .transition = transition,
                                           .callback = std::forward<decltype(callback)>(callback),
                                           .uuid = uuid });
@@ -101,8 +105,9 @@ private:
     };
   }
 
-  std::atomic<uuid_t> next_uuid{};
-  std::vector<callback_item> callbacks{};
+  std::atomic<uuid_t> next_uuid_{};
+  std::vector<callback_item> callbacks_{};
+  std::shared_ptr<sdbusplus::SdBusInterface> dbus_interface_{};
   tfc::logger::logger logger_;
 };
 
--- libs/operation_mode/src/operation_mode.cpp
+++ libs/operation_mode/src/operation_mode.cpp
@@ -1,6 +1,11 @@
 #include <tfc/operation_mode.hpp>
 
+#include <sdbusplus/sdbus.hpp>
+
 namespace tfc::operation {
+
+interface::interface(): logger_("operation") {}
+interface::interface(std::string_view log_key): logger_(log_key) {}
 
 void interface::set(tfc::operation::mode_e) const {
 
