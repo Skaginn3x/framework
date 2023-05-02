5cc135e8275d26e7608eaebed56788f3f20a71e9
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-05-02 16:20:35 +0000

    why am I doing this

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,13 @@
 #include <connection.hpp>
 #include <boost/asio.hpp>
+#include <sdbusplus/asio/connection.hpp>
 
 auto main(int, char**) -> int {
   boost::asio::io_context ctx{};
 
-  [[maybe_unused]] connection_test testme{ctx};
+//  [[maybe_unused]] connection_test testme{ctx};
+  [[maybe_unused]] sdbusplus::asio::connection foo(ctx);
+  [[maybe_unused]] sdbusplus::asio::connection foo1(ctx);
 
   ctx.run();
   return EXIT_SUCCESS;
