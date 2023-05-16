787431ed37ee23ec1af7f2453f22824664452ef2
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-05-16 10:27:38 +0000

    CMAKE generator expressions broke documentation

--- cmake/tfc_docs.cmake
+++ cmake/tfc_docs.cmake
@@ -2,8 +2,8 @@
 set(TFC_FRAMEWORK_PUBLIC_HEADER_DIRS "" CACHE INTERNAL "")
 macro(add_library_to_docs target_library)
   # Find public include directories and add to list
-  get_target_property(TEMPORARY_HEADER_DIR ${target_library} INTERFACE_INCLUDE_DIRECTORIES)
-  list(APPEND TFC_FRAMEWORK_PUBLIC_HEADER_DIRS ${TEMPORARY_HEADER_DIR})
+  get_target_property(TEMPORARY_SOURCE_DIR ${target_library} SOURCE_DIR)
+  list(APPEND TFC_FRAMEWORK_PUBLIC_HEADER_DIRS ${TEMPORARY_SOURCE_DIR}/inc)
   set(TFC_FRAMEWORK_PUBLIC_HEADER_DIRS ${TFC_FRAMEWORK_PUBLIC_HEADER_DIRS} CACHE INTERNAL "")
 endmacro()
 
