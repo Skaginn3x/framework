455dcaf1c1007a4377f35288dc2c43a13163ce8a
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-05-23 14:07:27 +0000

    confman stub

---
+++ libs/confman/stubs/inc/public/tfc/stubs/confman.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <filesystem>
+#include <system_error>
+#include <string_view>
+
+#include <boost/asio/io_context.hpp>
+
+#include <tfc/confman/detail/change.hpp>
+
+namespace tfc::stubs::confman {
+
+namespace asio = boost::asio;
+
+template <typename config_storage_t>
+class config {
+public:
+  using type = config_storage_t;
+  using storage_t = config_storage_t;
+
+  config(asio::io_context& ctx, std::string_view key) : config{ ctx, key, config_storage_t{} } {}
+
+  template <typename storage_type>
+    requires std::same_as<storage_t, std::remove_cvref_t<storage_type>>
+  config(asio::io_context&, std::string_view, storage_type&& def) : storage_{ std::forward<storage_type>(def) } {}
+
+  [[nodiscard]] auto value() const noexcept -> storage_t const& { return storage_.value(); }
+  auto operator->() const noexcept -> storage_t const* { return std::addressof(value()); }
+
+  [[nodiscard]] auto string() const -> std::string { return {}; }
+  [[nodiscard]] auto schema() const -> std::string { return {}; }
+
+  auto set_changed() const noexcept -> std::error_code {
+    std::invoke(set_changed_cb);
+    return {};
+  }
+
+  /// \brief get config key used to index the given object of type storage_t
+  [[nodiscard]] auto file() const noexcept -> std::filesystem::path const& {
+    static std::filesystem::path unused{};
+    return unused;
+  }
+
+  using change = tfc::confman::detail::change<config>;
+
+  auto make_change() noexcept -> change { return change{ *this }; }
+
+  auto from_string(std::string_view value) -> std::error_code { return {}; }
+
+  storage_t storage_{};
+  std::function<void(void)> set_changed_cb{ []() {} };
+};
+
+}
---
+++ libs/confman/stubs/inc/public/tfc/stubs/confman/file_storage.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <filesystem>
+#include <system_error>
+
+#include <boost/asio/io_context.hpp>
+
+#include <tfc/confman/detail/change.hpp>
+
+namespace tfc::stubs::confman {
+
+namespace asio = boost::asio;
+
+template <typename storage_t>
+class file_storage {
+public:
+  using type = storage_t;
+
+  file_storage(asio::io_context& ctx, std::filesystem::path const& file_path)
+      : file_storage{ ctx, file_path, storage_t{} } {}
+
+  file_storage(asio::io_context&, std::filesystem::path const&, auto&& default_value)
+      : storage_{ std::forward<decltype(default_value)>(default_value) } {}
+
+  auto error() const noexcept -> std::error_code const& {
+    static std::error_code unused{};
+    return unused;
+  }
+
+  auto file() const noexcept -> std::filesystem::path const& {
+    static std::filesystem::path unused{};
+    return unused;
+  }
+
+  /// \return Access to underlying value
+  auto value() const noexcept -> storage_t const& { return storage_; }
+
+  /// \return Access to underlying value
+  auto operator->() const noexcept -> storage_t const* { return std::addressof(value()); }
+
+  auto on_change(std::invocable auto&&) -> void { }
+
+  using change = tfc::confman::detail::change<file_storage>;
+
+  auto make_change() -> change { return change{ *this }; }
+
+  auto set_changed() const noexcept -> std::error_code {
+    std::invoke(set_changed_cb);
+    return {};
+  }
+
+  storage_t storage_{};
+  std::function<void(void)> set_changed_cb{ []() {} };
+};
+
+}
