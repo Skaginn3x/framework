02e2ddff7677f055ea58dd23c72009a34213706b
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-05-24 14:21:27 +0000

    build exes cmake option

--- cmake/tfc_options.cmake
+++ cmake/tfc_options.cmake
@@ -13,6 +13,9 @@ add_feature_info("TFC_DBUS_ORGANIZATION" TFC_DBUS_ORGANIZATION "D-Bus organizati
 option(BUILD_DOCS "Indicates whether documentation should be built." OFF)
 add_feature_info("BUILD_DOCS" BUILD_DOCS "Indicates whether documentation should be built.")
 
+option(BUILD_EXES "Indicates whether executables should be built." ON)
+add_feature_info("BUILD_EXES" BUILD_EXES "Indicates whether executables should be built.")
+
 option(ENABLE_CODE_COVERAGE_INSTRUMENTATION "Enable code instrumentation" OFF)
 add_feature_info("ENABLE_CODE_COVERAGE_INSTRUMENTATION" ENABLE_CODE_COVERAGE_INSTRUMENTATION
     "Enable code instrumentation to allow generating code coverage after running tests")
