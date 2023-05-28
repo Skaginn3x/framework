e2785028f61f7f655c1e379a927cd1d486d28c06
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-05-28 09:45:52 +0000

    testing wip confman

---
+++ libs/confman/testing/stubs/example/main.cpp
@@ -0,0 +1,18 @@
+#include <boost/asio.hpp>
+
+#include <tfc/progbase.hpp>
+#include <tfc/stubs/confman/file_storage.hpp>
+
+namespace asio = boost::asio;
+
+auto main(int argc, char** argv) -> int {
+  tfc::base::init(argc, argv);
+
+  asio::io_context ctx{};
+
+  tfc::stubs::confman::file_storage<int>{ ctx, "file_path" };
+
+  // inject to use case
+
+  return EXIT_SUCCESS;
+}
---
+++ libs/confman/testing/stubs/inc/public/tfc/stubs/confman/file_storage.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <filesystem>
+#include <system_error>
+
+#include <boost/asio/io_context.hpp>
+
+#include <tfc/confman/file_storage.hpp>
+
+namespace tfc::stubs::confman {
+
+namespace asio = boost::asio;
+
+/// \brief file_storage stub class removing every file system operations
+template <typename storage_t>
+class file_storage : public tfc::confman::file_storage<storage_t> {
+public:
+  using type = storage_t;
+
+  file_storage(asio::io_context& ctx, std::filesystem::path const& file_path)
+      : file_storage{ ctx, file_path, storage_t{} } {}
+
+  file_storage(asio::io_context& ctx, std::filesystem::path const&, auto&& default_value)
+      : tfc::confman::file_storage<storage_t>{ ctx } {
+    this->storage_ = std::forward<decltype(default_value)>(default_value);
+  }
+
+  auto value() const noexcept -> storage_t const& { return this->storage_; }
+  auto access() noexcept -> storage_t& { return this->storage_; }
+
+  auto operator->() const noexcept -> storage_t const* { return std::addressof(value()); }
+
+  using change = tfc::confman::detail::change<file_storage>;
+
+  auto make_change() -> change { return change{ *this }; }
+
+  auto set_changed() const noexcept -> std::error_code {
+    std::invoke(this->cb_);
+    return {};
+  }
+};
+
+}  // namespace tfc::stubs::confman
