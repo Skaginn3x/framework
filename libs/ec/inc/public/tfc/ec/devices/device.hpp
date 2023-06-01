8f4612e01b3f67aa72468cb7814ef7c8712566d2
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-06-01 13:40:26 +0000

    cleanup

--- libs/ec/inc/public/tfc/ec/devices/device.hpp
+++ libs/ec/inc/public/tfc/ec/devices/device.hpp
@@ -31,15 +31,6 @@ using devices_list = devices_type<
 >;
 // clang-format on
 
-using abt::easyecat;
-using beckhoff::ek1100;
-using beckhoff::el1008;
-using beckhoff::el2008;
-using beckhoff::el3054;
-using beckhoff::el4002;
-using schneider::atv320;
-using schneider::lxm32m;
-
 // todo appendable type list
 // https://b.atch.se/posts/constexpr-meta-container/
 // https://mc-deltat.github.io/articles/stateful-metaprogramming-cpp20
@@ -82,7 +73,7 @@ auto get_impl(boost::asio::io_context& ctx,
               devices_type<devices_t...>) -> std::unique_ptr<base> {
   std::unique_ptr<base> result{};
   (get_impl<manager_client_type, devices_t>(ctx, client, slave_index, vendor_id, product_code, result) || ...);
-  return result;
+  return result ? std::move(result) : std::make_unique<default_device>(slave_index);
 }
 
 template <typename manager_client_type>
@@ -92,23 +83,5 @@ auto get(boost::asio::io_context& ctx,
          auto vendor_id,
          auto product_code) -> std::unique_ptr<base> {
   return get_impl(ctx, client, slave_index, vendor_id, product_code, devices_list<manager_client_type>{});
-  //
-  //  if (devices_equal<atv320<manager_client_type>>(vendor_id, product_code))
-  //    return std::make_unique<atv320<manager_client_type>>(ctx, client, slave_index);
-  //  if (devices_equal<lxm32m<manager_client_type>>(vendor_id, product_code))
-  //    return std::make_unique<lxm32m<manager_client_type>>(ctx, client, slave_index);
-  //  if (devices_equal<easyecat<manager_client_type>>(vendor_id, product_code))
-  //    return std::make_unique<easyecat<manager_client_type>>(ctx, client, slave_index);
-  //  if (devices_equal<ek1100>(vendor_id, product_code))
-  //    return std::make_unique<ek1100>(slave_index);
-  //  if (devices_equal<el1008<manager_client_type>>(vendor_id, product_code))
-  //    return std::make_unique<el1008<manager_client_type>>(ctx, client, slave_index);
-  //  if (devices_equal<el2008<manager_client_type>>(vendor_id, product_code))
-  //    return std::make_unique<el2008<manager_client_type>>(ctx, client, slave_index);
-  //  if (devices_equal<el3054>(vendor_id, product_code))
-  //    return std::make_unique<el3054>(ctx, slave_index);
-  //  if (devices_equal<el4002>(vendor_id, product_code))
-  //    return std::make_unique<el4002>(ctx, slave_index);
-  //  return std::make_unique<default_device>(slave_index);
 }
 }  // namespace tfc::ec::devices
