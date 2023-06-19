6cb2d05d40de0513703d9bacc072fef2754cbf71
magni <magnimar@skaginn3x.com>
2023-06-19 11:46:48 +0000

    working on mqtt broadcast

--- exes/mqtt-broadcaster/src/main.cpp
+++ exes/mqtt-broadcaster/src/main.cpp
@@ -1,16 +1,11 @@
+#include <async_mqtt/all.hpp>
 #include <boost/asio.hpp>
+#include <iostream>
+#include <string>
 #include <tfc/confman/file_storage.hpp>
 #include <tfc/ipc.hpp>
 #include <tfc/ipc/details/dbus_server_iface.hpp>
 #include <tfc/progbase.hpp>
-//#include <mqtt_client_cpp.hpp>
-
-#include <iostream>
-#include <string>
-
-#include <boost/asio.hpp>
-
-#include <async_mqtt/all.hpp>
 
 namespace as = boost::asio;
 namespace am = async_mqtt;
@@ -18,7 +13,6 @@ namespace am = async_mqtt;
 std::vector<std::string> banned_strings = { "mainn", "ec_run", "operation-mode", "uint64_t" };
 
 auto take_out_bad_strings(std::vector<std::string> signals) -> std::vector<std::string> {
-
   for (auto it = signals.begin(); it != signals.end();) {
     bool found = false;
     for (auto const& banned_string : banned_strings) {
@@ -37,93 +31,120 @@ auto take_out_bad_strings(std::vector<std::string> signals) -> std::vector<std::
   return signals;
 }
 
-// mqtt://mqtt.eclipse.org:1883
-auto handle_message(std::string signal, auto const& val) -> void {
-  std::cout << "signal: " << signal << " val: " << val << std::endl;
+auto send_message(std::string const& slot_name,
+                  auto const& val,
+                  [[maybe_unused]] as::io_context& ctx,
+                  am::endpoint<am::role::client, am::protocol::mqtt>& amep) -> void {
+  std::ostringstream oss;
+  oss << val;
+  std::string payload = "Slot: " + slot_name + ", Value: " + oss.str();
 
-  am::endpoint<am::role::client, am::protocol::ws> amep {
-    am::protocol_version::v3_1_1,
-    ioc.get_executor()
-  };
+  std::cout << payload << "\n";
 
-  as::ip::tcp::socket resolve_sock{ioc};
-  as::ip::tcp::resolver res{resolve_sock.get_executor()};
+  amep.send(
+      am::v3_1_1::publish_packet{ *amep.acquire_unique_packet_id(), am::allocate_buffer("topic1"),
+                                  am::allocate_buffer(payload), am::qos::at_least_once },
+      [&amep, payload]([[maybe_unused]] am::system_error const& se) { std::cout << "Sent message: " << payload << "\n"; });
 
-  yield amep.send(
-      am::v3_1_1::publish_packet{
-          *amep.acquire_unique_packet_id(),
-          am::allocate_buffer("topic1"),
-          am::allocate_buffer("payload1"),
-          am::qos::at_least_once
-      },
-      *this
-  );
-
-  auto c = MQTT_NS::make_async_client(ioc, argv[1], argv[2]);
-  c->set_client_id("cid1");
-  c->set_clean_session(true);
+  //  amep.send(
+  //      am::v3_1_1::publish_packet{ *amep.acquire_unique_packet_id(), am::allocate_buffer("topic1"),
+  //                                  am::allocate_buffer(payload), am::qos::at_least_once },
+  //      [&amep, payload]([[maybe_unused]] am::system_error const& se) { std::cout << "Sent message: " << payload << "\n";
+  //      });
 }
-
-// this executable is supposed to connect to ipc-ruler, read a list of signals, make slots with callbacks for those signals
-// and then broadcast them to mqtt
 auto main(int argc, char** argv) -> int {
   tfc::base::init(argc, argv);
 
   boost::asio::io_context ctx;
 
-  tfc::ipc_ruler::ipc_manager_client ipc_client(ctx);
+  // Initialize the MQTT client endpoint
+  am::setup_log(am::severity_level::trace);
+  as::ip::tcp::socket resolve_sock{ ctx };
+  as::ip::tcp::resolver res{ resolve_sock.get_executor() };
+  am::endpoint<am::role::client, am::protocol::mqtt> amep{ am::protocol_version::v3_1_1, ctx.get_executor() };
 
-  std::vector<std::string> signals_on_client;
+  // Resolve the MQTT broker hostname
+  res.async_resolve(
+      "localhost", "1883", [&]([[maybe_unused]] boost::system::error_code ec, as::ip::tcp::resolver::results_type eps) {
+        // Underlying TCP connect
+        as::async_connect(amep.next_layer(), eps,
+                          [&]([[maybe_unused]] boost::system::error_code ec, as::ip::tcp::endpoint /*unused*/) {
+                            // Send MQTT CONNECT
+                            amep.send(
+                                am::v3_1_1::connect_packet{
+                                    true,         // clean_session
+                                    0x1234,       // keep_alive
+                                    am::allocate_buffer("cid1"),
+                                    am::nullopt,  // will
+                                    am::nullopt,  // username set like am::allocate_buffer("user1"),
+                                    am::nullopt   // password set like am::allocate_buffer("pass1")
+                                },
+                                [&]([[maybe_unused]] am::system_error const& se) {
+                                  amep.recv([&](am::packet_variant pv) {
+                                    pv.visit(am::overload{ [&]([[maybe_unused]] am::v3_1_1::connack_packet const& p) {
+                                                            // Do nothing here or do some initial setup if needed
+                                                          },
+                                                           [](auto const&) {} });
+                                  });
+                                });
+                          });
+      });
 
-  ipc_client.signals([&](const std::vector<tfc::ipc_ruler::signal>& signals) {
-    for (auto const& signal : signals) {
-      signals_on_client.push_back(signal.name);
-    }
-  });
+  //tfc::ipc_ruler::ipc_manager_client ipc_client(ctx);
 
-  ctx.run_for(std::chrono::seconds(1));
+  //std::vector<std::string> signals_on_client;
 
-  // take out all strings that are banned
-  signals_on_client = take_out_bad_strings(signals_on_client);
+  //ipc_client.signals([&](const std::vector<tfc::ipc_ruler::signal>& signals) {
+  //  for (auto const& signal : signals) {
+  //    signals_on_client.push_back(signal.name);
+  //  }
+  //});
 
-  for (auto const& signal : signals_on_client) {
-    std::cout << "name: " << signal << std::endl;
-  }
+  //ctx.run_for(std::chrono::seconds(1));
 
-  std::vector<tfc::ipc::details::any_recv_cb> connect_slots;
+  //// take out all strings that are banned
+  //signals_on_client = take_out_bad_strings(signals_on_client);
 
-  for (auto& signal_connect : signals_on_client) {
+  //for (auto const& signal : signals_on_client) {
+  //  std::cout << "name: " << signal << std::endl;
+  //}
 
-    connect_slots.emplace_back([&ctx](std::string_view sig) -> tfc::ipc::details::any_recv_cb {
+  //std::vector<tfc::ipc::details::any_recv_cb> connect_slots;
 
-      std::string slot_name = fmt::format("tfcctl_slot_{}", sig);
+  // timer for 3 seconds
+  as::steady_timer timer(ctx);
 
-      auto ipc{ tfc::ipc::details::create_ipc_recv_cb<tfc::ipc::details::any_recv_cb>(ctx, slot_name) };
-      std::visit(
+  timer.async_wait(
+      [&](auto const& ) { send_message("timer", "timer", ctx, amep); });
 
-          [&](auto&& receiver) {
+  timer.expires_from_now(std::chrono::seconds(3));
 
-            using receiver_t = std::remove_cvref_t<decltype(receiver)>;
+  //for (auto& signal_connect : signals_on_client) {
+  //  connect_slots.emplace_back([&ctx, &amep](std::string_view sig) -> tfc::ipc::details::any_recv_cb {
+  //    std::string slot_name = fmt::format("tfcctl_slot_{}", sig);
 
-            if constexpr (!std::same_as<std::monostate, receiver_t>) {
+  //    std::cout << "slot name: " << slot_name << std::endl;
 
-              auto error = receiver->init(
+  //    auto ipc{ tfc::ipc::details::create_ipc_recv_cb<tfc::ipc::details::any_recv_cb>(ctx, slot_name) };
+  //    std::visit(
 
-                  sig, [&, sig, slot_name](auto const& val) {
-                    handle_message(slot_name, val);
-                  });
+  //        [&](auto&& receiver) {
+  //          using receiver_t = std::remove_cvref_t<decltype(receiver)>;
 
-              if (error) {
-                std::cout << "Failed to connect: " << error.message() << std::endl;
-              }
-            }
-          },
-          ipc);
-      return ipc;
-    }(signal_connect));
-  }
+  //          if constexpr (!std::same_as<std::monostate, receiver_t>) {
+  //            auto error = receiver->init(
+
+  //                sig, [&, sig, slot_name](auto const& val) { send_message(slot_name, val, ctx, amep); });
+
+  //            if (error) {
+  //              std::cout << "Failed to connect: " << error.message() << std::endl;
+  //            }
+  //          }
+  //        },
+  //        ipc);
+  //    return ipc;
+  //  }(signal_connect));
+  //}
 
   ctx.run();
-
-  return 0;
 }
