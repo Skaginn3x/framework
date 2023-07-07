8b21172f449cab47e79d159e99c52bc4bf8c1e21
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-07-07 09:28:57 +0000

    add protobuf for sparkplug_b

---
+++ libs/mqtt-sparkplug/example/mqtt-sparkplug-example.cpp
@@ -0,0 +1,15 @@
+
+// specification https://sparkplug.eclipse.org/specification/version/3.0/documents/sparkplug-specification-3.0.0.pdf
+
+#include <sparkplug_b/sparkplug_b.pb.h>
+
+using org::eclipse::tahu::protobuf::Payload_Metric;
+
+int main() {
+  [[maybe_unused]] Payload_Metric const metric{};
+
+  // todo
+
+  return 0;
+}
+
