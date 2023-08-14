f01dc60627a38ac798bd18397d78cb454ecf7b5f
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-08-14 16:09:57 +0000

    Clang format

--- exes/ec/inc/public/tfc/ec/devices/beckhoff/EL2xxx.hpp
+++ exes/ec/inc/public/tfc/ec/devices/beckhoff/EL2xxx.hpp
@@ -6,9 +6,8 @@ class el2xxx : public base {
 public:
   explicit el2xxx(boost::asio::io_context& ctx, manager_client_type& client, uint16_t slave_index) : base(slave_index) {
     for (size_t i = 0; i < size; i++) {
-      bool_receivers_.emplace_back(
-          std::make_unique<tfc::ipc::bool_slot>(ctx, client, fmt::format("{}.{}.out.{}", name, slave_index, i),
-                                                std::bind_front(&el2xxx::set_output, this, i)));
+      bool_receivers_.emplace_back(std::make_unique<tfc::ipc::bool_slot>(
+          ctx, client, fmt::format("{}.{}.out.{}", name, slave_index, i), std::bind_front(&el2xxx::set_output, this, i)));
     }
   }
 
@@ -18,7 +17,7 @@ public:
   void process_data(std::span<std::byte>, std::span<std::byte> output) noexcept final {
     static_assert(size == 8 || size == 4 || size == 16, "Invalid size");
     output[0] = static_cast<std::byte>(output_states_.to_ulong() & 0xff);
-    if constexpr (size > 8){
+    if constexpr (size > 8) {
       output[1] = static_cast<std::byte>(output_states_.to_ulong() >> 8);
     }
   }
@@ -32,7 +31,6 @@ private:
 
 using std::string_view_literals::operator""sv;
 
-
 constexpr auto el2008_name = "EL2008"sv;
 template <typename manager_client_type>
 using el2008 = el2xxx<manager_client_type, 8, 0x7d83052, el2008_name>;
@@ -45,10 +43,9 @@ constexpr auto el2809_name = "EL2809"sv;
 template <typename manager_client_type>
 using el2809 = el2xxx<manager_client_type, 16, 0xaf93052, el2809_name>;
 
-
-  // void process_data(std::span<std::byte>, std::span<std::byte> output) noexcept final {
-  //   output[0] = static_cast<std::byte>(output_states_.to_ulong());
-  //   output[1] = static_cast<std::byte>(output_states_.to_ulong() >> 8);
-  // }
+// void process_data(std::span<std::byte>, std::span<std::byte> output) noexcept final {
+//   output[0] = static_cast<std::byte>(output_states_.to_ulong());
+//   output[1] = static_cast<std::byte>(output_states_.to_ulong() >> 8);
+// }
 
 }  // namespace tfc::ec::devices::beckhoff
