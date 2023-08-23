2cac2a5e740333c1b94cacc444dd0b3e1c183bea
magni <magnimar@skaginn3x.com>
2023-08-23 13:03:03 +0000

    adding comments

--- exes/mqtt-broadcaster/inc/config.hpp
+++ exes/mqtt-broadcaster/inc/config.hpp
@@ -3,8 +3,7 @@
 #include <tfc/confman.hpp>
 #include <tfc/confman/observable.hpp>
 
-// File under /etc/tfc/mqtt-broadcaster/def/mqtt_broadcaster.json which lists the signals that are supposed to be published
-// on the mqtt broker. Updating the signals in this file will cause the running program to stop and restart
+// File under /etc/tfc/mqtt-broadcaster/def/mqtt_broadcaster.json which specifies the connection values for the MQTT broker.
 struct config {
   std::string address{};
   std::string port{};
@@ -17,13 +16,13 @@ struct config {
   struct glaze {
     // clang-format off
     static constexpr auto value{ glz::object(
-        "address", &config::address,
-        "port", &config::port,
-        "username", &config::username,
-        "password", &config::password,
-        "node_id", &config::node_id,
-        "group_id", &config::group_id,
-        "client_id", &config::client_id
+        "address", &config::address, "Hostname or IP address of the MQTT broker",
+        "port", &config::port, "Port or service name of the MQTT broker",
+        "username", &config::username, "Username for the MQTT broker",
+        "password", &config::password, "Password for the MQTT broker",
+        "node_id", &config::node_id, "Spark Plug B Node ID, used to identify which node is sending information",
+        "group_id", &config::group_id, "Spark Plug B Group ID, used to identify which group the node belongs to",
+        "client_id", &config::client_id, "Spark Plug B Client ID, used to identify which client is sending information"
       ) };
     // clang-format on
     static constexpr auto name{ "mqtt_broadcaster" };
