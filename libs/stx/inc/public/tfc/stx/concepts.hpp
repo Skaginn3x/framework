fac499e2df3bb5e620918c947052fb31ddfc3a65
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-08-29 08:26:20 +0000

    add useful specialization template test

--- libs/stx/inc/public/tfc/stx/concepts.hpp
+++ libs/stx/inc/public/tfc/stx/concepts.hpp
@@ -10,4 +10,12 @@ concept nothrow_invocable = requires { std::is_nothrow_invocable_v<func, args...
 template <typename enum_t>
 concept is_enum = requires { std::is_enum_v<enum_t>; };
 
+// from
+// https://stackoverflow.com/questions/16337610/how-to-know-if-a-type-is-a-specialization-of-stdvector
+template <class, template <class...> class>
+inline constexpr bool is_specialization_v = false;
+
+template <template <class...> class type, class... args>
+inline constexpr bool is_specialization_v<type<args...>, type> = true;
+
 }  // namespace tfc::stx
