37113fe90dc018c4b2c645f7169215aaa8d151bc
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-09-12 18:19:24 +0000

    add exception to dbus naming

--- libs/dbus_util/inc/public/tfc/dbus/string_maker.hpp
+++ libs/dbus_util/inc/public/tfc/dbus/string_maker.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <string_view>
 
+#include <tfc/dbus/exception.hpp>
 #include <tfc/configure_options.hpp>
 #include <tfc/stx/string_view_join.hpp>
 
@@ -50,9 +51,15 @@ static auto constexpr const_dbus_path{ stx::string_view_join_v<detail::dbus_path
 auto constexpr make_dbus_name(std::string_view input_name) -> std::string {
   auto temporary{ detail::make<detail::dbus_name_prefix>(input_name) };
   std::ranges::replace(temporary, '/', '.');
-  std::ranges::replace(temporary, '-', '_');
-  static constexpr std::string_view change_dots_preceding_number_to{ "(\\.)+(?=\\d)" };
-  return std::regex_replace(temporary, std::regex{ change_dots_preceding_number_to.data() }, "_");
+  if (temporary.contains('-')) {
+    throw exception::invalid_name{ "{} contains illegal dbus character '-'", input_name };
+  }
+  std::smatch match_results{};
+  static constexpr std::string_view match_dots_preceding_number{ "(\\.)+(?=\\d)" };
+  if (std::regex_match(temporary, match_results, std::regex{ match_dots_preceding_number.data() })) {
+    throw exception::invalid_name{ "{} contains illegal dbus character '-'", input_name };
+  }
+  return temporary;
 }
 
 /// \brief make dbus path like /org/freedesktop/<service_name>
