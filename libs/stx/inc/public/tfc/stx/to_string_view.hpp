59c3539154aea95aa58cc679614ebdf0654f7c5a
Ómar Högni Guðmarsson <70097935+omarhogni@users.noreply.github.com>
2023-09-14 17:19:43 +0000

    Atv settings (#138)
    * Modified settings atv
    * make string_view from unsigned integral in hex format
    * ethercat setting is making automatic description on compile time
    
    ---------
    
    Co-authored-by: Jón Bjarni Bjarnason <jbbjarnason@gmail.com>

--- libs/stx/inc/public/tfc/stx/to_string_view.hpp
+++ libs/stx/inc/public/tfc/stx/to_string_view.hpp
@@ -3,10 +3,13 @@
 #include <concepts>
 #include <limits>
 #include <string_view>
+#include <utility>
 
 namespace tfc::stx {
 
-template <std::unsigned_integral auto integral>
+enum struct base_e : std::uint8_t { decimal = 10, hex = 16 };
+
+template <std::unsigned_integral auto integral, base_e base = base_e::decimal>
 struct to_string_view {
   template <std::size_t size>
   struct impl_out {
@@ -16,14 +19,22 @@ struct to_string_view {
   // Join all strings into a single std::array of chars
   static constexpr auto impl() noexcept {
     using integral_t = decltype(integral);
-    // digits10 is count from zero
-    // and the second plus 1 is zero termination, meaning if you would use the outputted data() on the
+    constexpr auto digits{ []() {
+      if constexpr (base == base_e::decimal) {
+        // digits10 is count from zero
+        return std::numeric_limits<integral_t>::digits10 + 1;
+      } else if constexpr (base == base_e::hex) {
+        return sizeof(decltype(integral)) * 2;
+      }
+    } };
+    // plus 1 is zero termination, meaning if you would use the outputted data() on the
     // string_view the next byte after the last character is guaranteed to be null
-    constexpr int buffer_size = std::numeric_limits<integral_t>::digits10 + 1 + 1;
+    constexpr int buffer_size = digits() + 1;
     impl_out<buffer_size> result{ .idx = buffer_size - 1 };
-    constexpr int radix = 10;  // Base 10 for decimal representation
+    constexpr auto radix = std::to_underlying(base);
     // Lookup table for digit characters
-    constexpr std::array<char, 10> digit_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    constexpr std::array<char, 16> digit_chars = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                                   '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
     integral_t abs_value = integral;
 
@@ -39,8 +50,8 @@ struct to_string_view {
   // View as a std::string_view
   static constexpr std::string_view value{ res.buffer.data() + res.idx + 1, res.buffer.size() - res.idx - 1 };
 };
-template <std::unsigned_integral auto integral>
-static constexpr auto to_string_view_v = to_string_view<integral>::value;
+template <std::unsigned_integral auto integral, base_e base = base_e::decimal>
+static constexpr auto to_string_view_v = to_string_view<integral, base>::value;
 
 namespace test {
 
@@ -51,6 +62,12 @@ static_assert(to_string_view_v<0U> == "0"sv);
 static_assert(to_string_view_v<65536U> == "65536"sv);
 static_assert(to_string_view_v<255U> == "255"sv);
 static_assert(to_string_view_v<18446744073709551615UL> == "18446744073709551615"sv);
+
+static_assert(to_string_view_v<15U, base_e::hex> == "F"sv);
+static_assert(to_string_view_v<10U, base_e::hex> == "A"sv);
+static_assert(to_string_view_v<65536U, base_e::hex> == "10000"sv);
+static_assert(to_string_view_v<255U, base_e::hex> == "FF"sv);
+static_assert(to_string_view_v<18446744073709551615UL, base_e::hex> == "FFFFFFFFFFFFFFFF"sv);
 
 }  // namespace test
 
