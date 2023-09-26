3ffaca3dd1bd475bbc2273ed88a100255fa64ec7
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-09-26 07:28:36 +0000

    add nolint

--- libs/ipc/src/item.cpp
+++ libs/ipc/src/item.cpp
@@ -11,7 +11,7 @@ namespace tfc::ipc::item {
 
 item make() {
   // todo how about storing the random engine in shared memory?
-  static std::mt19937_64 random_engine{};
+  static std::mt19937_64 random_engine{};  // NOLINT
   static bool first_call{ true };
   if (first_call) {
     first_call = false;
