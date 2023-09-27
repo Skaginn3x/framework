64eacb65bb26dcd84d028cc6e7be17aae934b8eb
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-09-27 13:11:55 +0000

    iso8601 not truncating time_point

--- libs/stx/tests/test_glaze_meta.cpp
+++ libs/stx/tests/test_glaze_meta.cpp
@@ -85,18 +85,18 @@ auto main() -> int {
         ut::expect(foo.value() == "Hell\0"sv)
             << foo.value().view();  // fixed string will always contain fixed number of characters hence the zero
       };
-  "chrono clock"_test = [] {
+  "steady clock should not be transposable"_test = [] {
     // transposing steady clock in json does not really make any sense
     static_assert(glz::name_v<std::chrono::steady_clock> == "glz::unknown");
   };
   "millisecond clock"_test = [] {
-    auto now{ std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()) };
+    auto now{ std::chrono::system_clock::now() };
     auto json{ glz::write_json(now) };
     ut::expect(glz::read_json<decltype(now)>(json).value() == now);
 
     std::chrono::system_clock::time_point time_point{};
     auto time_point_json{ glz::write_json(time_point) };
-    ut::expect(time_point_json == "\"1970-01-01T00:00:00.000+0000\"") << time_point_json;
+    ut::expect(time_point_json == "\"1970-01-01T00:00:00.000000+0000\"") << time_point_json;
   };
   return EXIT_SUCCESS;
 }
