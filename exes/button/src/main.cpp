7011330b38c0e26d24013c31a0e6a318aad6dc74
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-09-28 20:10:02 +0000

    Code review

--- exes/button/src/main.cpp
+++ exes/button/src/main.cpp
@@ -25,33 +25,37 @@ struct low {};
 struct timeout {};
 
 struct my_deps {
-  tfc::ipc::bool_signal& long_signal;
-  tfc::ipc::bool_signal& double_signal;
-  tfc::ipc::bool_signal& touch_signal;
+  my_deps(asio::io_context& ctx_para, tfc::logger::logger& logger_para, tfc::ipc_ruler::ipc_manager_client& client)
+      : ctx{ ctx_para }, logger{ logger_para }, double_signal{ ctx, client, "double_tap", "Button double tap" },
+        long_signal{ ctx, client, "long_press", "Button long press" }, touch_signal{ ctx, client, "touch", "Button touch" },
+        initial_time(system_clock::now()) {}
   asio::io_context& ctx;
   tfc::logger::logger& logger;
+  tfc::ipc::bool_signal double_signal;
+  tfc::ipc::bool_signal long_signal;
+  tfc::ipc::bool_signal touch_signal;
   std::chrono::time_point<system_clock, std::chrono::nanoseconds> initial_time;
 };
 
 // guards
-auto inital_time_set = [](my_deps& deps) { deps.initial_time = system_clock::now(); };
-auto long_press = [](my_deps& deps) {
+constexpr auto initial_time_set = [](my_deps& deps) { deps.initial_time = system_clock::now(); };
+constexpr auto long_press = [](my_deps& deps) {
   auto delta = system_clock::now() - deps.initial_time;
   return delta > 1000ms && delta < 2500ms;
 };
 
-auto short_press = [](my_deps& deps) {
+constexpr auto short_press = [](my_deps& deps) {
   auto delta = system_clock::now() - deps.initial_time;
   return delta < 1000ms;
 };
 
-auto too_long_press = [](my_deps& deps) {
+constexpr auto too_long_press = [](my_deps& deps) {
   auto delta = system_clock::now() - deps.initial_time;
   return delta >= 2500ms;
 };
 
 // actions
-auto start_timeout = [](const auto&, auto& state_machine, auto& deps, const auto& subs) {
+constexpr auto start_timeout = [](const auto&, auto& state_machine, auto& deps, const auto& subs) {
   // Create a timer from a shared_ptr
   auto deps_v = deps.value;
   auto timer = std::make_shared<asio::system_timer>(deps_v.ctx);
@@ -68,7 +72,7 @@ auto start_timeout = [](const auto&, auto& state_machine, auto& deps, const auto
 };
 
 // Actions
-auto send_signal = [](asio::io_context& ctx, tfc::ipc::bool_signal& signal, tfc::logger::logger& logger) {
+constexpr auto send_signal = [](asio::io_context& ctx, tfc::ipc::bool_signal& signal, tfc::logger::logger& logger) {
   signal.async_send(true, [&](const std::error_code& err, size_t) {
     if (err) {
       logger.warn("Failed to send, {}", err.message());
@@ -93,16 +97,17 @@ auto send_signal = [](asio::io_context& ctx, tfc::ipc::bool_signal& signal, tfc:
   });
 };
 
-auto send_long_press = [](my_deps& deps) { return send_signal(deps.ctx, deps.long_signal, deps.logger); };
+constexpr auto send_long_press = [](my_deps& deps) { return send_signal(deps.ctx, deps.long_signal, deps.logger); };
 
-auto send_touch = [](my_deps& deps) { return send_signal(deps.ctx, deps.touch_signal, deps.logger); };
+constexpr auto send_touch = [](my_deps& deps) { return send_signal(deps.ctx, deps.touch_signal, deps.logger); };
 
-auto send_double_tap = [](my_deps& deps) { return send_signal(deps.ctx, deps.double_signal, deps.logger); };
+constexpr auto send_double_tap = [](my_deps& deps) { return send_signal(deps.ctx, deps.double_signal, deps.logger); };
 
+/// A state machine for turning boolean signals into button events.
 struct button_state {
   auto operator()() {
     return sml::make_transition_table(
-        *"start"_s + sml::event<high> / inital_time_set = "choose"_s,
+        *"start"_s + sml::event<high> / initial_time_set = "choose"_s,
         "choose"_s + sml::event<low>[long_press] / send_long_press = "start"_s,
         "choose"_s + sml::event<low>[short_press] / start_timeout = "wait"_s,
         "choose"_s + sml::event<low>[too_long_press] = "start"_s,
@@ -118,34 +123,26 @@ auto main(int argc, char** argv) -> int {
   asio::io_context ctx{};
   tfc::ipc_ruler::ipc_manager_client client{ ctx };
 
-  tfc::ipc::bool_signal raw_signal{ ctx, client, "raw_value", "Raw button value" };
-  tfc::ipc::bool_signal double_tap_signal{ ctx, client, "double_tap", "Button double tap" };
-  tfc::ipc::bool_signal long_press_signal{ ctx, client, "long_press", "Button long press" };
-  tfc::ipc::bool_signal touch_signal{ ctx, client, "touch", "Button touch" };
-
   tfc::logger::logger logger("button");
 
-  auto deps = my_deps{ .long_signal = long_press_signal,
-                       .double_signal = double_tap_signal,
-                       .touch_signal = touch_signal,
-                       .ctx = ctx,
-                       .logger = logger,
-                       .initial_time = system_clock::now() };
+  auto deps = my_deps{ ctx, logger, client };
+
   sml::sm<button_state> sm(deps);
 
-  tfc::ipc::bool_slot slot{ ctx, client, "input", "Button input", [&](bool value) {
-                             logger.trace("Value: {}", value);
-                             raw_signal.async_send(value, [&](const std::error_code& err, size_t) {
-                               if (err) {
-                                 logger.warn("Failed to send raw_value, {}", err.message());
-                               }
-                             });
-                             if (value) {
-                               sm.process_event(sml::event<high>());
-                             } else {
-                               sm.process_event(sml::event<low>());
-                             }
-                           } };
+  tfc::ipc::bool_signal raw_signal{ ctx, client, "raw_value", "Raw button value" };
+  tfc::ipc::bool_slot input_slot{ ctx, client, "input", "Button input", [&](bool value) {
+                                   logger.trace("Value: {}", value);
+                                   raw_signal.async_send(value, [&](const std::error_code& err, size_t) {
+                                     if (err) {
+                                       logger.warn("Failed to send raw_value, {}", err.message());
+                                     }
+                                   });
+                                   if (value) {
+                                     sm.process_event(sml::event<high>());
+                                   } else {
+                                     sm.process_event(sml::event<low>());
+                                   }
+                                 } };
 
   ctx.run();
 
