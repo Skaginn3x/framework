7050ecc58dc5eccd996d0beba22c559f8f3b073f
Sindri Þór Harðarson <sindri@skaginn3x.com>
2023-10-03 08:39:21 +0000

    Exits with 0 if file exists

--- cockpit/IPC/install/create_link.js
+++ cockpit/IPC/install/create_link.js
@@ -7,8 +7,8 @@ const targetDir = path.join(os.homedir(), '.local', 'share', 'cockpit');
 
 // Ensure the target directory exists
 if (!fs.existsSync(targetDir)) {
-    console.log(`Target directory ${targetDir} does not exist. Creating...`);
-    fs.mkdirSync(targetDir, { recursive: true });
+  console.log(`Target directory ${targetDir} does not exist. Creating...`);
+  fs.mkdirSync(targetDir, { recursive: true });
 }
 
 // Change directory, as symlink must be run in the dir in which the symlink should reside
@@ -18,25 +18,25 @@ process.chdir(targetDir);
 const moduleRootDir = path.dirname(__dirname);
 // Gets the folder name for a symlink name
 const moduleFolderName = path.basename(moduleRootDir);
-console.log(`Linking ${targetDir} to ${moduleRootDir} as ${moduleFolderName}`);
 
 // Check if symlink already exists
 if (fs.existsSync(moduleFolderName)) {
-    console.log(`A file or directory with the name ${moduleFolderName} already exists in ${targetDir}.`);
-    process.exit(1);
+  console.log(`A file or directory with the name ${moduleFolderName} already exists in ${targetDir}.`);
+  process.exit(0);
 }
+console.log(`Linking ${targetDir} to ${moduleRootDir} as ${moduleFolderName}`);
 
 // Make the link, display errors if present
 fs.symlink(
-    path.join(moduleRootDir, 'build'),
-    moduleFolderName,
-    'dir',
-    (err) => {
-        if (err) {
-            console.error(`Failed to create symlink: ${err.message}`);
-            process.exit(1);
-        } else {
-            console.log(`Successfully created symlink at ${path.join(targetDir, moduleFolderName)}`);
-        }
+  path.join(moduleRootDir, 'build'),
+  moduleFolderName,
+  'dir',
+  (err) => {
+    if (err) {
+      console.error(`Failed to create symlink: ${err.message}`);
+      process.exit(1);
+    } else {
+      console.log(`Successfully created symlink at ${path.join(targetDir, moduleFolderName)}`);
     }
+  },
 );
