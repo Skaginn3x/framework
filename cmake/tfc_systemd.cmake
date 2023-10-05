34fd9b6a94ffc1d4a2f92935170b0b6da8c4adc6
magni <magnimar@skaginn3x.com>
2023-10-05 15:36:21 +0000

    moving to cmake file

--- cmake/tfc_systemd.cmake
+++ cmake/tfc_systemd.cmake
@@ -3,12 +3,13 @@ function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
 set(EXE_NAME ${EXE_TARGET})
 set(INSTALL_DIR ${CMAKE_INSTALL_BINDIR})
 
-configure_file("${CMAKE_SOURCE_DIR}/libs/systemctl/systemd/template.service.in" "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}.service")
+configure_file("${CMAKE_SOURCE_DIR}/cmake/systemd/tfc@.service" "${CMAKE_BINARY_DIR}/systemd/tfc@${EXE_TARGET}.service")
 
 install(
-  FILES "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}.service"
+  FILES "${CMAKE_BINARY_DIR}/systemd/tfc@${EXE_TARGET}.service"
   DESTINATION /usr/lib/systemd/system/
   CONFIGURATIONS Release
 )
 
 endfunction()
+
