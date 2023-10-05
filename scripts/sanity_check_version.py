de62659e2c2f598cc188ec786783d33f3271fc21
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-10-05 09:22:33 +0000

    Update cockpit function name

--- scripts/sanity_check_version.py
+++ scripts/sanity_check_version.py
@@ -14,28 +14,28 @@ def vcpkg_version_string():
     return read_and_parse('vcpkg.json')["version"]
 
 # Return the cockpit IPC version string
-def ipc_version_string():
+def cockpit_version_string():
     return read_and_parse('cockpit/IPC/package.json')["version"]
 
 # Return the base cmake version
 def cmake_project_version():
     json = read_and_parse('cmake/presets/cfg-build.json')['configurePresets']
     version_preset = [x for x in json if x['name'] == 'cfg-project-version']
-    if (len(version_preset) != 1):
+    if len(version_preset) != 1:
         print("Version preset not found in file!")
         exit(-1)
     version_preset = version_preset[0]
     return version_preset["cacheVariables"]["CMAKE_PROJECT_VERSION"]
 
 if __name__ == "__main__":
-    ipc_version   = ipc_version_string()
+    cockpit_version   = cockpit_version_string()
     vcpkg_version = vcpkg_version_string()
     cmake_version = cmake_project_version()
-    print(f"Cockpit ipc version {ipc_version}" )
+    print(f"Cockpit ipc version {cockpit_version}")
     print(f"vcpkg version {vcpkg_version}")
     print(f"Cmake version {cmake_version}")
 
-    if ipc_version != vcpkg_version:
+    if cockpit_version != vcpkg_version:
         print("ipc_version and vcpkg_version dont match!")
         exit(-1)
 
