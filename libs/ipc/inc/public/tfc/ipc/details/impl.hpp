f86e4d1f72da871badcba8b526d2474314b3e957
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-10-09 15:51:18 +0000

    me being stupid ?

--- libs/ipc/inc/public/tfc/ipc/details/impl.hpp
+++ libs/ipc/inc/public/tfc/ipc/details/impl.hpp
@@ -325,7 +325,7 @@ private:
     slot_.async_receive([bind_reference, callb = std::forward<decltype(callback)>(callback)](
                             std::expected<value_t, std::error_code> value) mutable {
       if (auto sptr = bind_reference.lock()) {
-        sptr->async_new_state(value, std::forward<decltype(callback)>(callb));
+        sptr->async_new_state(value, std::forward(callb));
       }
     });
   }
