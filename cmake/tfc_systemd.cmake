1194ffa7a779566b2f6df13a973a5450c291cc9d
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-10-10 15:13:23 +0000

    remove user

--- cmake/tfc_systemd.cmake
+++ cmake/tfc_systemd.cmake
@@ -1,12 +1,6 @@
-function(tfc_systemd_service_file EXE_TARGET DESCRIPTION SERVICE_USER)
-
-  if (NOT SERVICE_USER)
-    set(SERVICE_USER "")
-  endif ()
-
+function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
   set(EXE_NAME ${EXE_TARGET})
   set(INSTALL_DIR ${CMAKE_INSTALL_BINDIR})
-  set(USER ${SERVICE_USER})
 
   configure_file("${CMAKE_SOURCE_DIR}/cmake/systemd/tfc@.service" "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service")
 
@@ -15,5 +9,4 @@ function(tfc_systemd_service_file EXE_TARGET DESCRIPTION SERVICE_USER)
     DESTINATION /usr/lib/systemd/system/
     CONFIGURATIONS Release
   )
-
 endfunction()
