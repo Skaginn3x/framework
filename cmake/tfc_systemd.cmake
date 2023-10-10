19b8871a56e62aeffd44b7895cfedaf89d283a85
magni-mar <135731785+magni-mar@users.noreply.github.com>
2023-10-10 16:30:36 +0000

    Automatic creation of systemctl files (#183)
    * automatic creation of systemctl files
    
    * formatting
    
    * I think gitignore is ruining the pipes
    
    * updating ipc-ruler CMake
    
    * remove stdout log level from service file
    
    * revert gitignore
    
    * remove user
    
    * install operation mode
    
    * fix incorrect target
    
    * systemd service for ethercat with iface argument
    
    * systemd service for ethercat with iface argument
    
    ---------
    
    Co-authored-by: Jón Bjarni Bjarnason <jbbjarnason@gmail.com>

---
+++ cmake/tfc_systemd.cmake
@@ -0,0 +1,12 @@
+function(tfc_systemd_service_file EXE_TARGET DESCRIPTION)
+  set(EXE_NAME ${EXE_TARGET})
+  set(INSTALL_DIR ${CMAKE_INSTALL_BINDIR})
+
+  configure_file("${CMAKE_SOURCE_DIR}/cmake/systemd/tfc@.service" "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service")
+
+  install(
+    FILES "${CMAKE_BINARY_DIR}/systemd/${EXE_TARGET}@.service"
+    DESTINATION /usr/lib/systemd/system/
+    CONFIGURATIONS Release
+  )
+endfunction()
