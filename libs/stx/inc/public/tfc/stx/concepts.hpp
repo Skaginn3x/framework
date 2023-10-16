5230d0cb4424af18584ce10898bb48bfc900808d
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-10-16 13:10:02 +0000

    make sonar happy

--- libs/stx/inc/public/tfc/stx/concepts.hpp
+++ libs/stx/inc/public/tfc/stx/concepts.hpp
@@ -14,6 +14,9 @@ concept invocable = std::invocable<std::remove_cvref_t<func>, args...>;
 template <typename enum_t>
 concept is_enum = requires { requires std::is_enum_v<enum_t>; };
 
+template <typename compare_to_t, typename ... args_t>
+concept is_one_of = requires { requires (std::is_same_v<compare_to_t, args_t> || ...); };
+
 // from
 // https://stackoverflow.com/questions/16337610/how-to-know-if-a-type-is-a-specialization-of-stdvector
 template <class, template <class...> class>
