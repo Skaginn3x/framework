15fd634976dd41e1eed3c5b838324a1412d58a3d
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-10-19 07:36:50 +0000

    adding example with complicated structure in config

---
+++ libs/confman/testing/examples/confman_example_item.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <cstdint>
+#include <string>
+
+#include <fmt/chrono.h>
+#include <fmt/core.h>
+#include <mp-units/systems/si/si.h>
+#include <boost/asio.hpp>
+
+#include <tfc/confman.hpp>
+#include <tfc/confman/observable.hpp>
+#include <tfc/stx/glaze_meta.hpp>
+#include <tfc/utils/units_glaze_meta.hpp>
+#include <tfc/ipc/item.hpp>
+#include <tfc/ipc/details/item_glaze_meta.hpp>
+
+namespace asio = boost::asio;
+
+int main() {
+  asio::io_context ctx{};
+
+  tfc::confman::config<tfc::confman::observable<tfc::ipc::item::item>> const config{ ctx, "key" };
+  config->observe(
+      [](auto new_value, auto old_value) { fmt::print("new value: {}, old value: {}\n", new_value.to_json(), old_value.to_json()); });
+
+  fmt::print("Schema is: {}\n", config.schema());
+  fmt::print("Config is: {}\n", config.string());
+
+  ctx.run();
+  return 0;
+}
