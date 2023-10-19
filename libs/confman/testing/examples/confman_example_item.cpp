71c24ca6afe593233d8b2cbc337569af2b44c805
jbbjarnason <jbbjarnason@users.noreply.github.com>
2023-10-19 07:42:12 +0000

    Committing clang-format changes

--- libs/confman/testing/examples/confman_example_item.cpp
+++ libs/confman/testing/examples/confman_example_item.cpp
@@ -2,13 +2,13 @@
 #include <mp-units/systems/si/si.h>
 #include <boost/asio.hpp>
 
-#include <tfc/progbase.hpp>
 #include <tfc/confman.hpp>
 #include <tfc/confman/observable.hpp>
+#include <tfc/ipc/details/item_glaze_meta.hpp>
+#include <tfc/ipc/item.hpp>
+#include <tfc/progbase.hpp>
 #include <tfc/stx/glaze_meta.hpp>
 #include <tfc/utils/units_glaze_meta.hpp>
-#include <tfc/ipc/item.hpp>
-#include <tfc/ipc/details/item_glaze_meta.hpp>
 
 namespace asio = boost::asio;
 
@@ -18,8 +18,9 @@ int main(int argc, char** argv) {
   asio::io_context ctx{};
 
   tfc::confman::config<tfc::confman::observable<tfc::ipc::item::item>> const config{ ctx, "key" };
-  config->observe(
-      [](auto new_value, auto old_value) { fmt::print("new value: {}, old value: {}\n", new_value.to_json(), old_value.to_json()); });
+  config->observe([](auto new_value, auto old_value) {
+    fmt::print("new value: {}, old value: {}\n", new_value.to_json(), old_value.to_json());
+  });
 
   fmt::print("Schema is: {}\n", config.schema());
   fmt::print("Config is: {}\n", config.string());
--- libs/confman/testing/examples/nested.cpp
+++ libs/confman/testing/examples/nested.cpp
@@ -7,9 +7,9 @@
 #include <mp-units/systems/si/si.h>
 #include <boost/asio.hpp>
 
-#include <tfc/progbase.hpp>
 #include <tfc/confman.hpp>
 #include <tfc/confman/observable.hpp>
+#include <tfc/progbase.hpp>
 #include <tfc/stx/glaze_meta.hpp>
 #include <tfc/utils/units_glaze_meta.hpp>
 
