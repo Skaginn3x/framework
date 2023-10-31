7971a55c434aa17b9b3cd7fc1447d5878b343b19
Sindri Þór Harðarson <sindri@skaginn3x.com>
2023-10-31 16:46:29 +0000

    Added keyboard shortcut information to connections

--- cockpit/IPC/src/views/Connections.tsx
+++ cockpit/IPC/src/views/Connections.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
-import { Title } from '@patternfly/react-core';
+import { QuestionCircleIcon } from '@patternfly/react-icons';
+import {
+  Button, Modal, Title, Tooltip,
+} from '@patternfly/react-core';
+
 import { DarkModeType } from 'src/App';
 import CustomTable from '../Components/Table/Table';
 import useDbusInterface from '../Components/Interface/DbusInterface';
@@ -25,9 +29,47 @@ const Connections:React.FC<DarkModeType> = ({ isDark }) => {
   }, [dbusInterface.valid]);
 
   const { signalList, slotList, connections } = dbusInterface;
+  const [modalIsOpen, setModalIsOpen] = React.useState<boolean>(false);
+
+  const TooltipContent = (
+    <>
+      <Title headingLevel="h1" size="2xl" style={{ color: isDark ? '#EEE' : '#111' }}>
+        Keyboard Shortcuts
+      </Title>
+      <br />
+      <strong>Arrow keys:</strong>
+      <ul>
+        <li>Up/Down - Move up/down in table</li>
+      </ul>
+      <strong>If slot is selected:</strong>
+      <ul>
+        <li>Enter - Connect signal to slot</li>
+      </ul>
+      <strong>If signal is selected:</strong>
+      <ul>
+        <li>Enter - Disconnect signal from slot</li>
+        <li>Shift + Enter - Open Filter Dialog</li>
+      </ul>
+      <strong>General:</strong>
+      <ul>
+        <li>Ctrl + F - Open search</li>
+      </ul>
+    </>
+  );
 
   return (
     <>
+      <Tooltip
+        content="Keyboard Shortcuts"
+        title="Keyboard Shortcuts"
+      >
+        <QuestionCircleIcon
+          size={16}
+          color={isDark ? '#EEE' : '#111'}
+          style={{ position: 'absolute', top: '1rem', left: '1rem' }}
+          onClick={() => setModalIsOpen(true)}
+        />
+      </Tooltip>
       <Title headingLevel="h1" size="2xl" style={{ color: isDark ? '#EEE' : '#111' }}>
         Connections
       </Title>
@@ -40,6 +82,24 @@ const Connections:React.FC<DarkModeType> = ({ isDark }) => {
           isDark={isDark}
         />
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onEscapePress={() => setModalIsOpen(false)}
+        onClose={() => setModalIsOpen(false)}
+        style={{ color: isDark ? '#EEE' : '#111' }}
+        variant="small"
+        actions={[
+          <Button
+            key="close"
+            variant="primary"
+            onClick={() => setModalIsOpen(false)}
+          >
+            Close
+          </Button>,
+        ]}
+      >
+        {TooltipContent}
+      </Modal>
     </>
   );
 };
