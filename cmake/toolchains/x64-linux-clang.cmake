b28bd77991c179d41dd747d5570137dd836057da
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-11-03 13:22:18 +0000

    add include path to custom built libc++

--- cmake/toolchains/x64-linux-clang.cmake
+++ cmake/toolchains/x64-linux-clang.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_PROCESSOR x64)
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_FLAGS "-stdlib=libc++")
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1/ -I/cpproot/include/c++/v1")
 set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld")
