9e87f468c42373addf1bcb60aa47089540b9eedb
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-11-03 13:35:37 +0000

    add link path to custom built libc++

--- vcpkg-overlays/triplets/x64-linux-clang-dynamic.cmake
+++ vcpkg-overlays/triplets/x64-linux-clang-dynamic.cmake
@@ -5,12 +5,12 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1/ -I/cpproot/include/c++/v1")
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1/ -I/cpproot/include/c++/v1 -L/cpproot/lib/x86_64-unknown-linux-gnu/ ")
 set(LINK_FLAGS "-fuse-ld=lld -stdlib=libc++")
 
 set(ENV{CC} clang)
 set(ENV{CXX} clang++)
-set(ENV{CXXFLAGS} "-stdlib=libc++ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1/ -I/cpproot/include/c++/v1")
+set(ENV{CXXFLAGS} "-stdlib=libc++ -I/cpproot/include/x86_64-unknown-linux-gnu/c++/v1/ -I/cpproot/include/c++/v1 -L/cpproot/lib/x86_64-unknown-linux-gnu/ ")
 set(ENV{LDFLAGS} "-fuse-ld=lld -stdlib=libc++")
 
 set(VCPKG_FIXUP_ELF_RPATH ON)
