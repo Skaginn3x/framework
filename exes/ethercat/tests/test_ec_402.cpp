2437ca531ac1f721bf3de7f0adc1f4b3d9f6c79f
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-11-09 11:54:24 +0000

    test only with gcc

--- exes/ethercat/tests/test_ec_402.cpp
+++ exes/ethercat/tests/test_ec_402.cpp
@@ -38,8 +38,6 @@ auto main(int, char**) -> int {
 }
 
 namespace compile_tests {
-// bitcast is not completely constexpr in clang, memcpy is not constexpr, underlying behaviour of bitcast
-#ifndef __clang__
 using tfc::ec::cia_402::commands_e;
 using tfc::ec::cia_402::control_word;
 using tfc::ec::cia_402::states_e;
@@ -104,6 +102,8 @@ static_assert(states_e::fault == status_word{ .state_fault = 1, .voltage_enabled
 static_assert(states_e::fault == status_word{ .state_fault = 1, .state_quick_stop = 1 }.parse_state());
 static_assert(states_e::fault == status_word{ .state_fault = 1, .voltage_enabled = 1, .state_quick_stop = 1 }.parse_state());
 
+// bitcast is not completely constexpr in clang, memcpy is not constexpr, underlying behaviour of bitcast
+#ifndef __clang__
 static_assert(control_word::from_uint(static_cast<std::uint16_t>(
                   control_word{ .operating_state_switch_on = true, .operating_state_quick_stop = true, .halt = true })) ==
               control_word{ .operating_state_switch_on = true, .operating_state_quick_stop = true, .halt = true });
