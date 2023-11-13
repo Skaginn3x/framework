cb39e4303e40c52cdb84e6bba3ca1452bbf976e5
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-11-13 15:10:53 +0000

    more sane input names

--- exes/ethercat/src/devices/beckhoff.cpp
+++ exes/ethercat/src/devices/beckhoff.cpp
@@ -6,9 +6,9 @@ namespace tfc::ec::devices::beckhoff {
 
 using imc = tfc::ipc_ruler::ipc_manager_client;
 
-template class el1xxx<imc, el1002<imc>::size_v, el1002<imc>::product_code, el1002<imc>::name>;
-template class el1xxx<imc, el1008<imc>::size_v, el1008<imc>::product_code, el1008<imc>::name>;
-template class el1xxx<imc, el1809<imc>::size_v, el1809<imc>::product_code, el1809<imc>::name>;
+template class el1xxx<imc, el1002<imc>::size_v, el1002<imc>::entries_v, el1002<imc>::product_code, el1002<imc>::name>;
+template class el1xxx<imc, el1008<imc>::size_v, el1008<imc>::entries_v, el1008<imc>::product_code, el1008<imc>::name>;
+template class el1xxx<imc, el1809<imc>::size_v, el1809<imc>::entries_v, el1809<imc>::product_code, el1809<imc>::name>;
 
 template class el2xxx<imc, el2794<imc>::size_v, el2794<imc>::entries_v, el2794<imc>::product_code, el2794<imc>::name_v>;
 template class el2xxx<imc, el2004<imc>::size_v, el2004<imc>::entries_v, el2004<imc>::product_code, el2004<imc>::name_v>;
