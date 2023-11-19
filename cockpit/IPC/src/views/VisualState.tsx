71109494cbeea9603807cac1f7f64a126fe55fe7
Sindri Þór Harðarson <sindri@skaginn3x.com>
2023-11-19 15:39:34 +0000

    Live listening to changes on DBUS

--- cockpit/IPC/src/views/VisualState.tsx
+++ cockpit/IPC/src/views/VisualState.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Title } from '@patternfly/react-core';
 import { loadExternalScript } from 'src/Components/Interface/ScriptLoader';
 import { DarkModeType } from 'src/App';
 import parse from 'html-react-parser';
+import { TFC_DBUS_DOMAIN, TFC_DBUS_ORGANIZATION } from 'src/variables';
 
 declare global {
   interface Window { cockpit: any; }
@@ -12,55 +14,10 @@ declare global {
 // eslint-disable-next-line react/function-component-definition
 const VisualState:React.FC<DarkModeType> = ({ isDark }) => {
   const [svg, setSVG] = useState<any>();
-
-  function parseDBusObjects(svgString: string) {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
-    const dbusElements = xmlDoc.querySelectorAll('[dbus\\:interface], [dbus\\:path], [dbus\\:service]');
-
-    const dbusObjects = Array.from(dbusElements).map((element) => {
-      // Extract the placeholder value inside ${}
-      const placeholderMatch = element.textContent?.match(/\$\{([^}]+)\}/);
-      const placeholder = placeholderMatch ? placeholderMatch[1] : '';
-
-      return {
-        interface: element.getAttribute('dbus:interface') ?? '',
-        path: element.getAttribute('dbus:path') ?? '',
-        service: element.getAttribute('dbus:service') ?? '',
-        property: placeholder,
-      };
-    });
-
-    return dbusObjects;
-  }
-  function replaceSvgPlaceholders(svgString: string, interfaceName: string, process: string, path: string, replacementValue: string) {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
-
-    // Find all text elements that might contain placeholders
-    const textElements = xmlDoc.querySelectorAll('text');
-
-    textElements.forEach((element) => {
-      // Check if the element matches the specified criteria
-      if (element.getAttribute('dbus:interface') === interfaceName
-          && element.getAttribute('dbus:path') === path
-          && element.getAttribute('dbus:service') === process) {
-        // Replace placeholder in the text content
-        if (element.textContent?.includes('${')) {
-          // eslint-disable-next-line no-param-reassign
-          element.textContent = element.textContent.replace(/\$\{[^}]+\}/g, replacementValue);
-        }
-      }
-    });
-
-    // Serialize the modified XML back to a string
-    const serializer = new XMLSerializer();
-    return serializer.serializeToString(xmlDoc);
-  }
-
-  useEffect(() => {
-    loadExternalScript(() => {});
-    let initialsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  const [processDBUS, setProcessDBUS] = useState<any>();
+  const [data, setData] = useState<any>({});
+  const dataRef = useRef(data);
+  const initialsvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <svg
        sodipodi:docname="demo.svg"
        inkscape:version="1.3 (0e150ed6c4, 2023-07-21)"
@@ -76,6 +33,8 @@ const VisualState:React.FC<DarkModeType> = ({ isDark }) => {
        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
        xmlns:cc="http://creativecommons.org/ns#"
        xmlns:dc="http://purl.org/dc/elements/1.1/">
+       xmlns:dbus="http://www.skaginn3x.com/"
+       xmlns:tfc="http://www.skaginn3x.com/"
       <defs
          id="defs4">
         <rect
@@ -199,16 +158,17 @@ const VisualState:React.FC<DarkModeType> = ({ isDark }) => {
            cy="-43.435825"
            rx="8.4046373"
            ry="8.0472383"
-           tfc:signal="ethercat.def.bool.el1809.s4.in14"
-           tfc:on_true="green" />
+           tfcSignal="com.skaginn3x.operations.def.bool.run_button"
+           tfcOn_true="green" 
+           tfcOn_false="red"/>
         <text
            xml:space="preserve"
            transform="matrix(0.93749998,0,0,0.93749998,529.30384,-588.44598)"
            id="text1"
            style="white-space:pre;shape-inside:url(#rect2);fill:#000000;fill-opacity:0;stroke:#000000;stroke-width:0.714331;stroke-dasharray:none;stroke-opacity:1"
-           dbus:interface="com.skaginn3x.SensorControl"
-           dbus:path="/com/skaginn3x/StateMachines"
-           dbus:service="com.skaginn3x.tfc.sensor_control.exit_1_10"><tspan
+           dbusInterface="com.skaginn3x.Operations"
+           dbusPath="/com/skaginn3x/StateMachines"
+           dbusService="com.skaginn3x.tfc.operations.def"><tspan
              x="942.15039"
              y="607.71538"
              id="tspan3">\${State}</tspan></text>
@@ -217,28 +177,233 @@ const VisualState:React.FC<DarkModeType> = ({ isDark }) => {
            transform="matrix(0.93749998,0,0,0.93749998,431.60097,-577.8504)"
            id="text1-2"
            style="white-space:pre;shape-inside:url(#rect2-6);fill:#000000;fill-opacity:0;stroke:#000000;stroke-width:0.714331;stroke-dasharray:none;stroke-opacity:1"
-           tfc:signal="atv320.s6.in.freq"><tspan
+           tfcSignal="com.skaginn3x.sensor_control.sadd.json.discharge_request"><tspan
              x="942.15039"
              y="607.71538"
              id="tspan4">\${} Hz</tspan></text>
       </g>
-    </svg>`;
-    console.log(initialsvg);
-    const interfacesToChange = parseDBusObjects(initialsvg);
+    </svg>` as any;
+
+  function toggleDarkMode(svgString: string) {
+    if (isDark) {
+      svgString = svgString.replaceAll('stroke:#000000', 'stroke:#EEE');
+      svgString = svgString.replaceAll('fill:#000000', 'fill:#EEE');
+    } else {
+      svgString = svgString.replaceAll('stroke:#EEE', 'stroke:#000000');
+      svgString = svgString.replaceAll('fill:#EEE', 'fill:#000000');
+    }
+    return svgString;
+  }
+
+  function parseDBusTextObjects(svgString: string) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
+    // Check for parsing errors
+    if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
+      console.error('Error parsing XML');
+      return [];
+    }
+
+    const dbusElements = xmlDoc.querySelectorAll('[dbusInterface], [dbusPath], [dbusService]');
+
+    const dbusObjects = Array.from(dbusElements).map((element) => {
+      // Extract the placeholder value inside ${}
+      const placeholderMatch = element.textContent?.match(/\$\{([^}]+)\}/);
+      const placeholder = placeholderMatch ? placeholderMatch[1] : '';
+
+      return {
+        interface: element.getAttribute('dbusInterface') ?? '',
+        path: element.getAttribute('dbusPath') ?? '',
+        service: element.getAttribute('dbusService') ?? '',
+        property: placeholder,
+      };
+    });
+
+    return dbusObjects;
+  }
+
+  function replaceDBusSignals(svgString: string): string {
+    // data is of format: [{ com.skaginn3x.operations.def.bool.run_button: true }, { com.skaginn3x.operations.def.bool.run_button: false }}]
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
+    // Check for parsing errors
+    if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
+      console.error('Error parsing XML');
+      return '';
+    }
+
+    const dbusElements = xmlDoc.querySelectorAll('[tfcSignal]');
+
+    dbusElements.forEach((element) => {
+      // Check if the element matches the specified criteria
+      const signal = element.getAttribute('tfcSignal');
+      console.log('signal', signal);
+      console.log('1');
+      if (element.hasAttribute('tfcOn_true') && element.hasAttribute('tfcOn_false')) {
+        const onTrue = element.getAttribute('tfcOn_true');
+        const onFalse = element.getAttribute('tfcOn_false');
+        console.log('2');
+        // Replace placeholder in the text content
+        if (signal && onTrue && onFalse) {
+          element.setAttribute('style', '');
+          console.log('3', element.getAttribute('style'));
+          if (data[signal] === true) {
+            element.setAttribute('fill', onTrue);
+            element.setAttribute('stroke', onTrue);
+          } else {
+            element.setAttribute('fill', onFalse);
+            element.setAttribute('stroke', onFalse);
+          }
+        }
+      } else if (signal) {
+        console.log('4');
+        // Replace placeholder in the text content
+        const tspan = element.querySelector('tspan');
+        // replace
+        if (tspan && tspan.textContent?.includes('${}')) {
+          tspan.textContent = tspan.textContent.replace('${}', data[signal]);
+        }
+      }
+    });
+    // serialize the XML to a string
+    return new XMLSerializer().serializeToString(xmlDoc);
+  }
+
+  function parseDBusSignals(svgString: string) {
+    // Returns names of signals
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
+    // Check for parsing errors
+    if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
+      console.error('Error parsing XML');
+      return [];
+    }
+
+    const dbusElements = xmlDoc.querySelectorAll('[tfcSignal]');
+    const dbusSignals = Array.from(dbusElements).map((element) => {
+      // Extract the placeholder value inside ${}
+      const signal = element.getAttribute('tfcSignal') ?? '';
+      return signal;
+    });
+
+    return dbusSignals.reduce((acc: any, cur: any) => {
+      acc[cur] = null;
+      return acc;
+    }, {});
+  }
+
+  function replaceSvgPlaceholders(svgString: string, interfaceName: string, process: string, path: string, placeholder:string, replacementValue: string) {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(svgString, 'text/xml');
+    // Check for parsing errors
+    if (xmlDoc.getElementsByTagName('parsererror').length > 0) {
+      console.error('Error parsing XML');
+      return '';
+    }
+
+    // Find all text elements that might contain placeholders
+    const textElements = xmlDoc.querySelectorAll('text');
+
+    textElements.forEach((element) => {
+      // Check if the element matches the specified criteria
+      if (element.getAttribute('dbusInterface') === interfaceName
+          && element.getAttribute('dbusPath') === path
+          && element.getAttribute('dbusService') === process) {
+        // Replace placeholder in the text content
+        const tspan = element.querySelector('tspan');
+        if (tspan && tspan.textContent?.includes(`\${${placeholder}}`)) {
+          tspan.textContent = tspan.textContent.replace(`\${${placeholder}}`, replacementValue);
+        }
+      }
+      if (element.getAttribute('style')?.includes('stroke-opacity') && element.getAttribute('style')?.includes('fill-opacity')) {
+        // Replace placeholder in the text content
+        const style = element.getAttribute('style');
+        if (style) {
+          let newStyle = element.getAttribute('style');
+          newStyle = newStyle!.replace('stroke-opacity:1', 'stroke-opacity:0');
+          newStyle = newStyle.replace('fill-opacity:0', 'fill-opacity:1');
+          element.setAttribute('style', newStyle);
+        }
+      }
+    });
+    // serialize the XML to a string
+    return new XMLSerializer().serializeToString(xmlDoc);
+  }
+
+  function handleChange() {
+    const interfacesToChange = parseDBusTextObjects(initialsvg);
     const proxies = interfacesToChange.map((interfaceToChange) => {
-      const dbus = window.cockpit.dbus(interfaceToChange.service, interfaceToChange.path);
-      return dbus.proxy(interfaceToChange.interface);
+      if (window.cockpit) {
+        const dbus = window.cockpit.dbus(interfaceToChange.service);
+        return {
+          interface: interfaceToChange.interface,
+          path: interfaceToChange.path,
+          process: interfaceToChange.service,
+          placeholder: interfaceToChange.property,
+          proxy: dbus.proxy(interfaceToChange.interface, interfaceToChange.path),
+        };
+      }
+      return {};
     });
+    let newSvg = replaceDBusSignals(initialsvg);
     // resolve promises
-    proxies.forEach(async (proxy) => {
-      await proxy.wait().then(() => {
-        const replacementValue = proxy[interfacesToChange[0].property];
-        initialsvg = replaceSvgPlaceholders(svg, proxy.interface, proxy.process, proxy.path, replacementValue);
-      });
+    Promise.all(proxies.map(async (iface: any) => {
+      await iface.proxy.wait();
+      const replacementValue = iface.proxy.data[iface.placeholder];
+      newSvg = replaceSvgPlaceholders(newSvg, iface.interface, iface.process, iface.path, iface.placeholder, replacementValue);
+    })).then(() => {
+      setSVG(toggleDarkMode(newSvg));
     });
-    console.log(initialsvg);
-    setSVG(initialsvg);
+  }
+
+  useEffect(() => {
+    loadExternalScript(() => {
+      setData(parseDBusSignals(initialsvg));
+      setProcessDBUS(window.cockpit.dbus(null));
+      handleChange();
+    });
   }, []);
+
+  useEffect(() => {
+    if (!processDBUS) return;
+
+    const match = {
+      path_namespace: `/${TFC_DBUS_DOMAIN}/${TFC_DBUS_ORGANIZATION}`,
+    };
+    processDBUS.watch(match);
+
+    const notifyHandler = (newData: any) => {
+      const removePath = newData.detail[Object.keys(newData.detail)[0]];
+      console.log('removePath', removePath);
+      console.log('dataref', dataRef.current);
+      const ifaceName = Object.keys(removePath)[0];
+      if (Object.keys(dataRef.current).includes(ifaceName)) {
+        setData((prevState: any) => {
+          const newState = { ...prevState };
+          console.log('newState', newState);
+          newState[ifaceName] = removePath[ifaceName].Value;
+          return newState;
+        });
+      }
+    };
+
+    processDBUS.addEventListener('notify', notifyHandler);
+    // eslint-disable-next-line consistent-return
+    return () => {
+      processDBUS.removeEventListener('notify', notifyHandler);
+    };
+  }, [processDBUS]);
+
+  useEffect(() => {
+    dataRef.current = data;
+    handleChange();
+  }, [data]);
+
+  useEffect(() => {
+    if (svg) {
+      setSVG(toggleDarkMode(svg));
+    }
+  }, [isDark]);
 
   return (
     <div style={{ color: isDark ? '#EEE' : '#111' }}>
