8a0428974d85cbf375ddedd59920afd1bee83472
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-12-02 21:03:15 +0000

    revert beve test

--- libs/ipc/inc/public/tfc/ipc/details/dbus_server_iface.hpp
+++ libs/ipc/inc/public/tfc/ipc/details/dbus_server_iface.hpp
@@ -22,6 +22,7 @@
 #include <tfc/ipc/details/dbus_structs.hpp>
 #include <tfc/ipc/details/dbus_structs_glaze_meta.hpp>
 #include <tfc/ipc/enums.hpp>
+#include <tfc/ipc/glaze_meta.hpp>
 #include <tfc/logger.hpp>
 #include <tfc/progbase.hpp>
 
