0c12449379eed4fb8d48115db953f641ed129dc3
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-12-05 09:52:44 +0000

    remove weigher changes in ethercat

--- exes/ethercat/inc/public/tfc/ec/devices/device.hpp
+++ exes/ethercat/inc/public/tfc/ec/devices/device.hpp
@@ -12,8 +12,7 @@
 #include "beckhoff/EL4xxx.hpp"
 #include "beckhoff/EQ2339.hpp"
 #include "schneider/lxm32m.hpp"
-#include "schneider/atv320.hpp"
-#include "eilersen/4x60a.hpp"
+#include "tfc/ec/devices/schneider/atv320.hpp"
 
 namespace tfc::ec::devices {
 
@@ -36,8 +35,7 @@ using devices_list = devices_type<
   //beckhoff::el3054,
   beckhoff::el4002,
   schneider::atv320::device<manager_client_t>,
-  schneider::lxm32m<manager_client_t>,
-  eilersen::e4x60a::e4x60a<manager_client_t>
+  schneider::lxm32m<manager_client_t>
 >;
 // clang-format on
 
