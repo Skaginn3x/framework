1af13d6fb37ca5688e73e1b244cfc1e9248b6d06
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2023-12-06 18:28:32 +0000

    Some playing

--- exes/ttui/src/ttui.cpp
+++ exes/ttui/src/ttui.cpp
@@ -1,6 +1,7 @@
 #include "ftxui/component/captured_mouse.hpp"      // for ftxui
 #include "ftxui/component/component.hpp"           // for Menu
 #include "ftxui/component/component_options.hpp"   // for MenuOption
+#include "ftxui/component/loop.hpp"                // for ScreenInteractive
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
 
 #include <memory>
@@ -31,33 +32,59 @@ namespace po = boost::program_options;
 
 using variant_t = std::variant<bool, int64_t, uint64_t, double, std::string, std::vector<std::string>>;
 
-auto left_menu(const std::vector<std::string>* entries, int* selected, const ftxui::Component& right_menu)
-    -> ftxui::Component {
+std::vector<std::string> noticed_interfaces{};
+std::vector<std::variant<bool, uint64_t>> values{};
+std::vector<std::string> entries;
+int global_left_selected = 0;
+
+auto left_menu(const std::vector<std::string>*, int* selected, const ftxui::Component& right_menu) -> ftxui::Component {
   auto option = ftxui::MenuOption::Vertical();
 
-  option.on_enter = [selected, right_menu] { right_menu->TakeFocus(); };
+  option.on_enter = [selected, right_menu] {
+    global_left_selected = *selected;
+    std::string current_value = std::visit([](auto& value) { return std::to_string(value); }, values[*selected]);
+    entries = { current_value };
+    right_menu->TakeFocus();
+  };
 
-  return ftxui::Menu(entries, selected, option);
+  return ftxui::Menu(&noticed_interfaces, selected, option);
 };
-
-auto right_menu(const std::vector<std::string>* entries, int* selected) -> ftxui::Component {
+auto right_menu(int* selected) -> ftxui::Component {
   const auto option = ftxui::MenuOption::Vertical();
   // fill remaining horizontal space
-  return ftxui::Menu(entries, selected, option);
+  return ftxui::Menu(&entries, selected, option);
 };
 
 // GET DEMM DBUS DATA
-auto match_callback(sdbusplus::message_t& msg) -> void {
+auto match_callback(sdbusplus::message_t& msg, ftxui::Component left, ftxui::Component right ) -> void {
   // std::cout << msg.get_member() << std::endl;
   // std::cout << msg.get_signature() << std::endl;
   // std::cout << msg.get_sender() << std::endl;
   // std::cout << msg.get_interface() << std::endl;
   // std::cout << msg.get_path() << std::endl;
   if (msg.get_member() && std::string(msg.get_member()) == "PropertiesChanged") {
-    std::tuple<std::string, std::vector<std::pair<std::string, std::variant<bool, uint64_t>>>, std::vector<std::string>> container{};
+    std::tuple<std::string, std::vector<std::pair<std::string, std::variant<bool, uint64_t>>>, std::vector<std::string>>
+        container{};
     if (sdbusplus::utility::read_into_tuple(container, msg)) {
       std::string const& interface = std::get<0>(container);
-      std::visit([&interface](auto& value) { std::cout << interface << ": " << value << std::endl; }, std::get<1>(container)[0].second);
+      auto position = std::find(noticed_interfaces.begin(), noticed_interfaces.end(), interface);
+      auto value = std::get<1>(container)[0].second;
+      if (position == noticed_interfaces.end()) {
+        left->Render();
+        noticed_interfaces.emplace_back(interface);
+        values.emplace_back();
+      } else {
+        auto index = position - noticed_interfaces.begin();
+        values[index] = value;
+
+        std::string current_value = std::visit([](auto& value) { return std::to_string(value); }, values[index]);
+        entries = { current_value };
+        if (index == global_left_selected) {
+          right->Render();
+        }
+      }
+      // std::visit([&interface](auto& value) { std::cout << interface << ": " << value << std::endl; },
+      // std::get<1>(container)[0].second);
     }
   }
 }
@@ -78,35 +105,6 @@ auto main(int argc, char** argv) -> int {
       "list-slots", po::bool_switch(&list_slots), "List all available IPC slots");
   tfc::base::init(argc, argv, description);
 
-  asio::io_context ctx{};
-  std::shared_ptr<sdbusplus::asio::connection> connection =
-      std::make_shared<sdbusplus::asio::connection>(ctx, tfc::dbus::sd_bus_open_system_mon());
-  // Attach callback
-  auto match = std::make_unique<sdbusplus::bus::match::match>(*connection, "", match_callback);
-  // Become monitor method
-  auto mc = connection->new_method_call("org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus.Monitoring",
-                                        "BecomeMonitor");
-  mc.append<std::vector<std::string>, uint32_t>({ "path=/com/skaginn3x/Signals" }, 0);
-  auto reply = connection->call(mc);
-  if (reply.is_method_error()) {
-    if (reply.get_error() != nullptr)
-      throw std::runtime_error(reply.get_error()->message);
-    throw std::runtime_error("Unknown error");
-  }
-  // auto ucontainer = connection->new_method_call("org.freedesktop.DBus", "/org/freedesktop/DBus",
-  // "org.freedesktop.DBus.Monitoring",
-  //                                       "BecomeMonitor");
-  // ucontainer.append<std::tuple<std::string, std::vector<std::pair<std::string, std::variant<bool>>>,
-  // std::vector<std::string>>>(
-  //   {"test",std::vector<std::pair<std::string, std::variant<bool>>>{ std::pair<std::string, std::variant<bool>>{ "test",
-  //   true } }, std::vector<std::string>{ "test" }}
-  // );
-  // try{
-  //    connection->call(ucontainer);
-  // }catch (sdbusplus::exception::SdBusError& e){
-  // std::cout << ucontainer.get_signature() << " IS THIS THE REAL LIFE" << std::endl;
-  // }
-  ctx.run();
   auto screen = ftxui::ScreenInteractive::TerminalOutput();
 
   std::vector<std::string> left_menu_entries = {
@@ -123,7 +121,7 @@ auto main(int argc, char** argv) -> int {
   int right_menu_selected = 0;
 
   std::vector<std::string> services = { "hi" };
-  ftxui::Component right_menuz = right_menu(&right_menu_entries, &right_menu_selected);
+  ftxui::Component right_menuz = right_menu(&right_menu_selected);
   ftxui::Component left_menuz = left_menu(&services, &left_menu_selected, right_menuz);
 
   ftxui::Component container = ftxui::Container::Horizontal({ left_menuz, right_menuz });
@@ -150,6 +148,27 @@ auto main(int argc, char** argv) -> int {
            }) |
            ftxui::border | ftxui::flex;
   });
+  ftxui::Loop loop(&screen, renderer);
 
-  screen.Loop(renderer);
+  asio::io_context ctx{};
+  std::shared_ptr<sdbusplus::asio::connection> connection =
+      std::make_shared<sdbusplus::asio::connection>(ctx, tfc::dbus::sd_bus_open_system_mon());
+  // Attach callback
+  auto match = std::make_unique<sdbusplus::bus::match::match>(*connection, "", std::bind(match_callback, std::placeholders::_1, left_menuz, right_menuz));
+  // Become monitor method
+  auto mc = connection->new_method_call("org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus.Monitoring",
+                                        "BecomeMonitor");
+  mc.append<std::vector<std::string>, uint32_t>({ "path=/com/skaginn3x/Signals" }, 0);
+  auto reply = connection->call(mc);
+  if (reply.is_method_error()) {
+    if (reply.get_error() != nullptr)
+      throw std::runtime_error(reply.get_error()->message);
+    throw std::runtime_error("Unknown error");
+  }
+
+  // Run the loop until the screen quits
+  while (!loop.HasQuitted()) {
+    loop.RunOnce();
+    ctx.run_for(std::chrono::milliseconds(10));
+  }
 };
