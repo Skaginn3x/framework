f6c8a18ce2f5c37e498b41db39d49b079992408c
magni <magnimar@skaginn3x.com>
2023-12-07 11:55:28 +0000

    original config file

--- exes/mqtt-bridge/inc/config/broker.hpp
+++ exes/mqtt-bridge/inc/config/broker.hpp
@@ -3,10 +3,23 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <utility>
+#include <variant>
 
 #include <glaze/core/common.hpp>
 
 #include <structs.hpp>
+
+namespace tfc::mqtt::config {
+enum struct port_e : uint16_t { mqtt = 1883, mqtts = 8883 };
+}
+
+template <>
+struct glz::meta<tfc::mqtt::config::port_e> {
+  using enum tfc::mqtt::config::port_e;
+  static constexpr std::string_view name{ "tfc::mqtt::port_e" };
+  static constexpr auto value = glz::enumerate("mqtt", mqtt, "mqtts", mqtts);
+};
 
 template <>
 struct glz::meta<tfc::mqtt::structs::ssl_active_e> {
@@ -19,7 +32,7 @@ namespace tfc::mqtt::config {
 
 struct broker {
   std::string address{};
-  uint16_t port{};
+  std::variant<port_e, uint16_t> port{};
   structs::ssl_active_e ssl_active{};
   std::string username{};
   std::string password{};
@@ -27,19 +40,24 @@ struct broker {
 
   struct glaze {
     // clang-format off
-             static constexpr auto value{glz::object(
-                     "address", &broker::address, "Hostname or IP address of the MQTT broker",
-                     "port", &broker::port, "Port of the MQTT broker.",
-                     "ssl_active", &broker::ssl_active, "Whether or not to use SSL to connect to the MQTT broker",
-                     "username", &broker::username, "Username for the MQTT broker",
-                     "password", &broker::password, "Password for the MQTT broker",
-                     "client_id", &broker::client_id, "Client ID, used to identify which client is sending information"
-             )};
+            static constexpr auto value{glz::object(
+                    "address", &broker::address, "Hostname or IP address of the MQTT broker",
+                    "port", &broker::port,
+                    "Port of the MQTT broker. Possible values are: mqtt, mqtts or a custom port number",
+                    "ssl_active", &broker::ssl_active, "Whether or not to use SSL to connect to the MQTT broker",
+                    "username", &broker::username, "Username for the MQTT broker",
+                    "password", &broker::password, "Password for the MQTT broker",
+                    "client_id", &broker::client_id, "Client ID, used to identify which client is sending information"
+            )};
     // clang-format on
     static constexpr std::string_view name{ "broker" };
   };
 
-  // [[nodiscard]] auto get_port() const -> std::string { return std::to_string(static_cast<int>(port)); }
+  [[nodiscard]] auto get_port() const -> std::string {
+    return std::visit(
+        []<typename var>(var&& arg) { return std::to_string(static_cast<uint16_t>(std::forward<decltype(arg)>(arg))); },
+        port);
+  }
 };
 
 }  // namespace tfc::mqtt::config
