edd08e04175b6090ef5fe6db3f3db1a496281e9c
magni <magnimar@skaginn3x.com>
2023-12-12 16:51:32 +0000

    adding tests for all cases

--- libs/motor/tests/positioner_test.cpp
+++ libs/motor/tests/positioner_test.cpp
@@ -1,3 +1,4 @@
+#include <boost/asio.hpp>
 #include <boost/ut.hpp>
 #include <tfc/motors/positioner.hpp>
 #include <tfc/progbase.hpp>
@@ -18,6 +19,88 @@ int main(int argc, char** argv) {
     }
     expect(buff.front() == len + len - 1) << buff.front();
   };
+
+  //	new 	new	    old 	old
+  //	pin2	pin1	pin2	pin1	Result
+  //	----	----	----	----	------
+  //	0	0	1	1	+2  (assume pin1 edges only)
+  //	0	1	0	0	-1
+  //	0	1	0	1	no movement
+  //	0	1	1	0	-2  (assume pin1 edges only)
+  //	0	1	1	1	+1
+  //	1	0	0	0	+1
+  //	1	0	0	1	-2  (assume pin1 edges only)
+  //	1	0	1	0	no movement
+  //	1	0	1	1	-1
+  //	1	1	0	0	+2  (assume pin1 edges only)
+  //	1	1	0	1	-1
+  //	1	1	1	0	+1
+  //	1	1	1	1	no movement
+
+  //	new 	new	    old 	old
+  //	pin2	pin1	pin2	pin1	Result
+  //	----	----	----	----	------
+  //	0	0	0	0	no movement
+  "tachometer - no movement"_test = [] {
+    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+
+    expect(tacho.position_ == 0);
+
+    // No movement
+    tacho.first_tacho_update(false);
+    tacho.second_tacho_update(false);
+
+    expect(tacho.position_ == 0);
+  };
+
+  //	new 	new	    old 	old
+  //	pin2	pin1	pin2	pin1	Result
+  //	----	----	----	----	------
+  //	0	0	0	1	+1
+  "tachometer - +1"_test = [] {
+    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+
+    tacho.first_tacho_update(true);
+    tacho.first_tacho_update(false);
+
+    expect(tacho.position_ == 1);
+  };
+
+  //	new 	new	    old 	old
+  //	pin2	pin1	pin2	pin1	Result
+  //	----	----	----	----	------
+  //	0	0	1	0	-1
+
+  "tachometer - -1"_test = [] {
+    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == -1);
+
+    tacho.first_tacho_update(false);
+    tacho.second_tacho_update(false);
+
+    expect(tacho.position_ == -2);
+  };
+
+  //	new 	new	    old 	old
+  //	pin2	pin1	pin2	pin1	Result
+  //	----	----	----	----	------
+  //	0	0	1	1	+2  (assume pin1 edges only)
+  //  "tachometer - +2"_test = [] {
+  //    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+
+  //    tacho.first_tacho_update(true);
+  //    tacho.second_tacho_update(true);
+
+  //    expect(tacho.position_ == 2);
+
+  //    tacho.first_tacho_update(false);
+  //    tacho.second_tacho_update(false);
+
+  //    expect(tacho.position_ == 4);
+  //  };
 
   return EXIT_SUCCESS;
 }
