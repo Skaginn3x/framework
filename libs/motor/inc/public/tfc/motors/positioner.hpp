c1ef51a1f40013c1c6fae8a02c52db4ee097e7f0
magni <magnimar@skaginn3x.com>
2023-12-13 13:43:43 +0000

    refactor tachometer and positioner, along with tests

--- libs/motor/inc/public/tfc/motors/positioner.hpp
+++ libs/motor/inc/public/tfc/motors/positioner.hpp
@@ -6,13 +6,16 @@
 #include <type_traits>  // required by mp-units
 
 #include <fmt/format.h>
+#include <mp-units/quantity.h>
 #include <mp-units/systems/si/si.h>
+#include <mp-units/systems/si/unit_symbols.h>
 #include <boost/asio.hpp>
 #include <glaze/core/common.hpp>
 
 #include <tfc/confman.hpp>
 #include <tfc/ipc.hpp>
 #include <tfc/logger.hpp>
+#include <tfc/stx/concepts.hpp>
 #include <tfc/utils/units_glaze_meta.hpp>
 
 namespace tfc::motor::detail {
@@ -33,29 +36,28 @@ namespace tfc::motor {
 namespace asio = boost::asio;
 
 namespace detail {
-template <typename storage_t, std::size_t len>
-struct circular_buffer {
-  circular_buffer() = default;
-  template <typename... args_t>
-  constexpr auto emplace(args_t&&... args) {
-    front_ = insert_pos_;
-    std::construct_at(insert_pos_, std::forward<args_t>(args)...);
-    std::advance(insert_pos_, 1);
-    if (insert_pos_ == std::end(buffer_)) {
-      insert_pos_ = std::begin(buffer_);
+
+/// Tachometer with only one sensor, simply a counter upwards, direction cannot be determined
+struct single_tachometer {
+  single_tachometer() = default;
+  single_tachometer(std::function<void(std::int64_t)> position_update_callback) noexcept
+      : position_update_callback_{ std::move(position_update_callback) } {}
+
+  auto tacho_update(bool tacho_val) noexcept -> void {
+    if (tacho_val) {
+      position_ += 1;
+      if (position_update_callback_) {
+        (*position_update_callback_)(position_);
+      }
     }
   }
-  constexpr auto front() noexcept -> storage_t& { return *front_; }
-  constexpr auto front() const noexcept -> storage_t const& { return *front_; }
 
-  std::array<storage_t, len> buffer_{};
-  // front is invalid when there has no item been inserted yet, but should not matter much
-  typename std::array<storage_t, len>::iterator front_{ std::begin(buffer_) };
-  typename std::array<storage_t, len>::iterator insert_pos_{ std::begin(buffer_) };
+  std::int64_t position_{};
+  std::optional<std::function<void(std::int64_t)>> position_update_callback_;
 };
 
-template <typename time_point_t = asio::steady_timer::time_point, std::size_t circular_buffer_len = 1024>
-struct tachometer {
+/// Tachometer with two sensors, direction can be determined
+struct double_tachometer {
   // https://github.com/PaulStoffregen/Encoder/blob/master/Encoder.h#L303
   //                           _______         _______
   //               Pin1 ______|       |_______|       |______ Pin1
@@ -68,163 +70,193 @@ struct tachometer {
   //	0	0	0	0	no movement
   //	0	0	0	1	+1
   //	0	0	1	0	-1
-  //	0	0	1	1	+2  (assume pin1 edges only)
   //	0	1	0	0	-1
   //	0	1	0	1	no movement
-  //	0	1	1	0	-2  (assume pin1 edges only)
   //	0	1	1	1	+1
   //	1	0	0	0	+1
-  //	1	0	0	1	-2  (assume pin1 edges only)
   //	1	0	1	0	no movement
   //	1	0	1	1	-1
-  //	1	1	0	0	+2  (assume pin1 edges only)
   //	1	1	0	1	-1
   //	1	1	1	0	+1
   //	1	1	1	1	no movement
-  /*
-          // Simple, easy-to-read "documentation" version :-)
-          //
-          void update(void) {
-                  uint8_t s = state & 3;
-                  if (digitalRead(pin1)) s |= 4;
-                  if (digitalRead(pin2)) s |= 8;
-                  switch (s) {
-                          case 0: case 5: case 10: case 15:
-                                  break;
-                          case 1: case 7: case 8: case 14:
-                                  position++; break;
-                          case 2: case 4: case 11: case 13:
-                                  position--; break;
-                          case 3: case 12:
-                                  position += 2; break;
-                          default:
-                                  position -= 2; break;
-                  }
-                  state = (s >> 2);
-          }
-  */
 
-  void first_tacho_update(bool first_new_val) noexcept {
-    auto now{ time_point_t::clock::now() };
-    buffer_.emplace(first_new_val, buffer_.front().second_tacho_state, now);
-    if (!first_new_val) {
-      // let's only count rising edges
-      return;
+  double_tachometer() = default;
+  double_tachometer(std::function<void(std::int64_t)> position_update_callback) noexcept
+      : position_update_callback_{ std::move(position_update_callback) } {}
+
+  auto first_tacho_update(bool first_new_val) noexcept -> void {
+    if (first_tacho) {
+      if (second_tacho) {
+        if (!first_new_val) {
+          decrement();
+        }
+      } else {
+        if (!first_new_val) {
+          increment();
+        }
+      }
+    } else {
+      if (second_tacho) {
+        if (first_new_val) {
+          increment();
+        }
+      } else {
+        if (first_new_val) {
+          decrement();
+        }
+      }
     }
-    position_ += (buffer_.front().second_tacho_state ? -1 : 1);
-    // asio::post call owner with updated position
-  }
-  void second_tacho_update(bool second_new_val) noexcept {
-    auto now{ time_point_t::clock::now() };
-    buffer_.emplace(buffer_.front().first_tacho_state, second_new_val, now);
-    if (!second_new_val) {
-      // let's only count rising edges
-      return;
-    }
-    // Needs testing !!!
-    position_ += (buffer_.front().first_tacho_state ? 1 : -1);
-    // asio::post call owner with updated position
+
+    first_tacho = first_new_val;
   }
 
-  struct storage {
-    bool first_tacho_state{};
-    bool second_tacho_state{};
-    time_point_t time_point{};
-  };
-  circular_buffer<storage, circular_buffer_len> buffer_{};
+  auto second_tacho_update(bool second_new_val) noexcept -> void {
+    if (first_tacho) {
+      if (second_tacho) {
+        if (!second_new_val) {
+          increment();
+        }
+      } else {
+        if (second_new_val) {
+          decrement();
+        }
+      }
+    } else {
+      if (!second_tacho) {
+        if (second_new_val) {
+          increment();
+        }
+      } else {
+        if (!second_new_val) {
+          decrement();
+        }
+      }
+    }
+
+    second_tacho = second_new_val;
+  }
+
+  auto increment() noexcept -> void {
+    position_ += 1;
+    if (position_update_callback_) {
+      (*position_update_callback_)(position_);
+    }
+  }
+
+  auto decrement() noexcept -> void {
+    position_ -= 1;
+    if (position_update_callback_) {
+      (*position_update_callback_)(position_);
+    }
+  }
+
+  bool first_tacho{};
+  bool second_tacho{};
   std::int64_t position_{};
+  std::optional<std::function<void(std::int64_t)>> position_update_callback_;
 };
 
 }  // namespace detail
 
-template <mp_units::Quantity dimension_t = mp_units::quantity<mp_units::si::milli<mp_units::si::metre>, std::int64_t>>
-class positioner {
-public:
-  static constexpr auto reference{ dimension_t::reference };
-  struct config {
-    detail::tacho_config tacho{ detail::tacho_config::not_used };
-    dimension_t
-        displacement_per_pulse{};  // I would suggest default value as 2.54 cm (one inch) and ban 0 and negative values
-    struct glaze {
-      static constexpr std::string_view name{ "positioner_config" };
-      static constexpr auto value{
-        glz::object("tacho", &config::tacho, "displacement_per_tacho_pulse", &config::displacement_per_pulse)
-      };
+using namespace mp_units;
+using mp_units::si::unit_symbols::mm;
+
+struct config {
+  detail::tacho_config tacho{ detail::tacho_config::not_used };
+  // I would suggest default value as 2.54 cm (one inch) and ban 0 and negative values
+  quantity<mm> displacement_per_pulse{};
+  struct glaze {
+    static constexpr std::string_view name{ "positioner_config" };
+    static constexpr auto value{
+      glz::object("tacho", &config::tacho, "displacement_per_tacho_pulse", &config::displacement_per_pulse)
     };
   };
+};
 
-  positioner(asio::io_context& ctx, ipc_ruler::ipc_manager_client& client, std::string_view name)
+template <class ipc_client_t = ipc_ruler::ipc_manager_client&, class config_t = tfc::confman::config<config>>
+class positioner {
+public:
+  positioner(asio::io_context& ctx, ipc_client_t client, std::string_view name)
       : name_{ name }, ctx_{ ctx }, client_{ client } {
     switch (config_->tacho) {
       using enum detail::tacho_config;
       case not_used:
         break;
       case one_tacho: {
-        tacho_.emplace();
+        single_tacho_.emplace([this](std::int64_t counts) { this->tick(counts); });
         tacho_a.emplace(ctx_, client_, fmt::format("tacho_{}", name_),
-                        "Tachometer input, usually induction sensor directed to rotational metal star og ring of screws.",
-                        [this](bool val) { this->tacho_->first_tacho_update(val); });
+                        "Tachometer input, usually induction sensor directed to rotational metal star or ring of screws.",
+                        [this](bool val) { this->single_tacho_->tacho_update(val); });
         break;
       }
       case two_tacho: {
-        tacho_.emplace();
+        double_tacho_.emplace([this](std::int64_t counts) { this->tick(counts); });
         tacho_a.emplace(ctx_, client_, fmt::format("tacho_a_{}", name_),
                         "First input of tachometer, with two sensors, usually induction sensor directed to rotational metal "
                         "star og ring of screws.",
-                        [this](bool val) { this->tacho_->first_tacho_update(val); });
+                        [this](bool val) { this->double_tacho_->first_tacho_update(val); });
         tacho_b.emplace(ctx_, client_, fmt::format("tacho_b_{}", name_),
                         "Second input of tachometer, with two sensors, usually induction sensor directed to rotational "
                         "metal star og ring of screws.",
-                        [this](bool val) { this->tacho_->second_tacho_update(val); });
+                        [this](bool val) { this->double_tacho_->second_tacho_update(val); });
         break;
       }
     }
   }
-  void tick(std::int64_t tachometer_counts) {
-    // tachometer should call this function every time it increments/decrements its position
-    // this function should not do much work
+
+  /// Tachometer should call this function every time it increments/decrements its position
+  auto tick(std::int64_t tachometer_counts) -> void {
     absolute_position_ = config_->displacement_per_pulse * tachometer_counts;
-    if (notifications_.empty()) {
-      return;
-    }
-    // recursive call to notifications to propage events
-    // need to subtract notifications_.front().absolute_notification_position_ from absolute_position_
-    // and compare to displacement_per_pulse, if it is in range, call the notification
-    // or better yet make a timer and call the notification when timer expires
+    notify();
   }
 
-  struct notification {
-    dimension_t absolute_notification_position_{};
-  };
+  /// Check if any notifiers need to be notified
+  auto notify() -> void {
+    while (true) {
+      if (notifications_.empty()) {
+        break;
+      }
+      auto& notification = notifications_.front();
+      if (absolute_position_ >= notification.first) {
+        notification.second(absolute_position_);
+        notifications_.erase(notifications_.begin());
+        std::sort(notifications_.begin(), notifications_.end(), sort_notifications);
+      } else {
+        break;
+      }
+    }
+  }
 
-  auto notify_after([[maybe_unused]] dimension_t displacement, asio::completion_token_for<void()> auto&& token)
-      -> decltype(auto) {
-    // still yet to be implemented
-    // I would suggest making a struct instead of the lambda below, to store extra stuff if needed
-    // needs to be embedded into the notifications vector so the vector can complete the async operation
-    notifications_.emplace_back({ displacement + absolute_position_ });  // todo
-    // std::sort(notifications_); // sort by absolute position
-    return asio::async_compose<decltype(token), void()>([](auto& self) {}, std::forward<decltype(token)>(token), ctx_);
+  static auto sort_notifications(std::pair<quantity<mm>, std::function<void(quantity<mm>)>>& a,
+                                 std::pair<quantity<mm>, std::function<void(quantity<mm>)>>& b) -> bool {
+    return a.first < b.first;
+  }
+
+  /// Place notifier on the list of notifiers, which will be notified when the positioner has moved the specified distance
+  auto notify_after(quantity<mm> displacement, std::function<void(quantity<mm>)>& callback) -> void {
+    notifications_.emplace_back(displacement + absolute_position_, callback);
+    std::sort(notifications_.begin(), notifications_.end(), sort_notifications);
   }
 
   // Might be useful for homing sequences to grab the current position
+  [[nodiscard]] quantity<mm> position() const noexcept { return absolute_position_; }
   // Please note the resolution
-  [[nodiscard]] dimension_t position() const noexcept { return absolute_position_; }
-  [[nodiscard]] dimension_t resolution() const noexcept { return config_->displacement_per_pulse; }
+  [[nodiscard]] quantity<mm> resolution() const noexcept { return config_->displacement_per_pulse; }
 
 private:
   std::string name_;
   asio::io_context& ctx_;
-  ipc_ruler::ipc_manager_client& client_;
-  logger::logger logger_{ name_ };
-  confman::config<config> config_{ ctx_, fmt::format("positioner_{}", name_) };
-  std::optional<detail::tachometer<>> tacho_{};
-  std::optional<ipc::bool_slot> tacho_a{};
-  std::optional<ipc::bool_slot> tacho_b{};
+  ipc_client_t& client_;
+  // logger::logger logger_{ name_ };
+  config_t config_{ ctx_, fmt::format("positioner_{}", name_) };
+  std::optional<detail::single_tachometer> single_tacho_{};
+  std::optional<detail::double_tachometer> double_tacho_{};
+  std::optional<ipc::slot<ipc::details::type_bool, ipc_client_t&>> tacho_a{};
+  std::optional<ipc::slot<ipc::details::type_bool, ipc_client_t&>> tacho_b{};
   // todo overflow
   // in terms of conveyors, mm resolution, this would overflow when you have gone 1800 trips to Pluto back and forth
-  dimension_t absolute_position_{};
-  std::vector<notification> notifications_{};
+  quantity<mm> absolute_position_{};
+  std::vector<std::pair<quantity<mm>, std::function<void(quantity<mm>)>>> notifications_;
 };
 
 }  // namespace tfc::motor
--- libs/motor/tests/positioner_test.cpp
+++ libs/motor/tests/positioner_test.cpp
@@ -1,48 +1,68 @@
+#include <iostream>
+
 #include <boost/asio.hpp>
 #include <boost/ut.hpp>
 #include <tfc/motors/positioner.hpp>
 #include <tfc/progbase.hpp>
 
-using namespace mp_units::si::unit_symbols;
+#include <tfc/ipc/details/dbus_client_iface_mock.hpp>
+
+#include <mp-units/systems/si/si.h>
+
+using mp_units::quantity;
+using mp_units::si::unit_symbols::mm;
+using tfc::motor::detail::tacho_config;
+
+namespace asio = boost::asio;
+
+class single_tacho_config_mock {
+public:
+  single_tacho_config_mock(asio::io_context&, std::string_view) {}
+
+  tacho_config tacho{ tacho_config::one_tacho };
+  quantity<mm> displacement_per_pulse{ 1 * mm };
+
+  // Overload the dereference operator
+  single_tacho_config_mock* operator->() { return this; }
+};
+
+class double_tacho_config_mock {
+public:
+  double_tacho_config_mock(asio::io_context&, std::string_view) {}
+
+  tacho_config tacho{ tacho_config::two_tacho };
+  quantity<mm> displacement_per_pulse{ 1 * mm };
+
+  // Overload the dereference operator
+  double_tacho_config_mock* operator->() { return this; }
+};
 
 int main(int argc, char** argv) {
   using boost::ut::operator""_test;
   using boost::ut::expect;
-  using tfc::motor::detail::circular_buffer;
   tfc::base::init(argc, argv);
 
-  "circular_buffer_test moves pointer front when inserted to last item"_test = [] {
-    static constexpr std::size_t len{ 10 };
-    circular_buffer<int, 10> buff{};
-    for (std::size_t i = 0; i < len + len; ++i) {  // try two rounds
-      buff.emplace(i);
-    }
-    expect(buff.front() == len + len - 1) << buff.front();
-  };
+  /// These are the tests for the tachometer
+  /// Only one value can be updated at a time
+  ///	new	new	old	old
+  ///	pin1    pin2    pin1    pin2    Result
+  ///	----	----	----	----	------
+  ///	0       0	0       0	no movement
+  ///	1       0	0       0	-1
+  ///	0       1	0       0	+1
+  ///	0       0	1       0	+1
+  ///	0       0	0       1	-1
+  ///	1       0	1       0	no movement
+  ///	1       0	1       1	+1
+  ///	0       1	0       1	no movement
+  ///	0       1	1       1	-1
+  ///	1       1	1       0	-1
+  ///	1       1	0       1	+1
+  ///	1       1	1       1	no movement
 
-  //	new 	new	    old 	old
-  //	pin2	pin1	pin2	pin1	Result
-  //	----	----	----	----	------
-  //	0	0	1	1	+2  (assume pin1 edges only)
-  //	0	1	0	0	-1
-  //	0	1	0	1	no movement
-  //	0	1	1	0	-2  (assume pin1 edges only)
-  //	0	1	1	1	+1
-  //	1	0	0	0	+1
-  //	1	0	0	1	-2  (assume pin1 edges only)
-  //	1	0	1	0	no movement
-  //	1	0	1	1	-1
-  //	1	1	0	0	+2  (assume pin1 edges only)
-  //	1	1	0	1	-1
-  //	1	1	1	0	+1
-  //	1	1	1	1	no movement
-
-  //	new 	new	    old 	old
-  //	pin2	pin1	pin2	pin1	Result
-  //	----	----	----	----	------
-  //	0	0	0	0	no movement
-  "tachometer - no movement"_test = [] {
-    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+  ///	0   0   |   0   0   ->  no movement
+  "tachometer: 0"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
 
     expect(tacho.position_ == 0);
 
@@ -53,54 +73,290 @@ int main(int argc, char** argv) {
     expect(tacho.position_ == 0);
   };
 
-  //	new 	new	    old 	old
-  //	pin2	pin1	pin2	pin1	Result
-  //	----	----	----	----	------
-  //	0	0	0	1	+1
-  "tachometer - +1"_test = [] {
-    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+  ///	0   1   |   0   0   ->  -1
+  "tachometer: -1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
 
     tacho.first_tacho_update(true);
-    tacho.first_tacho_update(false);
+
+    expect(tacho.position_ == -1);
+  };
+
+  ///	1   0   |   0   0   ->   +1
+  "tachometer: +1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.second_tacho_update(true);
 
     expect(tacho.position_ == 1);
   };
 
-  //	new 	new	    old 	old
-  //	pin2	pin1	pin2	pin1	Result
-  //	----	----	----	----	------
-  //	0	0	1	0	-1
+  ///	0   0   |   0   1   ->   +1
+  "tachometer: +1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
 
-  "tachometer - -1"_test = [] {
-    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
-
-    tacho.second_tacho_update(true);
+    tacho.first_tacho_update(true);
 
     expect(tacho.position_ == -1);
 
     tacho.first_tacho_update(false);
-    tacho.second_tacho_update(false);
 
-    expect(tacho.position_ == -2);
+    expect(tacho.position_ == 0);
   };
 
-  //	new 	new	    old 	old
-  //	pin2	pin1	pin2	pin1	Result
-  //	----	----	----	----	------
-  //	0	0	1	1	+2  (assume pin1 edges only)
-  //  "tachometer - +2"_test = [] {
-  //    tfc::motor::detail::tachometer<boost::asio::steady_timer::time_point, 1024> tacho{};
+  ///	0   0   |   1   0   ->   -1
+  "tachometer: -1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
 
-  //    tacho.first_tacho_update(true);
-  //    tacho.second_tacho_update(true);
+    tacho.second_tacho_update(true);
 
-  //    expect(tacho.position_ == 2);
+    expect(tacho.position_ == 1);
 
-  //    tacho.first_tacho_update(false);
-  //    tacho.second_tacho_update(false);
+    tacho.second_tacho_update(false);
 
-  //    expect(tacho.position_ == 4);
-  //  };
+    expect(tacho.position_ == 0);
+  };
+
+  ///	0   1   |   0   1   ->   no movement
+  "tachometer: 0"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.first_tacho_update(true);
+
+    expect(tacho.position_ == -1);
+
+    tacho.first_tacho_update(true);
+
+    expect(tacho.position_ == -1);
+  };
+
+  ///	0   1   |   1   1   ->   +1
+  "tachometer: +1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.first_tacho_update(true);
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == -2) << tacho.position_;
+
+    tacho.second_tacho_update(false);
+    expect(tacho.position_ == -1);
+  };
+
+  ///	1   0   |   1   0   ->   no movement
+  "tachometer: 0"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == 1);
+
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == 1);
+  };
+
+  ///	1   0   |   1   1   ->   -1
+  "tachometer: -1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.first_tacho_update(true);
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == -2) << tacho.position_;
+
+    tacho.first_tacho_update(false);
+
+    expect(tacho.position_ == -3);
+  };
+
+  ///   1   1   |   0   1   ->   -1
+  "tachometer: -1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.first_tacho_update(true);
+
+    expect(tacho.position_ == -1) << tacho.position_;
+
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == -2) << tacho.position_;
+  };
+
+  ///   1   1   |   1   0   ->   +1
+  "tachometer: +1"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == 1) << tacho.position_;
+
+    tacho.first_tacho_update(true);
+
+    expect(tacho.position_ == 2) << tacho.position_;
+  };
+
+  ///   1   1   |   1   1   ->   no movement
+  "tachometer: 0"_test = [] {
+    tfc::motor::detail::double_tachometer tacho{};
+
+    tacho.first_tacho_update(true);
+    tacho.second_tacho_update(true);
+
+    expect(tacho.position_ == -2) << tacho.position_;
+
+    tacho.first_tacho_update(true);
+    tacho.second_tacho_update(true);
+    expect(tacho.position_ == -2) << tacho.position_;
+  };
+
+  "test callback function"_test = [] {
+    std::int64_t callback_counter = 0;
+
+    std::function<void(std::int64_t)> callback{ [&callback_counter](std::int64_t) { callback_counter++; } };
+
+    tfc::motor::detail::double_tachometer tacho{ callback };
+
+    tacho.first_tacho_update(true);
+
+    expect(callback_counter == 1);
+
+    tacho.second_tacho_update(true);
+
+    expect(callback_counter == 2);
+  };
+
+  "test positioner"_test = [] {
+    using mp_units::si::unit_symbols::mm;
+
+    asio::io_context io_context{};
+    tfc::ipc_ruler::ipc_manager_client_mock ipc_manager_client{ io_context };
+    tfc::motor::positioner<tfc::ipc_ruler::ipc_manager_client_mock&, single_tacho_config_mock> positioner{
+      io_context, ipc_manager_client, "positioner_name"
+    };
+
+    expect(positioner.position() == 0 * mm);
+
+    tfc::ipc::signal<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> signal{ io_context,
+                                                                                                     ipc_manager_client,
+                                                                                                     "tacho_signal" };
+
+    ipc_manager_client.connect(ipc_manager_client.slots_[0].name, signal.full_name(), [](std::error_code) {});
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    signal.send(true);
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 1 * mm) << positioner.position().numerical_value_;
+
+    signal.send(false);
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 1 * mm) << positioner.position().numerical_value_;
+
+    signal.send(true);
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 2 * mm) << positioner.position().numerical_value_;
+  };
+
+  "test notifications and single tacho"_test = [] {
+    using mp_units::si::unit_symbols::mm;
+    using mp_units::quantity;
+
+    asio::io_context io_context{};
+    tfc::ipc_ruler::ipc_manager_client_mock ipc_manager_client{ io_context };
+
+    tfc::motor::positioner<tfc::ipc_ruler::ipc_manager_client_mock&, single_tacho_config_mock> positioner{
+      io_context, ipc_manager_client, "positioner_name"
+    };
+
+    std::int64_t callback_counter = 0;
+
+    std::function<void(quantity<mm>)> callback{ [&callback_counter](quantity<mm>) { callback_counter++; } };
+
+    quantity<mm> position_1mm{ 1 * mm };
+    quantity<mm> position_2mm{ 2 * mm };
+    quantity<mm> position_3mm{ 3 * mm };
+
+    positioner.notify_after(position_3mm, callback);
+    positioner.notify_after(position_2mm, callback);
+    positioner.notify_after(position_1mm, callback);
+
+    expect(positioner.position() == 0 * mm);
+
+    tfc::ipc::signal<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> signal{ io_context,
+                                                                                                     ipc_manager_client,
+                                                                                                     "tacho_signal" };
+
+    ipc_manager_client.connect(ipc_manager_client.slots_[0].name, signal.full_name(), [](std::error_code) {});
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    signal.send(true);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 1 * mm) << positioner.position().numerical_value_;
+    expect(callback_counter == 1);
+
+    signal.send(false);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    signal.send(true);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 2 * mm) << positioner.position().numerical_value_;
+    expect(callback_counter == 2);
+
+    signal.send(false);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    signal.send(true);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 3 * mm) << positioner.position().numerical_value_;
+    expect(callback_counter == 3);
+  };
+
+  "test double tacho"_test = [] {
+    using mp_units::si::unit_symbols::mm;
+    using mp_units::quantity;
+
+    asio::io_context io_context{};
+    tfc::ipc_ruler::ipc_manager_client_mock ipc_manager_client{ io_context };
+
+    tfc::motor::positioner<tfc::ipc_ruler::ipc_manager_client_mock&, double_tacho_config_mock> positioner{
+      io_context, ipc_manager_client, "positioner_name"
+    };
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == 0 * mm);
+
+    tfc::ipc::signal<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> signal_a{ io_context,
+                                                                                                       ipc_manager_client,
+                                                                                                       "tacho_signal_a" };
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    tfc::ipc::signal<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> signal_b{ io_context,
+                                                                                                       ipc_manager_client,
+                                                                                                       "tacho_signal_b" };
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    ipc_manager_client.connect(ipc_manager_client.slots_[0].name, signal_a.full_name(), [](std::error_code) {});
+    ipc_manager_client.connect(ipc_manager_client.slots_[1].name, signal_b.full_name(), [](std::error_code) {});
+
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    signal_a.send(true);
+    io_context.run_for(std::chrono::milliseconds(100));
+
+    expect(positioner.position() == -1 * mm) << positioner.position().numerical_value_;
+  };
 
   return EXIT_SUCCESS;
 }
