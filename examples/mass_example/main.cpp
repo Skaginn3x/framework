af173febb747033c51fa7907423c2120ad214bc5
magni <magnimar@skaginn3x.com>
2023-12-18 16:36:13 +0000

    mass example

--- examples/mass_example/main.cpp
+++ examples/mass_example/main.cpp
@@ -17,52 +17,34 @@ int main(int argc, char** argv) {
   auto ctx{ asio::io_context() };
   tfc::ipc_ruler::ipc_manager_client_mock ipc_client_{ ctx };
 
- //   tfc::ipc::signal<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> weight_signal{
- //     ctx,
- //     ipc_client_,
- //     "weight",
- //   };
+  tfc::ipc::signal<tfc::ipc::details::type_mass, tfc::ipc_ruler::ipc_manager_client_mock&> weight_signal{
+    ctx,
+    ipc_client_,
+    "weight",
+  };
 
- //   tfc::ipc::slot<tfc::ipc::details::type_bool, tfc::ipc_ruler::ipc_manager_client_mock&> weight_slot{ ctx, ipc_client_,
- //                                                                                                       "weight",
- //                                                                                                       [](bool) {} };
+  tfc::ipc::slot<tfc::ipc::details::type_mass, tfc::ipc_ruler::ipc_manager_client_mock&> weight_slot{
+    ctx, ipc_client_, "weight", [](const tfc::ipc::details::mass_t&) {}
+  };
 
- //   ipc_client_.connect(ipc_client_.slots_[0].name, ipc_client_.signals_[0].name, [](std::error_code const&) {});
+  ipc_client_.connect(ipc_client_.slots_[0].name, ipc_client_.signals_[0].name, [](std::error_code const&) {});
 
- //   weight_signal.send(true);
+  weight_signal.send(100 * mp_units::si::gram);
 
- //   ctx.run_for(std::chrono::milliseconds(10));
+  ctx.run_for(std::chrono::milliseconds(10));
 
- //   std::cout << weight_slot.value().value() << std::endl;
+  auto check_value = [&weight_slot]() -> bool {
+    // check if std::optional contains value
+    if (weight_slot.value().has_value()) {
+      // check if std::expected contains value
+      if (weight_slot.value()->has_value()) {
+        return weight_slot.value()->value() == 100 * mp_units::si::gram;
+      }
+    }
+    return false;
+  };
 
-      tfc::ipc::signal<tfc::ipc::details::type_mass, tfc::ipc_ruler::ipc_manager_client_mock&> weight_signal{
-        ctx,
-        ipc_client_,
-        "weight",
-      };
-
-      tfc::ipc::slot<tfc::ipc::details::type_mass, tfc::ipc_ruler::ipc_manager_client_mock&> weight_slot{
-        ctx, ipc_client_, "weight", [](const tfc::ipc::details::mass_t&) {}
-      };
-
-      ipc_client_.connect(ipc_client_.slots_[0].name, ipc_client_.signals_[0].name, [](std::error_code const&) {});
-
-      weight_signal.send(100 * mp_units::si::gram);
-
-      ctx.run_for(std::chrono::milliseconds(10));
-
-      auto check_value = [&weight_slot]() -> bool {
-        // check if std::optional contains value
-        if (weight_slot.value().has_value()) {
-          // check if std::expected contains value
-          if (weight_slot.value()->has_value()) {
-            return weight_slot.value()->value() == 100 * mp_units::si::gram;
-          }
-        }
-        return false;
-      };
-
-   std::cout << "Value arrived on slot: " << (check_value() ? "true" : "false") << std::endl;
+  std::cout << "Value arrived on slot: " << (check_value() ? "true" : "false") << std::endl;
 
   return EXIT_SUCCESS;
 }
