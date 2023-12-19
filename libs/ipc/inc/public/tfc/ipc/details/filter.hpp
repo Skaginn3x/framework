a8a4a2f1f81089daa3f74b253d8150ebe358d707
magni <magnimar@skaginn3x.com>
2023-12-19 15:33:59 +0000

    add offset filter

--- libs/ipc/inc/public/tfc/ipc/details/filter.hpp
+++ libs/ipc/inc/public/tfc/ipc/details/filter.hpp
@@ -140,7 +140,10 @@ public:
 };
 
 template <typename value_t>
-  requires requires { requires(std::integral<value_t> || std::floating_point<value_t>) && !std::same_as<value_t, bool>; }
+  requires requires {
+    requires(std::integral<value_t> || std::floating_point<value_t> || std::is_same_v<ipc::details::mass_t, value_t>) &&
+                !std::same_as<value_t, bool>;
+  }
 struct filter<filter_e::offset, value_t> {
   value_t offset{};
   static constexpr filter_e type{ filter_e::offset };
@@ -150,7 +153,11 @@ struct filter<filter_e::offset, value_t> {
     auto exe = asio::get_associated_executor(completion_token);
     return asio::async_compose<decltype(completion_token), void(std::expected<value_t, std::error_code>)>(
         [this, copy = value](auto& self) {
-          self.complete(copy + offset);  //
+          if constexpr (std::is_same_v<value_t, ipc::details::mass_t>) {
+            self.complete(copy.value() + offset.value());  //
+          } else {
+            self.complete(copy + offset);                  //
+          }
         },
         completion_token, exe);
   }
@@ -251,7 +258,7 @@ struct any_filter_decl<std::string> {
 template <>
 struct any_filter_decl<ipc::details::mass_t> {
   using value_t = ipc::details::mass_t;
-  using type = std::variant<filter<filter_e::filter_out, value_t>>;
+  using type = std::variant<filter<filter_e::filter_out, value_t>, filter<filter_e::offset, value_t>>;
 };
 // json?
 template <typename value_t>
--- libs/ipc/testing/tests/filter_test.cpp
+++ libs/ipc/testing/tests/filter_test.cpp
@@ -91,8 +91,8 @@ auto main(int, char**) -> int {
           co_return;
         },
         asio::detached);
-    test.ctx.run_one_for(std::chrono::seconds{ 1 });  // co_spawn
-    test.ctx.run_one_for(std::chrono::seconds{ 1 });  // async_process
+    test.ctx.run_one_for(std::chrono::seconds{ 1 });    // co_spawn
+    test.ctx.run_one_for(std::chrono::seconds{ 1 });    // async_process
     tfc::testing::clock::set_ticks(tfc::testing::clock::now() + std::chrono::milliseconds{ 1 });
     test.ctx.run_for(std::chrono::milliseconds{ 50 });  // try polling for this given time
   };
@@ -145,8 +145,8 @@ auto main(int, char**) -> int {
           co_return;
         },
         asio::detached);
-    ctx.run_one_for(std::chrono::seconds{ 1 });  // co_spawn
-    ctx.run_one_for(std::chrono::seconds{ 1 });  // async process (async_compose)
+    ctx.run_one_for(std::chrono::seconds{ 1 });        // co_spawn
+    ctx.run_one_for(std::chrono::seconds{ 1 });        // async process (async_compose)
     tfc::testing::clock::set_ticks(tfc::testing::clock::now() + std::chrono::milliseconds{ 1 });
     ctx.run_one_for(std::chrono::milliseconds{ 30 });  // poll timer once more, but nothing happens
     // down to business, 1 millisecond has elapsed since the timer was made with 10 millisec wait let's change the value
@@ -235,6 +235,24 @@ auto main(int, char**) -> int {
           return_value = co_await filter_out_test.async_process(40 * mp_units::si::gram, asio::use_awaitable);
           expect(return_value.has_value());
           expect(return_value.value().value() == 40 * mp_units::si::gram);
+          co_return;
+        },
+        asio::detached);
+    ctx.run_one_for(std::chrono::seconds{ 1 });
+  };
+
+  "mass filter offset"_test = []() {
+    asio::io_context ctx{};
+    asio::co_spawn(
+        ctx,
+        []() -> asio::awaitable<void> {
+          filter<filter_e::offset, tfc::ipc::details::mass_t> filter_offset_test{ .offset = 100 * mp_units::si::gram };
+          auto return_value = co_await filter_offset_test.async_process(100 * mp_units::si::gram, asio::use_awaitable);
+          expect(return_value.has_value());
+          expect(return_value.value().value() == 200 * mp_units::si::gram);
+          return_value = co_await filter_offset_test.async_process(40 * mp_units::si::gram, asio::use_awaitable);
+          expect(return_value.has_value());
+          expect(return_value.value().value() == 140 * mp_units::si::gram);
           co_return;
         },
         asio::detached);
