f5e4878cdb5e904686d1061be474eccd8e1fbc9b
magni <magnimar@skaginn3x.com>
2023-12-21 16:05:23 +0000

    change tests in accordance

--- exes/mqtt-bridge/inc/signal_names.hpp
+++ exes/mqtt-bridge/inc/signal_names.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <string>
 #include <vector>
 
 #include <tfc/ipc.hpp>
--- exes/mqtt-bridge/tests/inc/config/publish_signals_mock.hpp
+++ exes/mqtt-bridge/tests/inc/config/publish_signals_mock.hpp
@@ -9,8 +9,12 @@ namespace tfc::mqtt::config {
 
 namespace asio = boost::asio;
 
+struct sig_name {
+  std::string value{};
+};
+
 struct publish_signals_owner_mock {
-  std::vector<std::string> publish_signals{};
+  std::vector<sig_name> publish_signals{};
 };
 
 class publish_signals_mock {
@@ -18,7 +22,8 @@ class publish_signals_mock {
 
 public:
   publish_signals_mock(asio::io_context const&, std::string const&) {
-    owner_.publish_signals = { "test_mqtt_bridge.def.bool.bool_signal", "second", "third" };
+    owner_.publish_signals = { sig_name{ "test_mqtt_bridge.def.bool.bool_signal" }, sig_name{ "second" },
+                               sig_name{ "third" } };
   }
 
   [[nodiscard]] auto value() const -> publish_signals_owner_mock const& { return owner_; }
