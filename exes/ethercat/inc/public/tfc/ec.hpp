5a1c9ee88af771885b9b87493d382bf4d0910ca0
magni-mar <magni-mar@users.noreply.github.com>
2023-12-27 16:53:43 +0000

    :art: Committing clang-format changes

--- exes/ethercat/inc/public/tfc/ec.hpp
+++ exes/ethercat/inc/public/tfc/ec.hpp
@@ -10,9 +10,9 @@
 #include <tfc/confman.hpp>
 #include <tfc/dbus/sd_bus.hpp>
 #include <tfc/dbus/string_maker.hpp>
+#include <tfc/ec/config/ethercat.hpp>
 #include <tfc/ec/devices/device.hpp>
 #include <tfc/ec/soem_interface.hpp>
-#include <tfc/ec/config/ethercat.hpp>
 
 namespace tfc::ec {
 using std::chrono::duration;
