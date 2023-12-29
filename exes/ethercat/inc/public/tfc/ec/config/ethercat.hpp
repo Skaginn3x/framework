ab0906551addbacc251ab193d53445b7a986f2f8
magni-mar <magni-mar@users.noreply.github.com>
2023-12-29 14:01:02 +0000

    :art: Committing clang-format changes

--- exes/ethercat/inc/public/tfc/ec/config/ethercat.hpp
+++ exes/ethercat/inc/public/tfc/ec/config/ethercat.hpp
@@ -13,46 +13,45 @@
 
 namespace tfc::ec::config {
 
-    struct network_interface {
-        std::string value{};
+struct network_interface {
+  std::string value{};
 
-        struct glaze {
-            static constexpr auto value = &network_interface::value;
-            static constexpr std::string_view name{"network_interface"};
-        };
-    };
+  struct glaze {
+    static constexpr auto value = &network_interface::value;
+    static constexpr std::string_view name{ "network_interface" };
+  };
+};
 
 }  // namespace tfc::ec::config
 
-template<>
+template <>
 struct tfc::json::detail::to_json_schema<tfc::ec::config::network_interface> {
-    template<auto Opts>
-    static void op(auto &s, auto &) noexcept {
-        s.oneOf = std::vector<tfc::json::detail::schematic>{};
+  template <auto Opts>
+  static void op(auto& s, auto&) noexcept {
+    s.oneOf = std::vector<tfc::json::detail::schematic>{};
 
-        // clang-format off
+    // clang-format off
         PRAGMA_CLANG_WARNING_PUSH_OFF(-Wexit-time-destructors)
         PRAGMA_CLANG_WARNING_PUSH_OFF(-Wglobal-constructors)
         thread_local auto interfaces{tfc::global::get_interfaces()};
         PRAGMA_CLANG_WARNING_POP
         PRAGMA_CLANG_WARNING_POP
-        // clang-format on
+    // clang-format on
 
-        for (auto const &interface: interfaces) {
-            s.oneOf->emplace_back(tfc::json::detail::schematic{
-                    .attributes{
-                            tfc::json::schema{.title = interface, .description = interface, .constant = interface}}});
-        }
+    for (auto const& interface : interfaces) {
+      s.oneOf->emplace_back(tfc::json::detail::schematic{
+          .attributes{ tfc::json::schema{ .title = interface, .description = interface, .constant = interface } } });
     }
+  }
 };
 
 namespace tfc::ec::config {
-    struct bus {
-        network_interface primary_interface{tfc::global::get_interfaces()[0]};
-        struct glaze {
-            static constexpr auto value{glz::object("primary_interface", &bus::primary_interface, "Primary interface")};
-            static constexpr std::string_view name{"config::bus"};
-        };
-    };
+struct bus {
+  network_interface primary_interface{ tfc::global::get_interfaces()[0] };
+  struct glaze {
+    static constexpr auto value{ glz::object("primary_interface", &bus::primary_interface, "Primary interface") };
+    static constexpr std::string_view name{ "config::bus" };
+  };
+};
 
 }  // namespace tfc::ec::config
