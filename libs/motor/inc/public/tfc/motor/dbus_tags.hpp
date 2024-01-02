f8dffdbcaa8a31dbc6bd14ee14b66c4ec0b91f3a
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-01-02 12:58:32 +0000

    Update libs/motor/inc/public/tfc/motor/dbus_tags.hpp

--- libs/motor/inc/public/tfc/motor/dbus_tags.hpp
+++ libs/motor/inc/public/tfc/motor/dbus_tags.hpp
@@ -16,7 +16,7 @@ static constexpr std::string_view service{ "Ethercat" };  // needs to match the 
 static constexpr std::string_view service_name{ tfc::dbus::const_dbus_name<detail::service> };
 static constexpr std::string_view path{ tfc::dbus::const_dbus_path<detail::path_postfix> };
 
-static auto make_interface_name(std::string_view implementation_name, std::uint16_t slave_id) -> std::string {
+static inline auto make_interface_name(std::string_view implementation_name, std::uint16_t slave_id) -> std::string {
   return tfc::dbus::make_dbus_name(fmt::format("{}_{}", implementation_name, slave_id));
 }
 
