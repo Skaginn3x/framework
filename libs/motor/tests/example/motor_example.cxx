2263de64d13b3a15949a43162f54c1ac86195862
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-01-03 21:50:04 +0000

    comment out unimplemented api call in libmotor

--- libs/motor/tests/example/motor_example.cxx
+++ libs/motor/tests/example/motor_example.cxx
@@ -48,7 +48,7 @@ auto main(int argc, char** argv) -> int {
   my_motor.stop();   // Stop freewheel
 
   // Absolute positioning relative to home position
-  my_motor.move(10 * mp_units::percent, 10 * m, [](auto){});
+  // my_motor.move(10 * mp_units::percent, 10 * m, [](auto){});
   my_motor.move(10 * m, [](const std::error_code&) {});
   my_motor.move_home([](const std::error_code&) {});
 
