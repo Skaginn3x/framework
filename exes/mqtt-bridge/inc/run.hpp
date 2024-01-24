5dfb4b945b282125f18b122bf8d90c40fe692dd2
magni <magnimar@skaginn3x.com>
2024-01-24 13:30:12 +0000

    basics for a broker running

---
+++ exes/mqtt-bridge/inc/run.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <boost/asio.hpp>
+
+#include <tfc/logger.hpp>
+
+#include <spark_plug_interface.hpp>
+#include <tfc_to_external.hpp>
+#include <external_to_tfc.hpp>
+
+namespace asio = boost::asio;
+
+namespace tfc::mqtt {
+
+    class run {
+
+    public:
+
+        explicit run(asio::io_context &io_ctx_) : io_ctx_(io_ctx_) {}
+
+        auto start() -> asio::awaitable<void> {
+            while (true) {
+                logger.trace("----------------------------------------------------------------------------");
+                logger.trace("Event loop started");
+
+                tfc::mqtt::spark_plug sp_interface{io_ctx_};
+
+                tfc::mqtt::tfc_to_ext tfc_to_ext{io_ctx_, sp_interface};
+
+                tfc::mqtt::ext_to_tfc ext_to_tfc{io_ctx_};
+
+                bool connection_success = co_await sp_interface.connect_mqtt_client();
+
+                if (!connection_success) {
+                    continue;
+                }
+
+                bool subscribe_success = co_await sp_interface.subscribe_to_ncmd();
+
+                if (!subscribe_success) {
+                    continue;
+                }
+
+                ext_to_tfc.create_outward_signals();
+
+                tfc_to_ext.set_signals();
+
+                sp_interface.set_value_change_callback(
+                        std::bind_front(&tfc::mqtt::ext_to_tfc::receive_new_value, &ext_to_tfc));
+
+                co_await asio::steady_timer{io_ctx_, std::chrono::seconds{1}}.async_wait(asio::use_awaitable);
+
+                auto signals = tfc_to_ext.get_signals();
+
+                asio::cancellation_signal cancel_signal{};
+
+                bool restart_needed = false;
+
+                asio::co_spawn(sp_interface.strand(),
+                               sp_interface.wait_for_payloads(
+                                       std::bind_front(&tfc::mqtt::spark_plug::process_payload, &sp_interface),
+                                       restart_needed),
+                               asio::bind_cancellation_slot(cancel_signal.slot(), asio::detached));
+
+                while (!restart_needed) {
+                    co_await asio::steady_timer{sp_interface.strand(), std::chrono::seconds{5}}.async_wait(
+                            asio::use_awaitable);
+                }
+
+                cancel_signal.emit(asio::cancellation_type::all);
+
+                tfc_to_ext.clear_signals();
+            }
+        }
+
+    private:
+        asio::io_context &io_ctx_;
+        tfc::logger::logger logger{"run_loop"};
+    };
+}
---
+++ exes/mqtt-bridge/tests/src/integration_tests.cpp
@@ -0,0 +1,100 @@
+// #include <run.hpp>
+
+// #include <../inc/broker_runner.hpp>
+// #include <async_mqtt/
+
+#include <boost/asio.hpp>
+//
+namespace asio = boost::asio;
+
+#include <thread>
+#include <optional>
+
+#include <boost/process.hpp>
+#include <boost/process/args.hpp>
+#include <boost/asio.hpp>
+#include <boost/predef.h>
+
+#include <boost/test/unit_test.hpp>
+
+namespace as = boost::asio;
+namespace pr = boost::process;
+// namespace am = async_mqtt;
+
+// inline bool launch_broker_required() {
+//     auto argc = boost::unit_test::framework::master_test_suite().argc;
+//     if (argc >= 3) {
+//         auto argv = boost::unit_test::framework::master_test_suite().argv;
+//         auto launch = std::string_view(argv[2]);
+//         if (launch == "no" || launch == "no_launch") {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+inline void kill_broker() {
+    // if (!launch_broker_required()) return;
+    std::system("pkill broker");
+}
+
+struct broker_killer {
+    broker_killer() {
+        kill_broker();
+    }
+};
+
+struct broker_runner : broker_killer {
+    broker_runner(
+            std::string const &config = "st_broker.conf",
+            std::string const &auth = "st_auth.json"
+    ) {
+        // if (launch_broker_required()) {
+        brk.emplace(pr::args({"../../tool/broker", "--cfg", config, "--auth_file", auth}));
+        // }
+        {
+            as::io_context ioc;
+            as::ip::address address = boost::asio::ip::make_address("127.0.0.1");
+            as::ip::tcp::endpoint endpoint{address, 1883};
+            as::ip::tcp::socket s{ioc};
+            std::function<void(boost::system::error_code const &)> f =
+                    [&](boost::system::error_code const &ec) {
+                        if (ec) {
+                            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                            s = as::ip::tcp::socket{ioc};
+                            s.async_connect(
+                                    endpoint,
+                                    f
+                            );
+                        }
+                    };
+            s.async_connect(
+                    endpoint,
+                    f
+            );
+            ioc.run();
+        }
+    }
+
+    ~broker_runner() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        kill_broker();
+        if (brk) brk->join();
+    }
+
+    std::optional<pr::child> brk;
+};
+
+auto main() -> int {
+    asio::io_context io_ctx{};
+
+    broker_runner broker_i{};
+
+//     tfc::mqtt::run running{io_ctx};
+//
+//     asio::co_spawn(io_ctx, running.start(), asio::detached);
+
+    io_ctx.run();
+
+    return 0;
+}
