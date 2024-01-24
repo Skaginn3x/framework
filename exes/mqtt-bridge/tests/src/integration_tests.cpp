170b9550caa53c8a741a3d690bed4f7fd05ea066
magni <magnimar@skaginn3x.com>
2024-01-24 16:38:30 +0000

    working broker, now need to trim fat

--- exes/mqtt-bridge/tests/src/integration_tests.cpp
+++ exes/mqtt-bridge/tests/src/integration_tests.cpp
@@ -1,15 +1,18 @@
 #include <thread>
 #include <optional>
 
-// #include <async-mqtt/test/system/broker_running.hpp
+#include <boost/asio.hpp>
 
+#include <broker/test/system/broker_runner.hpp>
+
+namespace asio = boost::asio;
 
 auto main() -> int {
     asio::io_context io_ctx{};
 
     broker_runner broker_i{};
 
-    // io_ctx.run();
+    io_ctx.run();
 
     return 0;
 }
---
+++ exes/mqtt-bridge/tests/tool/broker.cpp
@@ -0,0 +1,487 @@
+// Copyright Takatoshi Kondo 2022
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <thread>
+#include <stdexcept>
+
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+#include <boost/asio/signal_set.hpp>
+
+#include <async_mqtt/predefined_underlying_layer.hpp>
+#include <async_mqtt/broker/endpoint_variant.hpp>
+#include <async_mqtt/broker/broker.hpp>
+#include <async_mqtt/broker/constant.hpp>
+#include <async_mqtt/broker/fixed_core_map.hpp>
+#include <async_mqtt/setup_log.hpp>
+
+namespace am = async_mqtt;
+namespace as = boost::asio;
+
+inline
+// void run_broker(boost::program_options::variables_map const &vm) {
+    void run_broker() {
+    try {
+        as::io_context timer_ioc;
+
+        using epv_t = am::endpoint_variant<
+                am::role::server,
+                am::protocol::mqtt
+        >;
+
+        am::broker<
+                epv_t
+        > brk{timer_ioc};
+
+        auto num_of_iocs = 1;
+        //  auto num_of_iocs =
+        //      [&] () -> std::size_t {
+        //          if (vm.count("iocs")) {
+        //              return vm["iocs"].as<std::size_t>();
+        //          }
+        //          return 1;
+        //      } ();
+
+        std::size_t num_of_cores = std::thread::hardware_concurrency();
+
+//         if (num_of_iocs == 0) {
+//             num_of_iocs = num_of_cores;
+//             ASYNC_MQTT_LOG("mqtt_broker", info)
+//                 << "iocs set to auto decide (0). Automatically set to " << num_of_iocs;
+//         }
+
+        std::size_t threads_per_ioc = 1;
+        //  [&] () -> std::size_t {
+        //      if (vm.count("threads_per_ioc")) {
+        //          return vm["threads_per_ioc"].as<std::size_t>();
+        //      }
+        //      return 1;
+        //  } ();
+
+       //  if (threads_per_ioc == 0) {
+       //      threads_per_ioc = std::min(std::size_t(std::thread::hardware_concurrency()), std::size_t(4));
+       //      ASYNC_MQTT_LOG("mqtt_broker", info)
+       //              << "threads_per_ioc set to auto decide (0). Automatically set to "
+       //              << threads_per_ioc;
+       //  }
+
+        ASYNC_MQTT_LOG("mqtt_broker", info)
+                << "iocs:" << num_of_iocs
+                << " threads_per_ioc:" << threads_per_ioc
+                << " total threads:" << num_of_iocs * threads_per_ioc;
+
+        // -----------------------------------------------------------
+//         {
+// # Configure username/login
+//             "authentication": [
+//             {
+//                 "name": "u1",
+//                         "method": "plain_password",
+//                         "password": "passforu1"
+//             }
+//             ,
+//             {
+// # Authenticates user by client certificate
+//                 "name": "cid1",
+//                         "method": "client_cert"
+//             }
+//             ,
+//             {
+//                 "name": "u3",
+//                         "method": "sha256",
+//                         "salt": "mysalt",
+// # mysaltpassforu3
+// # you can get it `echo -n mysaltpassforu3 | sha256sum`
+// # don't forget -n
+//                         "digest": "9ed77f119f694e5e543201a97cc8db226cf36c814c4406fe2eac5b9b1f084ba9"
+//             }
+//             ]
+//             ,
+// # Give access to topics
+//             "authorization": [
+//             {
+// # Specified users and groups are denied to subscribe and publish on this topic
+//                 "topic": "#",
+//                         "allow": {
+//                     "sub": ["u1"],
+//                     "pub": ["u1"]
+//                 }
+//             }
+//             ]
+//         }
+//
+        // -----------------------------------------------------------
+
+
+        // if (vm.count("auth_file")) {
+            // std::string auth_file = vm["auth_file"].as<std::string>();
+            std::string auth_file =  R"(
+
+         {
+ # Configure username/login
+             "authentication": [
+             {
+                 "name": "u1",
+                         "method": "plain_password",
+                         "password": "passforu1"
+             }
+             ,
+             {
+ # Authenticates user by client certificate
+                 "name": "cid1",
+                         "method": "client_cert"
+             }
+             ,
+             {
+                 "name": "u3",
+                         "method": "sha256",
+                         "salt": "mysalt",
+ # mysaltpassforu3
+ # you can get it `echo -n mysaltpassforu3 | sha256sum`
+ # don't forget -n
+                         "digest": "9ed77f119f694e5e543201a97cc8db226cf36c814c4406fe2eac5b9b1f084ba9"
+             }
+             ]
+             ,
+ # Give access to topics
+             "authorization": [
+             {
+ # Specified users and groups are denied to subscribe and publish on this topic
+                 "topic": "#",
+                         "allow": {
+                     "sub": ["u1"],
+                     "pub": ["u1"]
+                 }
+             }
+             ]
+         }
+        )";
+
+
+
+            if (!auth_file.empty()) {
+                ASYNC_MQTT_LOG("mqtt_broker", info)
+                        << "auth_file:" << auth_file;
+
+                std::ifstream input(auth_file);
+
+                if (input) {
+                    am::security security;
+                    security.load_json(input);
+                    brk.set_security(am::force_move(security));
+                } else {
+                    ASYNC_MQTT_LOG("mqtt_broker", warning)
+                            << "Authorization file '"
+                            << auth_file
+                            << "' not found,  broker doesn't use authorization file.";
+                }
+            }
+        //}
+
+        as::io_context accept_ioc;
+
+        std::mutex mtx_con_iocs;
+        std::vector<as::io_context> con_iocs(num_of_iocs);
+        BOOST_ASSERT(!con_iocs.empty());
+
+        std::vector<
+                as::executor_work_guard<
+                        as::io_context::executor_type
+                >
+        > guard_con_iocs;
+        guard_con_iocs.reserve(con_iocs.size());
+        for (auto &con_ioc: con_iocs) {
+            guard_con_iocs.emplace_back(con_ioc.get_executor());
+        }
+
+        auto con_iocs_it = con_iocs.begin();
+
+        auto con_ioc_getter =
+                [&mtx_con_iocs, &con_iocs, &con_iocs_it]() -> as::io_context & {
+                    std::lock_guard<std::mutex> g{mtx_con_iocs};
+                    auto &ret = *con_iocs_it++;
+                    if (con_iocs_it == con_iocs.end()) con_iocs_it = con_iocs.begin();
+                    return ret;
+                };
+
+        // mqtt (MQTT on TCP)
+        am::optional<as::ip::tcp::endpoint> mqtt_endpoint;
+        am::optional<as::ip::tcp::acceptor> mqtt_ac;
+        std::function<void()> mqtt_async_accept;
+        // if (vm.count("tcp.port")) {
+            // mqtt_endpoint.emplace(as::ip::tcp::v4(), vm["tcp.port"].as<std::uint16_t>());
+            mqtt_endpoint.emplace(as::ip::tcp::v4(), 1883);
+            mqtt_ac.emplace(accept_ioc, *mqtt_endpoint);
+            mqtt_async_accept =
+                    [&] {
+                        auto epsp =
+                                am::endpoint<am::role::server, am::protocol::mqtt>::create(
+                                        am::protocol_version::undetermined,
+                                        con_ioc_getter().get_executor()
+                                );
+
+                        auto &lowest_layer = epsp->lowest_layer();
+                        mqtt_ac->async_accept(
+                                lowest_layer,
+                                [&mqtt_async_accept, &brk, epsp]
+                                        (boost::system::error_code const &ec) mutable {
+                                    if (ec) {
+                                        ASYNC_MQTT_LOG("mqtt_broker", error)
+                                                << "TCP accept error:" << ec.message();
+                                    } else {
+                                        brk.handle_accept(epv_t{force_move(epsp)});
+                                    }
+                                    mqtt_async_accept();
+                                }
+                        );
+                    };
+
+            mqtt_async_accept();
+        // }
+
+        std::thread th_accept{
+                [&accept_ioc] {
+                    try {
+                        accept_ioc.run();
+                    }
+                    catch (std::exception const &e) {
+                        ASYNC_MQTT_LOG("mqtt_broker", error)
+                                << "th_accept exception:" << e.what();
+                    }
+                    ASYNC_MQTT_LOG("mqtt_broker", trace) << "accept_ioc.run() finished";
+                }
+        };
+
+        as::executor_work_guard<
+                as::io_context::executor_type
+        > guard_timer_ioc(timer_ioc.get_executor());
+
+        std::thread th_timer{
+                [&timer_ioc] {
+                    try {
+                        timer_ioc.run();
+                    }
+                    catch (std::exception const &e) {
+                        ASYNC_MQTT_LOG("mqtt_broker", error)
+                                << "th_timer exception:" << e.what();
+                    }
+                    ASYNC_MQTT_LOG("mqtt_broker", trace) << "timer_ioc.run() finished";
+                }
+        };
+        std::vector<std::thread> ts;
+        ts.reserve(num_of_iocs * threads_per_ioc);
+        // auto fixed_core_map = vm["fixed_core_map"].as<bool>();
+        auto fixed_core_map = false;
+
+        std::size_t ioc_index = 0;
+        for (auto &con_ioc: con_iocs) {
+            for (std::size_t i = 0; i != threads_per_ioc; ++i) {
+                ts.emplace_back(
+                        [&con_ioc, ioc_index, num_of_cores, fixed_core_map] {
+                            try {
+                                if (fixed_core_map) {
+                                    am::map_core_to_this_thread(ioc_index % num_of_cores);
+                                }
+                                con_ioc.run();
+                            }
+                            catch (std::exception const &e) {
+                                ASYNC_MQTT_LOG("mqtt_broker", error)
+                                        << "th con exception:" << e.what();
+                            }
+                            ASYNC_MQTT_LOG("mqtt_broker", trace) << "con_ioc.run() finished";
+                        }
+                );
+            }
+            ++ioc_index;
+        }
+
+        as::io_context ioc_signal;
+        as::signal_set signals{ioc_signal, SIGINT, SIGTERM};
+        signals.async_wait(
+                [](
+                        boost::system::error_code const &ec,
+                        int num
+                ) {
+                    if (!ec) {
+                        ASYNC_MQTT_LOG("mqtt_broker", trace)
+                                << "Signal " << num << " received. exit program";
+                        exit(-1);
+                    }
+                }
+        );
+        std::thread th_signal{
+                [&] {
+                    try {
+                        ioc_signal.run();
+                    }
+                    catch (std::exception const &e) {
+                        ASYNC_MQTT_LOG("mqtt_broker", error)
+                                << "th_signal exception:" << e.what();
+                    }
+                }
+        };
+
+        th_accept.join();
+        ASYNC_MQTT_LOG("mqtt_broker", trace) << "th_accept joined";
+
+        for (auto &g: guard_con_iocs) g.reset();
+        for (auto &t: ts) t.join();
+        ASYNC_MQTT_LOG("mqtt_broker", trace) << "ts joined";
+
+        guard_timer_ioc.reset();
+        th_timer.join();
+        ASYNC_MQTT_LOG("mqtt_broker", trace) << "th_timer joined";
+
+        signals.cancel();
+        th_signal.join();
+        ASYNC_MQTT_LOG("mqtt_broker", trace) << "th_signal joined";
+    }
+    catch (std::exception const &e) {
+        ASYNC_MQTT_LOG("mqtt_broker", error) << e.what();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    try {
+        boost::program_options::options_description desc;
+
+        boost::program_options::options_description general_desc("General options");
+        general_desc.add_options()
+                ("help", "produce help message")
+                (
+                        "cfg",
+                        boost::program_options::value<std::string>()->default_value("broker.conf"),
+                        "Load configuration file"
+                )
+                (
+                        "silent",
+                        boost::program_options::value<bool>()->default_value(false),
+                        "if true, then set options are not output"
+                )
+                (
+                        "iocs",
+                        boost::program_options::value<std::size_t>()->default_value(1),
+                        "Number of io_context. If set 0 then automatically decided by hardware_concurrency()."
+                )
+                (
+                        "threads_per_ioc",
+                        boost::program_options::value<std::size_t>()->default_value(1),
+                        "Number of worker threads for each io_context."
+                )
+                (
+                        "verbose",
+                        boost::program_options::value<unsigned int>()->default_value(1),
+                        "set verbose level, possible values:\n 0 - Fatal\n 1 - Error\n 2 - Warning\n 3 - Info\n 4 - Debug\n 5 - Trace"
+                )
+                (
+                        "certificate",
+                        boost::program_options::value<std::string>(),
+                        "Certificate file for TLS connections"
+                )
+                (
+                        "private_key",
+                        boost::program_options::value<std::string>(),
+                        "Private key file for TLS connections"
+                )
+                (
+                        "verify_file",
+                        boost::program_options::value<std::string>(),
+                        "Root certificate used for verification of the client"
+                )
+                (
+                        "verify_field",
+                        boost::program_options::value<std::string>()->default_value("CN"),
+                        "Field to be used from certificate for authenticating clients"
+                )
+                (
+                        "auth_file",
+                        boost::program_options::value<std::string>(),
+                        "Authentication file"
+                )
+                (
+                        "fixed_core_map",
+                        boost::program_options::value<bool>()->default_value(false),
+                        "Use the specific CPU core by ioc."
+                );
+
+        boost::program_options::options_description notls_desc("TCP Server options");
+        notls_desc.add_options()
+                ("tcp.port", boost::program_options::value<std::uint16_t>(), "default port (TCP)");
+        desc.add(general_desc).add(notls_desc);
+
+        boost::program_options::options_description ws_desc("TCP websocket Server options");
+        ws_desc.add_options()
+                ("ws.port", boost::program_options::value<std::uint16_t>(), "default port (TCP)");
+
+        desc.add(ws_desc);
+
+        boost::program_options::options_description tls_desc("TLS Server options");
+        tls_desc.add_options()
+                ("tls.port", boost::program_options::value<std::uint16_t>(), "default port (TLS)");
+
+        desc.add(tls_desc);
+
+        boost::program_options::options_description tlsws_desc("TLS Websocket Server options");
+        tlsws_desc.add_options()
+                ("wss.port", boost::program_options::value<std::uint16_t>(), "default port (TLS)");
+        desc.add(tlsws_desc);
+
+        boost::program_options::variables_map vm;
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+
+        std::string config_file = vm["cfg"].as<std::string>();
+        if (!config_file.empty()) {
+            std::ifstream input(vm["cfg"].as<std::string>());
+            if (input) {
+                boost::program_options::store(boost::program_options::parse_config_file(input, desc), vm);
+            } else {
+                std::cerr
+                        << "Configuration file '"
+                        << config_file
+                        << "' not found,  broker doesn't use configuration file."
+                        << std::endl;
+            }
+        }
+
+        boost::program_options::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
+            return 1;
+        }
+        if (!vm["silent"].as<bool>()) {
+            std::cout << "Set options:" << std::endl;
+            for (auto const &e: vm) {
+                std::cout << boost::format("%-28s") % e.first.c_str() << " : ";
+                if (auto p = boost::any_cast<std::string>(&e.second.value())) {
+                    std::cout << *p;
+                } else if (auto p = boost::any_cast<std::size_t>(&e.second.value())) {
+                    std::cout << *p;
+                } else if (auto p = boost::any_cast<std::uint32_t>(&e.second.value())) {
+                    std::cout << *p;
+                } else if (auto p = boost::any_cast<std::uint16_t>(&e.second.value())) {
+                    std::cout << *p;
+                } else if (auto p = boost::any_cast<unsigned int>(&e.second.value())) {
+                    std::cout << *p;
+                } else if (auto p = boost::any_cast<bool>(&e.second.value())) {
+                    std::cout << std::boolalpha << *p;
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        am::setup_log();
+
+        run_broker();
+    }
+    catch (std::exception const &e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
