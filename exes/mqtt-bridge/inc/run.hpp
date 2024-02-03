b9e61399abef9dc532dbe152a1c6794088740436
Magni Mar Magnason <magnimar@skaginn3x.com>
2024-02-03 19:11:53 +0000

    test is ready and working, code is working, but can't destruct object for unknown reason

---
+++ exes/mqtt-bridge/inc/run.hpp
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <type_traits>
+
+#include <async_mqtt/all.hpp>
+#include <boost/asio.hpp>
+
+#include <tfc/confman.hpp>
+#include <tfc/ipc.hpp>
+#include <tfc/logger.hpp>
+
+#include <client.hpp>
+#include <external_to_tfc.hpp>
+#include <spark_plug_interface.hpp>
+#include <tfc_to_external.hpp>
+
+namespace asio = boost::asio;
+
+namespace tfc::mqtt {
+    template<class config_t = confman::config<config::bridge>,
+        class mqtt_client_t = client_n,
+        class ipc_client_t = ipc_ruler::ipc_manager_client>
+    class run {
+        using spark_plug = spark_plug_interface<config_t, mqtt_client_t>;
+        using ext_to_tfc = external_to_tfc<ipc_client_t &, config_t>;
+        using tfc_to_ext = tfc_to_external<config_t, mqtt_client_t, ipc_client_t &>;
+
+    public:
+        explicit run(asio::io_context &io_ctx) : io_ctx_(io_ctx), ipc_client_(io_ctx) {
+        }
+
+        explicit run(asio::io_context &io_ctx, ipc_client_t ipc_client) : io_ctx_(io_ctx), ipc_client_(ipc_client) {
+            static_assert(std::is_lvalue_reference<ipc_client_t>::value);
+        }
+
+        auto start() -> asio::awaitable<void> {
+            while (true) {
+                restart_needed_ = false;
+
+                logger.trace("----------------------------------------------------------------------------");
+                logger.trace("Event loop started");
+
+                spark_plug sp_interface_{io_ctx_, config_};
+
+                tfc_to_ext tfc_to_exter_{
+                    io_ctx_, sp_interface_, ipc_client_, config_
+                };
+
+                ext_to_tfc exter_to_tfc_{io_ctx_, config_, ipc_client_};
+
+                bool connection_success = co_await sp_interface_.connect_mqtt_client();
+
+                if (!connection_success) {
+                    continue;
+                }
+
+                bool subscribe_success = co_await sp_interface_.subscribe_to_ncmd();
+
+                if (!subscribe_success) {
+                    continue;
+                }
+
+                exter_to_tfc_.create_outward_signals();
+
+                tfc_to_exter_.set_signals();
+
+                tfc_to_exter_.register_signal_change_callback(restart_needed_);
+
+                sp_interface_.
+                        set_value_change_callback(std::bind_front(&ext_to_tfc::receive_new_value, &exter_to_tfc_));
+
+                co_await asio::steady_timer{io_ctx_, std::chrono::seconds{1}}.async_wait(asio::use_awaitable);
+
+                asio::cancellation_signal cancel_signal{};
+
+                co_spawn(
+                    sp_interface_.strand(),
+                    sp_interface_.wait_for_payloads(std::bind_front(&spark_plug::process_payload, &sp_interface_),
+                                                    restart_needed_),
+                    bind_cancellation_slot(cancel_signal.slot(), asio::detached));
+
+                io_ctx_.run_for(std::chrono::seconds{1});
+
+                while (!restart_needed_) {
+                    logger.trace("restart_needed = {}", restart_needed_);
+                    co_await asio::steady_timer{sp_interface_.strand(), std::chrono::seconds{1}}.async_wait(
+                        asio::use_awaitable);
+                }
+
+                cancel_signal.emit(asio::cancellation_type::all);
+
+                io_ctx_.run_for(std::chrono::seconds{1});
+
+            }
+            co_return;
+        }
+
+        auto config() -> config_t & { return config_; }
+
+    private:
+        asio::io_context &io_ctx_;
+        ipc_client_t ipc_client_;
+        bool restart_needed_{false};
+        config_t config_{io_ctx_, "mqtt"};
+        logger::logger logger{"run_loop"};
+    };
+} // namespace tfc::mqtt
--- exes/mqtt-bridge/inc/tfc_to_external.hpp
+++ exes/mqtt-bridge/inc/tfc_to_external.hpp
@@ -1,173 +1,175 @@
 #pragma once
 
-#include <chrono>
 #include <concepts>
-#include <memory>
+#include <optional>
 #include <string>
 #include <type_traits>
 #include <variant>
 #include <vector>
 
 #include <boost/asio.hpp>
-#include <sdbusplus/bus/match.hpp>
 
 #include <tfc/ipc.hpp>
 #include <tfc/logger.hpp>
 #include <tfc/progbase.hpp>
 
-#include <config/publish_signals.hpp>
 #include <signal_names.hpp>
 #include <spark_plug_interface.hpp>
-#include <structs.hpp>
 
 namespace tfc::mqtt {
+    namespace asio = boost::asio;
 
-namespace asio = boost::asio;
+    template<class config_t, class mqtt_client_t, class ipc_client_t>
+    class tfc_to_external {
+    public:
+        explicit tfc_to_external(asio::io_context &io_ctx,
+                                 spark_plug_interface<config_t, mqtt_client_t> &spark_plug_i,
+                                 config_t &config)
+            : io_ctx_(io_ctx), spark_plug_interface_(spark_plug_i), config_(config), ipc_client_(io_ctx_) {
+        }
 
-template <class publish_signals_config_t, class spark_plug_config_t, class mqtt_client_t, class ipc_client_t>
-class tfc_to_external {
-public:
-  explicit tfc_to_external(asio::io_context& io_ctx,
-                           tfc::mqtt::spark_plug_interface<spark_plug_config_t, mqtt_client_t>& spark_plug_i)
-      : io_ctx_(io_ctx), spark_plug_interface_(spark_plug_i), ipc_client_(io_ctx_) {}
+        explicit tfc_to_external(asio::io_context &io_ctx,
+                                 spark_plug_interface<config_t, mqtt_client_t> &spark_plug_i,
+                                 ipc_client_t ipc_client,
+                                 config_t &config)
+            : io_ctx_(io_ctx), spark_plug_interface_(spark_plug_i), config_(config), ipc_client_(ipc_client) {
+            static_assert(std::is_lvalue_reference<ipc_client_t>::value);
+        }
 
-  explicit tfc_to_external(asio::io_context& io_ctx,
-                           tfc::mqtt::spark_plug_interface<spark_plug_config_t, mqtt_client_t>& spark_plug_i,
-                           ipc_client_t ipc_client)
-      : io_ctx_(io_ctx), spark_plug_interface_(spark_plug_i), ipc_client_(ipc_client) {
-    static_assert(std::is_lvalue_reference<ipc_client_t>::value);
-  }
-
-  /// This function converts tfc types to Spark Plug B types
-  /// More information can be found (page 76) in the spec under section 6.4.16 data types:
-  /// https://sparkplug.eclipse.org/specification/version/3.0/documents/sparkplug-specification-3.0.0.pdf
-  static auto type_enum_convert(tfc::ipc::details::type_e type) -> DataType {
-    using enum tfc::ipc::details::type_e;
-    switch (type) {
-      case unknown:
-        return DataType::Unknown;
-      case _bool:
-        return DataType::Boolean;
-      case _int64_t:
-        return DataType::Int64;
-      case _uint64_t:
-        return DataType::UInt64;
-      case _double_t:
-        return DataType::Double;
-      case _string:
-      case _json:
-        return DataType::String;
-      case _mass:
-        return DataType::Template;
-    }
-    return DataType::Unknown;
-  }
-
-  /// Spark Plug B disallows the use of some special characters in the signal name.
-  static auto format_signal_name(std::string signal_name_to_format) -> std::string {
-    for (const auto& forbidden_char : { '+', '#', '-', '/' }) {
-      std::ranges::replace(signal_name_to_format.begin(), signal_name_to_format.end(), forbidden_char, '_');
-    }
-
-    std::ranges::replace(signal_name_to_format.begin(), signal_name_to_format.end(), '.', '/');
-    return signal_name_to_format;
-  }
-
-  auto set_signals() -> void {
-    logger_.trace("Starting to add new signals...");
-    ipc_client_.signals(
-        [this](std::vector<ipc_ruler::signal> const& signals) { handle_incoming_signals_from_ipc_client(signals); });
-  }
-
-  auto is_publish_signal(std::string signal_name) -> bool {
-    // check if signal is a writeable signal
-    if (signal_name.starts_with(tfc::base::get_exe_name())) {
-      logger_.trace("Signal is a writeable signal");
-      return true;
-    }
-
-    for (const auto& publish_signal : config_.value().publish_signals) {
-      if (publish_signal.value == signal_name) {
-        logger_.trace("Signal {} is in the list of signals to publish", signal_name);
-        return true;
-      }
-    }
-    logger_.trace("Signal {} is not in the list of signals to publish", signal_name);
-    return false;
-  }
-
-  auto handle_incoming_signals_from_ipc_client(std::vector<tfc::ipc_ruler::signal> const& signals) -> void {
-    tfc::global::set_signals(signals);
-    logger_.trace("Received {} new signals to add.", signals.size());
-
-    signals_.clear();
-    spb_variables_.clear();
-
-    signals_.reserve(signals.size());
-    spb_variables_.reserve(signals.size());
-
-    for (const auto& signal : signals) {
-      if (!is_publish_signal(signal.name)) {
-        continue;
-      }
-
-      signals_.emplace_back(ipc::details::make_any_slot_cb::make(signal.type, io_ctx_, signal.name));
-
-      spb_variables_.emplace_back(format_signal_name(signal.name.data()), type_enum_convert(signal.type), std::nullopt,
-                                  signal.description.data());
-
-      logger_.trace("Connecting: {}", signal.name);
-
-      auto& slot = signals_.back();
-      auto& variable = spb_variables_.back();
-
-      std::visit(
-          [this, &variable](auto&& receiver) {
-            using receiver_t = std::remove_cvref_t<decltype(receiver)>;
-            if constexpr (!std::same_as<receiver_t, std::monostate>) {
-              auto error_code = receiver->connect(receiver->name(), [this, &variable](auto&& value) {
-                variable.value = value;
-                spark_plug_interface_.update_value(variable);
-              });
-              if (error_code) {
-                logger_.trace("Error connecting to signal: {}, error: {}", receiver->name(), error_code.message());
-              }
+        /// This function converts tfc types to Spark Plug B types
+        /// More information can be found (page 76) in the spec under section 6.4.16 data types:
+        /// https://sparkplug.eclipse.org/specification/version/3.0/documents/sparkplug-specification-3.0.0.pdf
+        static auto type_enum_convert(ipc::details::type_e type) -> DataType {
+            using enum ipc::details::type_e;
+            switch (type) {
+                case unknown:
+                    return DataType::Unknown;
+                case _bool:
+                    return DataType::Boolean;
+                case _int64_t:
+                    return DataType::Int64;
+                case _uint64_t:
+                    return DataType::UInt64;
+                case _double_t:
+                    return DataType::Double;
+                case _string:
+                case _json:
+                    return DataType::String;
+                case _mass:
+                    return DataType::Template;
             }
-          },
-          slot);
-    }
+            return DataType::Unknown;
+        }
 
-    logger_.trace("All new signals added. Preparing to send NBIRTH and start signals...");
+        /// Spark Plug B disallows the use of some special characters in the signal name.
+        static auto format_signal_name(std::string signal_name_to_format) -> std::string {
+            for (const auto &forbidden_char: {'+', '#', '-', '/'}) {
+                std::ranges::replace(signal_name_to_format.begin(), signal_name_to_format.end(), forbidden_char, '_');
+            }
 
-    set_current_values();
-  }
+            std::ranges::replace(signal_name_to_format.begin(), signal_name_to_format.end(), '.', '/');
+            return signal_name_to_format;
+        }
 
-  auto set_current_values() -> void {
-    logger_.info("Setting current values to interface");
+        auto set_signals() -> void {
+            logger_.trace("Starting to add new signals...");
+            ipc_client_.signals(
+                [this](std::vector<ipc_ruler::signal> const &signals) {
+                    handle_incoming_signals_from_ipc_client(signals);
+                });
+        }
 
-    spark_plug_interface_.set_current_values(spb_variables_);
-    spark_plug_interface_.send_current_values();
-  }
+        auto register_signal_change_callback(bool &ipc_ruler_changed) -> void {
+            match_object_ = ipc_client_.register_properties_change_callback(
+                [&ipc_ruler_changed](sdbusplus::message_t &) { ipc_ruler_changed = true; });
+        }
 
-  auto get_signals() -> std::vector<tfc::ipc::details::any_slot_cb>& { return signals_; }
+        auto is_publish_signal(std::string signal_name) -> bool {
+            // check if signal is a writeable signal
+            if (signal_name.starts_with(base::get_exe_name())) {
+                logger_.trace("Signal is a writeable signal");
+                return true;
+            }
 
-  auto clear_signals() -> void { signals_.clear(); }
+            for (const auto &publish_signal: config_.value().publish_signals) {
+                if (publish_signal.value == signal_name) {
+                    logger_.trace("Signal {} is in the list of signals to publish", signal_name);
+                    return true;
+                }
+            }
+            logger_.trace("Signal {} is not in the list of signals to publish", signal_name);
+            return false;
+        }
 
-private:
-  asio::io_context& io_ctx_;
-  spark_plug_interface<spark_plug_config_t, mqtt_client_t>& spark_plug_interface_;
-  ipc_client_t ipc_client_;
-  tfc::logger::logger logger_{ "tfc_to_external" };
-  std::vector<tfc::ipc::details::any_slot_cb> signals_;
-  std::vector<structs::spark_plug_b_variable> spb_variables_;
-  publish_signals_config_t config_{ io_ctx_, "publish_signals_config" };
+        auto handle_incoming_signals_from_ipc_client(std::vector<ipc_ruler::signal> const &signals) -> void {
+            global::set_signals(signals);
+            logger_.trace("Received {} new signals to add.", signals.size());
 
-  friend class test_tfc_to_external;
-};
+            signals_.clear();
+            spb_variables_.clear();
 
-using tfc_to_ext = tfc_to_external<tfc::confman::config<config::publish_signals>,
-                                   tfc::confman::config<config::spark_plug_b>,
-                                   client<endpoint_client, tfc::confman::config<config::broker>>,
-                                   tfc::ipc_ruler::ipc_manager_client>;
+            signals_.reserve(signals.size());
+            spb_variables_.reserve(signals.size());
 
-}  // namespace tfc::mqtt
+            for (const auto &signal: signals) {
+                if (!is_publish_signal(signal.name)) {
+                    continue;
+                }
+
+                signals_.emplace_back(ipc::details::make_any_slot_cb::make(signal.type, io_ctx_, signal.name));
+
+                spb_variables_.emplace_back(format_signal_name(signal.name.data()), type_enum_convert(signal.type),
+                                            std::nullopt,
+                                            signal.description.data());
+
+                logger_.trace("Connecting: {}", signal.name);
+
+                auto &slot = signals_.back();
+                auto &variable = spb_variables_.back();
+
+                std::visit(
+                    [this, &variable](auto &&receiver) {
+                        using receiver_t = std::remove_cvref_t<decltype(receiver)>;
+                        if constexpr (!std::same_as<receiver_t, std::monostate>) {
+                            auto error_code = receiver->connect(receiver->name(), [this, &variable](auto &&value) {
+                                variable.value = value;
+                                spark_plug_interface_.update_value(variable);
+                            });
+                            if (error_code) {
+                                logger_.trace("Error connecting to signal: {}, error: {}", receiver->name(),
+                                              error_code.message());
+                            }
+                        }
+                    },
+                    slot);
+            }
+
+            logger_.trace("All new signals added. Preparing to send NBIRTH and start signals...");
+
+            set_current_values();
+        }
+
+        auto set_current_values() -> void {
+            logger_.info("Setting current values to interface");
+
+            spark_plug_interface_.set_current_values(spb_variables_);
+            spark_plug_interface_.send_current_values();
+        }
+
+        auto get_signals() -> std::vector<ipc::details::any_slot_cb> & { return signals_; }
+
+        auto clear_signals() -> void { signals_.clear(); }
+
+    private:
+        asio::io_context &io_ctx_;
+        spark_plug_interface<config_t, mqtt_client_t> &spark_plug_interface_;
+        config_t &config_;
+        ipc_client_t ipc_client_;
+        logger::logger logger_{"tfc_to_external"};
+        std::vector<ipc::details::any_slot_cb> signals_;
+        std::vector<structs::spark_plug_b_variable> spb_variables_;
+        std::unique_ptr<sdbusplus::bus::match::match> match_object_;
+
+        friend class test_tfc_to_external;
+    };
+} // namespace tfc::mqtt
