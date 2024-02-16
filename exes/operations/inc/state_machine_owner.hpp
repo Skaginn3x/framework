66dcc46d770dfcbd15fbe4d3c66af4e09c6b6133
Magni Mar Magnason <magnimar@skaginn3x.com>
2024-02-16 11:36:02 +0000

    minimizing changes

--- exes/operations/inc/state_machine_owner.hpp
+++ exes/operations/inc/state_machine_owner.hpp
@@ -81,6 +81,7 @@ public:
   auto running_signal() const noexcept -> auto const& { return running_; }
   auto stopping_signal() const noexcept -> auto const& { return stopping_; }
   auto cleaning_signal() const noexcept -> auto const& { return cleaning_; }
+  auto emergency_signal() const noexcept -> auto const& { return emergency_out_; }
   auto mode_signal() const noexcept -> auto const& { return mode_; }
   auto mode_str_signal() const noexcept -> auto const& { return mode_str_; }
   auto stop_reason_str_signal() const noexcept -> auto const& { return stop_reason_str_; }
@@ -119,6 +120,7 @@ private:
   bool_signal_t running_{ ctx_, mclient_, "running" };
   bool_signal_t stopping_{ ctx_, mclient_, "stopping" };
   bool_signal_t cleaning_{ ctx_, mclient_, "cleaning" };
+  bool_signal_t emergency_out_{ ctx_, mclient_, "emergency_out" };
   bool_signal_t fault_out_{ ctx_, mclient_, "fault" };
   uint_signal_t mode_{ ctx_, mclient_, "mode" };
   string_signal_t mode_str_{ ctx_, mclient_, "mode" };
