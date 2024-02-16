19f8ad50de32ba1e8ffdd9f0a8814d1aa191e352
Jón Bjarni <jbbjarnason@gmail.com>
2024-02-16 14:31:22 +0000

    Update exes/operations/inc/state_machine_owner.hpp
    Co-authored-by: Ómar Högni Guðmarsson <ohg@skaginn3x.com>

--- exes/operations/inc/state_machine_owner.hpp
+++ exes/operations/inc/state_machine_owner.hpp
@@ -120,7 +120,7 @@ private:
   bool_signal_t running_{ ctx_, mclient_, "running" };
   bool_signal_t stopping_{ ctx_, mclient_, "stopping" };
   bool_signal_t cleaning_{ ctx_, mclient_, "cleaning" };
-  bool_signal_t emergency_out_{ ctx_, mclient_, "emergency_out" };
+  bool_signal_t emergency_out_{ ctx_, mclient_, "emergency" };
   bool_signal_t fault_out_{ ctx_, mclient_, "fault" };
   uint_signal_t mode_{ ctx_, mclient_, "mode" };
   string_signal_t mode_str_{ ctx_, mclient_, "mode" };
