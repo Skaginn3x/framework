460ee47c6201a0c667e6e7293dbd99a96873ad2a
magni-mar <magni-mar@users.noreply.github.com>
2024-02-20 09:02:54 +0000

    :art: Committing clang-format changes

--- exes/mqtt-bridge/src/main.cpp
+++ exes/mqtt-bridge/src/main.cpp
@@ -11,10 +11,9 @@ auto main(int argc, char* argv[]) -> int {
 
   asio::io_context io_ctx{};
 
-  tfc::mqtt::run<
-      tfc::confman::config<tfc::mqtt::config::bridge>,
-      tfc::mqtt::client<tfc::mqtt::endpoint_client, tfc::confman::config<tfc::mqtt::config::bridge>>,
-      tfc::ipc_ruler::ipc_manager_client>
+  tfc::mqtt::run<tfc::confman::config<tfc::mqtt::config::bridge>,
+                 tfc::mqtt::client<tfc::mqtt::endpoint_client, tfc::confman::config<tfc::mqtt::config::bridge>>,
+                 tfc::ipc_ruler::ipc_manager_client>
       running{ io_ctx };
 
   co_spawn(io_ctx, running.start(), asio::detached);
