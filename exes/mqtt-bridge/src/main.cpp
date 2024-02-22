e639f5d0937b2f427c43469550e943e6c3f682c7
Magni Mar Magnason <magnimar@skaginn3x.com>
2024-02-22 13:26:19 +0000

    fix reference

--- exes/mqtt-bridge/src/main.cpp
+++ exes/mqtt-bridge/src/main.cpp
@@ -17,9 +17,10 @@ auto main(int argc, char *argv[]) -> int {
     asio::io_context io_ctx{};
 
     tfc::ipc_ruler::ipc_manager_client ipc_client{io_ctx};
+
   tfc::mqtt::run<tfc::confman::config<tfc::mqtt::config::bridge>,
                  tfc::mqtt::client<tfc::mqtt::endpoint_client, tfc::confman::config<tfc::mqtt::config::bridge>>,
-                 tfc::ipc_ruler::ipc_manager_client>
+                 tfc::ipc_ruler::ipc_manager_client&>
       running{ io_ctx, ipc_client };
   
     co_spawn(io_ctx, running.start(), asio::detached);
