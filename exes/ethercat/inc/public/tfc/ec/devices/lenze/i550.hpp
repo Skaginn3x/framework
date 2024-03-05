0613408d1adb412b2dd6a43717cb01ddb1c35771
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-03-05 10:01:48 +0000

    add product code

--- exes/ethercat/inc/public/tfc/ec/devices/lenze/i550.hpp
+++ exes/ethercat/inc/public/tfc/ec/devices/lenze/i550.hpp
@@ -8,7 +8,7 @@ public:
   ~i550() final;
   explicit i550(uint16_t slave_index) : base(slave_index) {}
   void process_data(std::span<std::byte>, std::span<std::byte>) final {}
-  static constexpr uint32_t product_code = 0x44c2c52;
-  static constexpr uint32_t vendor_id = 0x2;
+  static constexpr uint32_t product_code = 0x69055000;
+  static constexpr uint32_t vendor_id = 0x3b;
 };
 }  // namespace tfc::ec::devices::lenze
