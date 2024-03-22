c3e913cf65158819da41b2e7473fbef5f6979bed
Magni Mar Magnason <magnimar@skaginn3x.com>
2024-03-22 10:33:15 +0000

    reduce duplicity

--- exes/operations/inc/details/state_machine_owner_impl.hpp
+++ exes/operations/inc/details/state_machine_owner_impl.hpp
@@ -20,38 +20,22 @@ state_machine_owner<signal_t, slot_t, sml_t>::state_machine_owner(asio::io_conte
                                                                          tfc::dbus::make_dbus_name("Operations")) },
       states_{ std::make_shared<state_machine_t>(detail::state_machine<state_machine_owner>{ *this }, sml_interface_) } {
 
-starting_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send starting_ signal false, error: {}", err.message());
-});
-running_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send running_ signal false, error: {}", err.message());
-});
-stopping_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send stopping_ signal false, error: {}", err.message());
-});
-cleaning_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send cleaning_ signal false, error: {}", err.message());
-});
-emergency_out_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send emergency_out_ signal false, error: {}", err.message());
-});
-fault_out_.async_send(false, [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send fault_out_ signal false, error: {}", err.message());
-});
-mode_str_.async_send("", [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send mode_ signal false, error: {}", err.message());
-});
-stop_reason_str_.async_send("", [&](std::error_code const& err,  std::size_t) {
-  if (err)
-    logger_.info("Unable to send stop_reason_str_ signal false, error: {}", err.message());
-});
+  auto constexpr print_error = [&](std::string signal_name) {
+  return [this, signal_name](std::error_code const& err, std::size_t) {
+    if (err) {
+    logger_.info("Unable to send {} signal false, error: {}", signal_name, err.message());
+    }
+  };
+ };
+
+starting_.async_send(false, print_error("starting"));
+running_.async_send(false, print_error("running"));
+stopping_.async_send(false, print_error("stopping"));
+cleaning_.async_send(false, print_error("cleaning"));
+emergency_out_.async_send(false, print_error("emergency_out"));
+fault_out_.async_send(false, print_error("fault_out"));
+mode_str_.async_send("", print_error("mode_str"));
+stop_reason_str_.async_send("", print_error("stop_reason_str"));
 
   dbus_interface_->initialize();
 }
