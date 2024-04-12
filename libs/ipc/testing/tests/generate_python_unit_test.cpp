dd452bf31847000d36c802fccf58d00db912dbbc
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-04-12 09:40:24 +0000

    Added a target to generate python unit tests

---
+++ libs/ipc/testing/tests/generate_python_unit_test.cpp
@@ -0,0 +1,15 @@
+
+// Generate a test case for python comparing the C++ implementation of the item class
+
+#include <fmt/printf.h>
+#include <fmt/format.h>
+#include <tfc/ipc/item.hpp>
+
+int main(){
+  for(int species_index = 0; species_index < 26 * 26 * 26; species_index++) {
+    std::string code(tfc::ipc::item::fao::species::from_int(species_index)->code.data());
+    fmt::println("        self.assertEqual(le.from_int_to_3a({}), \"{}\")", species_index, code);
+    fmt::println("        self.assertEqual(le.from_3a_to_int(\"{}\"), {})", code, species_index);
+  }
+  return 0;
+}
