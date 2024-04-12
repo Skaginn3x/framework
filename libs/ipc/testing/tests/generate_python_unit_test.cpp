0f8aa6009de2c75d5bd38c5568e66e0e41203086
omarhogni <omarhogni@users.noreply.github.com>
2024-04-12 09:41:05 +0000

    :art: Committing clang-format changes

--- libs/ipc/testing/tests/generate_python_unit_test.cpp
+++ libs/ipc/testing/tests/generate_python_unit_test.cpp
@@ -1,12 +1,12 @@
 
 // Generate a test case for python comparing the C++ implementation of the item class
 
-#include <fmt/printf.h>
 #include <fmt/format.h>
+#include <fmt/printf.h>
 #include <tfc/ipc/item.hpp>
 
-int main(){
-  for(int species_index = 0; species_index < 26 * 26 * 26; species_index++) {
+int main() {
+  for (int species_index = 0; species_index < 26 * 26 * 26; species_index++) {
     std::string code(tfc::ipc::item::fao::species::from_int(species_index)->code.data());
     fmt::println("        self.assertEqual(le.from_int_to_3a({}), \"{}\")", species_index, code);
     fmt::println("        self.assertEqual(le.from_3a_to_int(\"{}\"), {})", code, species_index);
