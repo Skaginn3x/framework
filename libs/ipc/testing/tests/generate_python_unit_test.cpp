9f7fd88465e4c3e005b6e62c534c2b8286b220c8
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-04-12 10:56:05 +0000

    Implicit conversion of int

--- libs/ipc/testing/tests/generate_python_unit_test.cpp
+++ libs/ipc/testing/tests/generate_python_unit_test.cpp
@@ -6,7 +6,7 @@
 #include <tfc/ipc/item.hpp>
 
 int main() {
-  for (int species_index = 0; species_index < 26 * 26 * 26; species_index++) {
+  for (std::uint16_t species_index = 0; species_index < 26 * 26 * 26; species_index++) {
     std::string code(tfc::ipc::item::fao::species::from_int(species_index)->code.data());
     fmt::println("        self.assertEqual(le.from_int_to_3a({}), \"{}\")", species_index, code);
     fmt::println("        self.assertEqual(le.from_3a_to_int(\"{}\"), {})", code, species_index);
