baf148218007e6b203e888c533f541188f598000
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-05-21 13:01:52 +0000

    some trivial left overs from comments

--- exes/ethercat/inc/public/tfc/ec/devices/base.hpp
+++ exes/ethercat/inc/public/tfc/ec/devices/base.hpp
@@ -192,8 +192,8 @@ public:
       if (output.size() != sizeof(output_pdo)) {
         if (output_buffer_valid_) {
           logger_.info("Output size mismatch, expected {} bytes, got {} bytes", sizeof(output_pdo), output.size());
+          output_buffer_valid_ = false;
         }
-        output_buffer_valid_ = false;
         if constexpr (stx::is_detected_v<details::pdo_error_t, impl_t>) {
           static_cast<impl_t*>(this)->pdo_error();
         }
@@ -208,8 +208,8 @@ public:
       if (input.size() != sizeof(input_pdo)) {
         if (input_buffer_valid_) {
           logger_.info("Input size mismatch, expected {} bytes, got {} bytes", sizeof(input_pdo), input.size());
+          input_buffer_valid_ = false;
         }
-        input_buffer_valid_ = false;
         if constexpr (stx::is_detected_v<details::pdo_error_t, impl_t>) {
           static_cast<impl_t*>(this)->pdo_error();
         }
