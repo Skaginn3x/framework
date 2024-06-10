bc92e5aa9f9df2272050560801f23d7a5d6f5efe
Ómar Högni Guðmarsson <ohg@skaginn3x.com>
2024-06-10 15:30:41 +0000

    Allow ipc-ruler to be in memory

--- exes/ipc-ruler/src/ipc-ruler.cpp
+++ exes/ipc-ruler/src/ipc-ruler.cpp
@@ -1,12 +1,18 @@
 #include <sdbusplus/asio/connection.hpp>
 #include <tfc/confman/file_storage.hpp>
 #include <tfc/ipc/details/dbus_server_iface.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 auto main(int argc, char** argv) -> int {
-  tfc::base::init(argc, argv);
+  auto description{ tfc::base::default_description() };
+  bool in_memory = false;
+  description.add_options()("memory", po::bool_switch(&in_memory), "Run the database in memory, non persistent");
+  tfc::base::init(argc, argv, description);
 
   boost::asio::io_context ctx;
-  auto ipc_manager = std::make_unique<tfc::ipc_ruler::ipc_manager>();
+  auto ipc_manager = std::make_unique<tfc::ipc_ruler::ipc_manager>(in_memory);
 
   tfc::ipc_ruler::ipc_manager_server const dbus_ipc_server(ctx, std::move(ipc_manager));
 
