e5fbb90c834ef26748dc386e2d9dd9223965c3f9
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-06-18 14:25:24 +0000

    compile time checks for snitch client and runtime where relevant

--- libs/snitch/inc/public/tfc/snitch.hpp
+++ libs/snitch/inc/public/tfc/snitch.hpp
@@ -4,10 +4,13 @@
 #include <string_view>
 
 #include <fmt/core.h>
+#include <fmt/args.h>
 
+#include <tfc/logger.hpp>
 #include <tfc/stx/concepts.hpp>
 #include <tfc/stx/basic_fixed_string.hpp>
 #include <tfc/snitch/common.hpp>
+#include <tfc/snitch/format_extension.hpp>
 
 namespace tfc::snitch {
 
@@ -22,23 +25,40 @@ concept named_arg = requires(T t) {
   { t.value };
 };
 
-template <variance var, stx::basic_fixed_string description, stx::basic_fixed_string details>
+template <variance var, stx::basic_fixed_string in_description, stx::basic_fixed_string in_details>
 class alarm {
 public:
+  static constexpr std::string_view description{ in_description };
+  static constexpr std::string_view details{ in_details };
+  static constexpr auto description_arg_keys{ detail::arg_names<description>() };
+  static constexpr auto details_arg_keys{ detail::arg_names<details>() };
+  static constexpr auto keys_count{ std::max(description_arg_keys.size(), details_arg_keys.size()) };
+
   /// \example alarm<{}, "short desc", "long desc"> warn(fmt::arg("key", "value"), fmt::arg("key2", 1));
-  alarm(named_arg auto&& ... default_args) {
-    // todo
+  alarm(named_arg auto&& ... default_args) : default_values_{ std::make_pair(default_args.name, fmt::format("{}", default_args.value))... } {
+    static_assert(detail::check_all_arguments_named(description), "All arguments must be named");
+    static_assert(detail::check_all_arguments_no_format(description), "All arguments may not have format specifiers");
+    [[maybe_unused]] static constexpr int num_args = sizeof...(default_args);
+    static_assert(num_args <= keys_count, "Too many default arguments");
   }
 
   template <stx::invocable callback_t>
     requires (var.requires_acknowledgement)
-  void on_acknowledgement(callback_t&& callback) {
+  void on_ack(callback_t&& callback) {
   }
 
-
-
-
-
+  void set(named_arg auto&& ... args) {
+    fmt::dynamic_format_arg_store<fmt::format_context> store;
+    for (auto const& [key, value] : default_values_) {
+      store.push_back(fmt::arg(key.c_str(), value));
+    }
+    (store.push_back(args), ...);
+    logger_.debug(fmt::vformat(description, store));
+    logger_.debug(fmt::vformat(details, store));
+  }
+private:
+  std::unordered_map<std::string, std::string> default_values_;
+  logger::logger logger_{ "snitch" };
 };
 
 template <stx::basic_fixed_string description, stx::basic_fixed_string details = "">
--- libs/snitch/tests/snitch_test.cpp
+++ libs/snitch/tests/snitch_test.cpp
@@ -3,8 +3,39 @@
 
 #include <tfc/progbase.hpp>
 #include <tfc/snitch.hpp>
+#include <glaze/util/string_literal.hpp>
 
 using std::string_view_literals::operator""sv;
+
+namespace test {
+using tfc::snitch::detail::check_all_arguments_named;
+static_assert(check_all_arguments_named(glz::chars<"foo">));
+static_assert(check_all_arguments_named(glz::chars<"foo {name}">));
+static_assert(!check_all_arguments_named(glz::chars<"foo {}">));
+static_assert(!check_all_arguments_named(glz::chars<"foo {:.2f}">));
+
+using tfc::snitch::detail::check_all_arguments_no_format;
+static_assert(check_all_arguments_no_format(glz::chars<"foo {name}">));
+static_assert(!check_all_arguments_no_format(glz::chars<"foo {name:.2f}">));
+
+using tfc::snitch::detail::arg_count;
+static_assert(arg_count(glz::chars<"foo">) == 0);
+static_assert(arg_count(glz::chars<"foo {name}">) == 1);
+static_assert(arg_count(glz::chars<"foo {name} {name}">) == 2);
+static_assert(arg_count(glz::chars<"foo {name} {name} {name}">) == 3);
+
+using tfc::snitch::detail::arg_names;
+static_assert(arg_names<glz::chars<"foo {name}">>()[0] == "name");
+static_assert(arg_names<glz::chars<"foo {name} {name}">>()[0] == "name");
+
+static_assert(arg_names<glz::chars<"foo {name} {name}">>().size() == 1);
+// todo this below shouldn't be possible, remove if causes compile error and celebrate
+static_assert(arg_names<glz::chars<"foo {name} {name}">>()[1] == "");
+
+static_assert(arg_names<glz::chars<"foo {name} {name} {bar}">>()[1] == "bar");
+static_assert(arg_names<glz::chars<"foo {name} {name} {bar}">>().size() == 2);
+
+}  // namespace test
 
 auto main(int argc, char** argv) -> int {
   using boost::ut::operator""_test;
@@ -13,9 +44,10 @@ auto main(int argc, char** argv) -> int {
   tfc::base::init(argc, argv);
 
   "snitches"_test = [] {
-    tfc::snitch::info<"short desc", "long desc"> info(fmt::arg("key1", "value"), fmt::arg("key2", 1));
-    fmt::print("{}", 1);
-    // warn.on_acknowledgement([]{});
+    tfc::snitch::info<"short desc {name}", "long desc {name} {index}"> tank(fmt::arg("name", "hello"), fmt::arg("index", 42));
+    tank.set();
+
+    // warn.on_ack([]{});
 
   };
 }
